7586ddeedbcbbcab8631fc072f8c4996a9d0b36a
Jelle Aalbers <jelleaalbers@gmail.com>
2019-10-11 14:11:17 +0200

    Windows workarounds

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -8,29 +8,36 @@ test_run_id = '180423_1021'
 
 def test_straxen():
     with tempfile.TemporaryDirectory() as temp_dir:
-        print("Temporary directory is ", temp_dir)
-        os.chdir(temp_dir)
+        try:
+            print("Temporary directory is ", temp_dir)
+            os.chdir(temp_dir)
 
-        print("Downloading test data (if needed)")
-        st = straxen.contexts.demo()
+            print("Downloading test data (if needed)")
+            st = straxen.contexts.demo()
 
-        run_df = st.select_runs(available='raw_records')
-        run_id = run_df.iloc[0]['name']
-        assert run_id == test_run_id
+            run_df = st.select_runs(available='raw_records')
+            print(run_df)
+            run_id = run_df.iloc[0]['name']
+            assert run_id == test_run_id
 
-        print("Test processing")
-        df = st.get_df(run_id, 'event_info')
-        assert len(df) > 0
-        assert 'cs1' in df.columns
-        assert df['cs1'].sum() > 0
+            print("Test processing")
+            df = st.get_df(run_id, 'event_info')
+            assert len(df) > 0
+            assert 'cs1' in df.columns
+            assert df['cs1'].sum() > 0
 
-        # TODO: find a way to break up the tests
-        # surely pytest has common startup/cleanup?
+            # TODO: find a way to break up the tests
+            # surely pytest has common startup/cleanup?
 
-        print("Test mini analysis")
-        @straxen.mini_analysis(requires=('raw_records',))
-        def count_rr(raw_records):
-            return len(raw_records)
+            print("Test mini analysis")
+            @straxen.mini_analysis(requires=('raw_records',))
+            def count_rr(raw_records):
+                return len(raw_records)
 
-        n = st.count_rr(test_run_id)
-        assert n > 100
+            n = st.count_rr(test_run_id)
+            assert n > 100
+
+        # On windows, you cannot delete the current process'
+        # working directory, so we have to chdir out first.
+        finally:
+            os.chdir('..')
