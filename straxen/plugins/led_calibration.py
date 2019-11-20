b41906691e43a0df60aa1ec815c8382f8e081487
Giovanni Volta <gvolta@dali-login1.rcc.local>
2019-11-20 10:08:40 -0600

    Functions moved outside the definition of the PlugIn Class

--- straxen/plugins/led_calibration.py
+++ straxen/plugins/led_calibration.py
@@ -1,15 +1,19 @@
-import straxen
+'''
+Dear nT analyser, 
+if you want to complain please contact: chiara@physik.uzh.ch, gvolta@physik.uzh.ch
+'''
+
 import strax
+import numpy as np
 import pandas as pd
 from tqdm import tqdm
-import numpy as np
 import numba
 from numba import njit
 
-export, __all__ = strax.exporter()
+#export, __all__ = strax.exporter()
+# QUESTION: what does 'export, __all__ = strax.exporter()' do?
+# ANSWER: [fill in]
 
-
-@export
 @strax.takes_config(
     strax.Option('LED_window',
                  default=(125, 250),
@@ -28,8 +32,8 @@ class LEDCalibration(strax.Plugin):
     - amplitudeNOISE: amplitude of the LED on run in a window far from the signal one.
     '''
     
-    __version__ = '0.0.5'
-    depends_on = 'raw_records'
+    __version__ = '0.0.6'
+    depends_on = ('raw_records',)
     # Options below copied from other plugins, need to be reviewed by an expert
     data_kind = 'led_cal_0' 
     compressor = 'zstd'
@@ -44,48 +48,6 @@ class LEDCalibration(strax.Plugin):
              ('dt', np.int16, 'Time resolution in ns'),
              ('length', np.int32, 'Length of the interval in samples')
             ]
-    
-    def get_amplitude(raw_records, window):
-        
-        '''
-        Needed for the SPE computation.
-        Take the maximum in two different regions, where there is the signal and where there is not.
-        '''
-
-        on = []
-        off = []
-        for r in raw_records:
-            amp_LED = np.max(r['data'][window[0]:window[1]])
-            amp_NOISE = np.max(r['data'][2*window[0]:2*window[1]])
-            on.append(amp_LED)
-            off.append(amp_NOISE)
-        on = np.array(on, dtype=[('amplitudeLED', '<i4')])
-        off = np.array(off, dtype=[('amplitudeNOISE', '<i4')])
-        return on, off
-
-    def get_area(raw_records, window):
-        
-        '''
-        Needed for the gain computation.
-        Sum the data in the defined window to get the area.
-        This is done in 6 integration window and it returns the average area.
-        '''
-        
-        left = window[0]
-        end_pos = [window[1]+2*i for i in range(6)]
-        n_channel_s = np.arange(0, 249, 1) # TODO: to change during nT commissioning or add in configuration options
-        Area = np.array(raw_records[['channel', 'area']],dtype=[('channel','int16'),('area','float32')])
-
-        for n_channel in n_channel_s:
-            wf_tmp = raw_records[raw_records['channel'] == n_channel]
-            area = 0
-            for right in end_pos:
-                area += wf_tmp['data'][:,left:right].sum(axis=1)
-
-        mask = np.where(Area['channel'] == n_channel)[0]
-        Area['area'][mask] = area.astype(np.float)/6.
-        return Area
-
     
     def compute(self, raw_records):
         
@@ -105,3 +67,43 @@ class LEDCalibration(strax.Plugin):
         temp['area'] = area['area']
         
         return temp
+
+# QUESTIONS: can some nice functions of numba.njit be used? What does @export mean?
+# ANSWERS: [fill in]
+    
+def get_amplitude(raw_records, window):
+    '''
+    Needed for the SPE computation.
+    Take the maximum in two different regions, where there is the signal and where there is not.
+    '''
+    on = []
+    off = []
+    for r in raw_records:
+        amp_LED = np.max(r['data'][window[0]:window[1]])
+        amp_NOISE = np.max(r['data'][2*window[0]:2*window[1]])
+        on.append(amp_LED)
+        off.append(amp_NOISE)
+    on = np.array(on, dtype=[('amplitudeLED', '<i4')])
+    off = np.array(off, dtype=[('amplitudeNOISE', '<i4')])
+    return on, off
+
+def get_area(raw_records, window):
+    '''
+    Needed for the gain computation.
+    Sum the data in the defined window to get the area.
+    This is done in 6 integration window and it returns the average area.
+    '''
+    left = window[0]
+    end_pos = [window[1]+2*i for i in range(6)]
+    n_channel_s = np.arange(0, 249, 1) # TODO: to change during nT commissioning or add in configuration options
+    Area = np.array(raw_records[['channel', 'area']],dtype=[('channel','int16'),('area','float32')])
+    
+    for n_channel in n_channel_s:
+        wf_tmp = raw_records[raw_records['channel'] == n_channel]
+        area = 0
+        for right in end_pos:
+            area += wf_tmp['data'][:,left:right].sum(axis=1)
+
+    mask = np.where(Area['channel'] == n_channel)[0]
+    Area['area'][mask] = area.astype(np.float)/6.
+    return Area
