2e19030e43ab0c804b1c7b4d40d4a62b223a11e0
Giovanni Volta <gvolta@dali-login1.rcc.local>
2019-12-03 07:57:10 -0600

    Spero funzioni (Giovanni)

--- straxen/plugins/led_calibration.py
+++ straxen/plugins/led_calibration.py
@@ -79,15 +79,18 @@ def get_amplitude(raw_records, LED_window, noise_window):
     Needed for the SPE computation.
     Take the maximum in two different regions, where there is the signal and where there is not.
     '''
-    on = np.zeros(len(raw_records))
-    off = np.zeros(len(raw_records))
-    for r in raw_records:
-        amp_LED = np.max(r['data'][LED_window[0]:LED_window[1]])
-        amp_NOISE = np.max(r['data'][noise_window[0]:noise_window[1]])
-        on.append(amp_LED)
-        off.append(amp_NOISE)
-    on = np.array(on, dtype=[('amplitudeLED', '<i4')])
-    off = np.array(off, dtype=[('amplitudeNOISE', '<i4')])
+    #on = np.zeros(len(raw_records), dtype=[('amplitudeLED', '<i4')])
+    #off = np.zeros(len(raw_records), dtype=[('amplitudeNOISE', '<i4')])
+    
+    on  = np.array(np.max(r['data'][LED_window[0]:LED_window[1]]) for r in raw_records, dtype=[('amplitudeLED', '<i4')])
+    off = np.array(np.max(r['data'][noise_window[0]:noise_window[1]]) for r in raw_records, dtype=[('amplitudeNOISE', '<i4')])
+    #for r in raw_records:
+    #    amp_LED = np.max(r['data'][LED_window[0]:LED_window[1]])
+    #    amp_NOISE = np.max(r['data'][noise_window[0]:noise_window[1]])
+    #    on.append(amp_LED)
+    #    off.append(amp_NOISE)
+    #on = np.array(on, dtype=[('amplitudeLED', '<i4')])
+    #voff = np.array(off, dtype=[('amplitudeNOISE', '<i4')])
     return on, off
 
 def get_area(raw_records, LED_window):
