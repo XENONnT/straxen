24cd1b03254f90f689fb15369762c7005fb7ddd8
Jelle Aalbers <jelleaalbers@gmail.com>
2020-01-17 16:06:29 +0100

    Bump version: 0.3.5 → 0.4.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open('HISTORY.md') as file:
     history = file.read()
 
 setuptools.setup(name='straxen',
-                 version='0.3.5',
+                 version='0.4.0',
                  description='Streaming analysis for XENON',
                  author='Straxen contributors, the XENON collaboration',
                  url='https://github.com/XENONnT/straxen',
--- straxen/__init__.py
+++ straxen/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.3.5'
+__version__ = '0.4.0'
 
 from .common import *
 from .itp_map import *
