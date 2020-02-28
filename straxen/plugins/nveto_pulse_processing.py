e929823a80736df93074943998509ded124ab703
Wenz <dwenz@students.uni-mainz.de>
2020-02-28 13:48:35 +0100

    Added the rest of the nVETOPulseEdges plugin. Also did some minor modifications and bug fixes.

--- straxen/plugins/nveto_pulse_processing.py
+++ straxen/plugins/nveto_pulse_processing.py
@@ -2,10 +2,10 @@ import numba
 import numpy as np
 
 import strax
+import straxen
 from straxen import get_to_pe
 from straxen import get_resource
 export, __all__ = strax.exporter()
-
 
 
 @export
@@ -36,10 +36,10 @@ class nVETOPulseProcessing(strax.Plugin):
     depends_on = 'nveto_raw_records'
 
     provides = 'nveto_records'
-    dtype = nveto_pulses_dtype  # Might be the same as records.
+    dtype = straxen.nveto_pulses_dtype()  # Might be the same as records.
 
     def setup(self):
-        self.hit_thresholds = strax.get_resource(self.config['adc_thresholds'], fmt='npy')
+        self.hit_thresholds = get_resource(self.config['adc_thresholds'], fmt='npy')
 
     def compute(self, nveto_raw_records):
         # Do not trust in DAQ + strax.baseline to leave the
@@ -99,12 +99,10 @@ class nVETOPulseEdges(strax.Plugin):
     depends_on = 'nveto_records'
 
     provides = 'nveto_pulses'
-    dtype = nveto_pulses_dtype()
+    dtype = straxen.nveto_pulses_dtype()
 
     def setup(self):
-        self.to_pe = get_to_pe(self.run_id, self.config['to_pe_file'])
         self.hit_thresholds = get_resource(self.config['adc_thresholds'], fmt='npy')
-
 
     def compute(self, nveto_records):
         # Search again for hits in records:
@@ -121,7 +119,7 @@ class nVETOPulseEdges(strax.Plugin):
         nveto_pulses = strax.sort_by_time(nveto_pulses)
 
         # Check if hits can be split:
-
+        nveto_pulses = split_pulses(nveto_records, nveto_pulses)
 
         return dict(nveto_pulses=nveto_pulses)
 
@@ -145,7 +143,8 @@ def nveto_pulses_dtype(n_widths=11):
 @numba.njit(nogil=True, cache=True)
 def concat_overlapping_hits(hits,
                             extensions,
-                            last_hit_in_channel, _result_buffer=None):
+                            last_hit_in_channel,
+                            _result_buffer=None):
     """
     Function which concatenates overlapping hits into a single one.
 
@@ -157,12 +156,15 @@ def concat_overlapping_hits(hits,
             The array must be empty and containing the fields "time", "endtime",
             "channel" and "dt".
 
-    Returns:
-        np.array: Array of the pre_nveto_pulses datastructure containing the start
-            and end points of the PMT pulses in unix time.
+    Keyword Args:
+        _result_buffer (None): please see strax.growing_result.
 
     TODO: Somehow when making last_hit_in_channel a keyword argument
         numba crashes...
+
+    Returns:
+        np.array: Array of the pre_nveto_pulses data structure containing the start
+            and end points of the PMT pulses in unix time.
     """
     buffer = _result_buffer
     offset = 0
@@ -197,18 +199,34 @@ def concat_overlapping_hits(hits,
                 res['channel'] = lhc['channel']
                 res['dt'] = lhc['dt']
                 offset += 1
+                if offset == len(buffer):
+                    yield offset
+                    offset = 0
 
                 # Updating current last hit:
                 lhc['time'] = st
                 lhc['endtime'] = et
                 lhc['channel'] = hc
 
+    # We went through so now we have to save all remaining hits:
+    mask = last_hit_in_channel['time'] != 0
+    for lhc in last_hit_in_channel[mask]:
+        res = buffer[offset]
+        res['time'] = lhc['time']
+        res['endtime'] = lhc['endtime']
+        res['channel'] = lhc['channel']
+        res['dt'] = lhc['dt']
+        offset += 1
+        if offset == len(buffer):
+            yield offset
+            offset = 0
+    yield offset
 
-@export
+
 @numba.njit(cache=True, nogil=True)
 def get_pulse_data(nveto_records, hit, start_index=0):
     """
-    Seraches in a given nveto_record data_chunk for the data a
+    Searches in a given nveto_record data_chunk for the data a
     specified hit.
 
     Args:
@@ -226,7 +244,7 @@ def get_pulse_data(nveto_records, hit, start_index=0):
         int: Next start index.
 
     Notes:
-        For the ussage of start_index function assumes nveto_records are
+        For the usage of start_index function assumes nveto_records are
         sorted in time. (Should also be true for hit(s) if looping over
         them).
     """
@@ -257,11 +275,8 @@ def get_pulse_data(nveto_records, hit, start_index=0):
 
         # We found the start of our event:
         if dt <= nvr_length_time:
-            if not found_start:
-                start_record = index
-
             if dt < 0:
-                # If this happened our hit should have been in an earlier
+                # If this happend our hit should have been in an earlier
                 # record.
                 # TODO: should we through an error here?
                 res[:] = -42
@@ -283,6 +298,127 @@ def get_pulse_data(nveto_records, hit, start_index=0):
 
         if res_start == nsamples:
             return res, start_index
+
+
+@strax.growing_result(nveto_pulses_dtype(), chunk_size=int(1e4))
+@numba.njit(cache=True, nogil=True)
+def split_pulses(records, pulses, min_height=25, min_ratio=0, _result_buffer=None):
+    """
+    Function which checks for a given pulse if the pulse should be
+    split.
+
+    Note:
+        A pulse is split at the local minimum between two maxima if
+        one the height difference between one of the maxima and the
+        minimum exceeds a certain threshold, or if
+
+    Args:
+        records (np.array):
+        pulses (np.array):
+
+    Returns:
+        np.array
+    """
+    buffer = _result_buffer
+    offset = 0
+    record_offset = 0
+    for pulse in pulses:
+        # Get data and split pulses:
+        data, record_offset = get_pulse_data(records, pulse, record_offset)
+        edges = _split_pulse(data, (0, len(data)),  min_height=min_height, min_ratio=min_ratio)
+        edges = edges[edges >= 0]
+
+        # Convert edges into times:
+        start_time = pulse['time']
+        edges_times = edges * pulse['dt'] + start_time
+        # Loop over edges and store them:
+        nedges = len(edges_times) - 1
+        for ind in range(nedges):
+            res = buffer[offset]
+            res['time'] = edges_times[ind]
+            res['endtime'] = edges_times[ind + 1]
+            res['channel'] = pulse['channel']
+            if nedges:
+                res['split_i'] = ind + 1
+            else:
+                res['split_i'] = ind  # 0 is reserved for events which were not split.
+            offset += 1
+            if len(buffer) == offset:
+                yield offset
+    yield offset
+
+
+@numba.njit
+def _split_pulse(data, edges, min_height=25, min_ratio=0):
+    """
+    Function which splits the PMT pulses if necessary.
+
+    Args:
+        data (np.array):
+        edges (tuble):
+
+    Keyword Args:
+        min_height (int):
+        min_ratio (float):
+
+    Returns:
+        np.array: Array containing the indices of the pulses. The
+            rest is set to -1. The array has the same length as
+            np.diff(edges), since we do not know apriori the number
+            of pulses.
+    """
+    d = data[edges[0]:edges[1]]
+    res = np.ones(len(d), np.int16) * -1  # There cannot be more splot points than sample
+    ind = 1
+    res[0] = edges[0]
+    for split_point in find_split_points(d, min_height=min_height, min_ratio=min_ratio):
+        res[ind] = split_point + edges[0]
+        ind += 1
+    if ind == 1:
+        res[ind] = edges[1]
+    return res
+
+
+# -----------------------------------------------
+# Taken from split_peaks and adopted to our needs
+# Thanks to the author(Jelle?).
+# -----------------------------------------------
+@numba.jit(nopython=True, nogil=True, cache=True)
+def find_split_points(w, min_height=0, min_ratio=0):
+    """"Yield indices of prominent local minima in w
+    If there was at least one index, yields len(w)-1 at the end
+    """
+    found_one = False
+    last_max = -99999999999999.9
+    min_since_max = 99999999999999.9
+    min_since_max_i = 0
+
+    for i, x in enumerate(w):
+        if x < min_since_max:
+            # New minimum since last max
+            min_since_max = x
+            min_since_max_i = i
+
+        if min(last_max, x) > max(min_since_max + min_height,
+                                  min_since_max * min_ratio):
+            # Significant local minimum: tell caller,
+            # reset both max and min finder
+            yield min_since_max_i
+            found_one = True
+            last_max = x
+            min_since_max = 99999999999999.9
+            min_since_max_i = i
+
+        if x > last_max:
+            # New max, reset minimum finder state
+            # Notice this is AFTER the split check,
+            # to accomodate very fast rising second peaks
+            last_max = x
+            min_since_max = 99999999999999.9
+            min_since_max_i = i
+
+    if found_one:
+        yield len(w)
 
 
 @export
