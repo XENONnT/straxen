e22e0348374211c5b96c1c2b55dfa890b836b149
jorana <jorana@nikhef.nl>
2020-03-16 11:59:59 +0100

    add n_tpc_pmts_1T to exports

--- straxen/common.py
+++ straxen/common.py
@@ -19,7 +19,7 @@ import pandas as pd
 
 import strax
 export, __all__ = strax.exporter()
-__all__ += ['straxen_dir', 'first_sr1_run', 'tpc_r', 'n_tpc_pmts', 'aux_repo']
+__all__ += ['straxen_dir', 'first_sr1_run', 'tpc_r', 'n_tpc_pmts_1T', 'aux_repo']
 
 straxen_dir = os.path.dirname(os.path.abspath(
     inspect.getfile(inspect.currentframe())))
