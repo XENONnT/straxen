420f2a8e94de9963056ef1c24e1d1e6c1a4f3f33
Giovanni Volta <gvolta@dali-login1.rcc.local>
2020-03-23 10:08:50 -0500

    Fix __init__.py

--- straxen/plugins/__init__.py
+++ straxen/plugins/__init__.py
@@ -16,9 +16,6 @@ from .peak_processing import *
 from . import event_processing
 from .event_processing import *
 
-from . import cuts
-from .cuts import *
-
 from . import led_calibration
 from .led_calibration import *
 
