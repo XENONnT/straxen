adc65bcdec077162b2fecfc7a78fa28d13e9e27c
Jelle Aalbers <jelleaalbers@gmail.com>
2020-04-04 11:36:47 +0200

    Prevent divergence in hitfinder options

--- straxen/plugins/pulse_processing.py
+++ straxen/plugins/pulse_processing.py
@@ -3,7 +3,17 @@ import numpy as np
 
 import strax
 import straxen
+
 export, __all__ = strax.exporter()
+
+
+# These are also needed in peaklets, since hitfinding is repeated
+HITFINDER_OPTIONS = tuple([
+    strax.Option(
+        'hit_min_amplitude',
+        default=15,
+        help='Minimum hit amplitude in ADC counts above baseline. '
+             'Specify as a tuple of length n_tpc_pmts, or a number.')])
 
 
 @export
@@ -51,12 +61,6 @@ export, __all__ = strax.exporter()
         help='Save (left, right) samples besides hits; cut the rest'),
 
     strax.Option(
-        'hit_min_amplitude',
-        default=15,
-        help='Minimum hit amplitude in ADC counts above baseline. '
-             'Specify as a tuple of length n_tpc_pmts, or a number.'),
-
-    strax.Option(
         'n_tpc_pmts', type=int,
         help='Number of TPC PMTs'),
 
@@ -66,6 +70,7 @@ export, __all__ = strax.exporter()
         help='Crash if any of the pulses in raw_records overlap with others '
              'in the same channel'),
 
+    *HITFINDER_OPTIONS,
 )
 class PulseProcessing(strax.Plugin):
     """
