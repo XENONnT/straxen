178e546b6d8737f78ea94962910d3329e662b005
Daniel Wenz <43881800+WenzDaniel@users.noreply.github.com>
2020-06-02 18:18:27 +0200

    Fixed +1 bug for nhits.
    Touching window return indices hence we have to add one.

--- straxen/plugins/peaklet_processing.py
+++ straxen/plugins/peaklet_processing.py
@@ -153,6 +153,7 @@ class Peaklets(strax.Plugin):
         # Update nhits of peaklets:
         counts = strax.touching_windows(hits, peaklets)
         counts = np.diff(counts, axis=1).flatten()
+        counts += 1
         peaklets['n_hits'] = counts
         
         return dict(peaklets=peaklets,
