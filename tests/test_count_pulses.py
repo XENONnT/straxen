9f44516170b586808e87f46cfead3fd7961b3990
Daniel Wenz <43881800+WenzDaniel@users.noreply.github.com>
2020-07-01 12:43:11 +0200

    Fixed NO_PULSE_COUNTS bug.

--- tests/test_count_pulses.py
+++ tests/test_count_pulses.py
@@ -47,7 +47,7 @@ def _check_pulse_count(records):
         
         # Check baseline values:
         # nan does not exist for integer:
-        mean = strax.NO_PULSE_COUNTS if np.isnan(np.mean(rc0['baseline'])) else int(np.mean(rc0['baseline']))
+        mean = straxen.NO_PULSE_COUNTS if np.isnan(np.mean(rc0['baseline'])) else int(np.mean(rc0['baseline']))
         assert count['baseline_mean'][ch] == mean
         if not np.isnan(count['baseline_rms_mean'][ch]):
             assert count['baseline_rms_mean'][ch] == np.mean(rc0['baseline_rms'])
