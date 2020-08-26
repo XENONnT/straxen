8ba86b0cb71b2377aa4143ac43770a6f0c82fb02
Daniel Wenz <dwenz@kabelmail.de>
2020-08-26 13:55:24 +0200

    Added docstrings to plugins.

--- straxen/plugins/nveto_pulse_processing.py
+++ straxen/plugins/nveto_pulse_processing.py
@@ -22,7 +22,8 @@ export, __all__ = strax.exporter()
 )
 class nVETOPulseProcessing(strax.Plugin):
     """
-    nVETO equivalent of pulse processing.
+    nVETO equivalent of pulse processing. Not much more more to say
+    about.
     """
     __version__ = '0.0.4'
 
