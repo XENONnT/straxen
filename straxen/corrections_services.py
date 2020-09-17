ee0b5ee2e506b9a9fe008a3412e3ba836de6a5c6
ahiguera-mx <ahiguera-mx@fried.rice.edu>
2020-09-17 16:51:27 -0500

    Improve mongo access

--- straxen/corrections_services.py
+++ straxen/corrections_services.py
@@ -6,9 +6,11 @@ import pytz
 from bson.son import SON
 import pymongo
 import numpy as np
+from warnings import warn
 
 import strax
 import straxen
+from .rundb import default_mongo_url, backup_mongo_urls
 
 export, __all__ = strax.exporter()
 
@@ -21,20 +23,41 @@ class CmtServices():
     stage to remove detector effects. Information on the strax implementation
     can be found at https://github.com/AxFoundation/strax/blob/master/strax/corrections.py
     """
-    def __init__(self):
-
-        self.interface = strax.CorrectionsInterface(
-                host='xenon1t-daq.lngs.infn.it',
-                username=straxen.get_secret('correctionsdb_username'),
-                password=straxen.get_secret('correctionsdb_password'),
-                database_name='corrections')
-
+    def __init__(self, host=None, username='nt_analysis', password=None):
         """
         :param host: DB host
         :param username: DB username
         :param password: DB password
-        :param database_name: DB name
         """
+        self.host = host
+        self.username = username
+        self.password = password
+        if self.username.endswith('analysis'):
+            self.password = straxen.get_secret('rundb_password')
+
+        if host is None:
+            mongo_connections = [default_mongo_url, *backup_mongo_urls]
+            for url in mongo_connections:
+                try:
+                    mongo_url = f'mongodb://{url}'
+
+                    self.interface = strax.CorrectionsInterface(
+                        host=mongo_url,
+                        username=self.username,
+                        password=self.password,
+                        database_name='corrections')
+                    break
+                except pymongo.errors.ServerSelectionTimeoutError:
+                    warn(f'Cannot connect to to Mongo url: {url}')
+                    if url == mongo_connections[-1]:
+                        raise pymongo.errors.ServerSelectionTimeoutError(
+                            'Cannot connect to any Mongo url')
+        else:
+            self.interface = strax.CorrectionsInterface(
+                    host=self.host,
+                    username=self.username,
+                    password=self.password,
+                    database_name='corrections')
 
     def get_corrections_config(self, run_id, correction=None, config_model=None):
         """
@@ -175,10 +198,7 @@ class CmtServices():
                 {"$sort": SON([("time", 1)])}]
 
         if xenon1t:
-            collection = pymongo.MongoClient(
-                    'mongodb://pax:@xenon1t-daq.lngs.infn.it:27017/run',
-                    password=straxen.get_secret('xenon1t_rundb_password'))['run']['runs_new']
-
+            collection = pymongo.MongoClient(mongo_url, password=rundb_password)['run']['runs_new']
             pipeline = [
                     {'$match':
                         {"name": run_id, "detector": "tpc", "end":
