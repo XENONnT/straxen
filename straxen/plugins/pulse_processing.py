285ae791e671b9d31ae73e1f2ba44b990ea3cdad
sophiaandaloro <sja5@rice.edu>
2020-09-23 11:48:40 -0500

    HE veto length clipped at chunk end time

--- straxen/plugins/pulse_processing.py
+++ straxen/plugins/pulse_processing.py
@@ -176,7 +176,7 @@ class PulseProcessing(strax.Plugin):
 
         if len(r) and self.hev_enabled:
             r, r_vetoed, veto_regions = software_he_veto(
-                r, self.to_pe,
+                r, self.to_pe, end,
                 area_threshold=self.config['tail_veto_threshold'],
                 veto_length=self.config['tail_veto_duration'],
                 veto_res=self.config['tail_veto_resolution'],
@@ -257,7 +257,7 @@ class PulseProcessingHighEnergy(PulseProcessing):
 
 
 @export
-def software_he_veto(records, to_pe,
+def software_he_veto(records, to_pe, chunk_end,
                      area_threshold=int(1e5),
                      veto_length=int(3e6),
                      veto_res=int(1e3),
@@ -308,11 +308,10 @@ def software_he_veto(records, to_pe,
     #  - Have a fixed maximum length (else we can't use the strax hitfinder on them)
     #  - Never extend beyond the current chunk
     #  - Do not overlap
-    # TODO: pass the chunk endtime! For now we just use the last record endtime
     veto_start = peaks['time']
     veto_end = np.clip(peaks['time'] + veto_length,
                        None,
-                       strax.endtime(records[-1]))
+                       chunk_end)
     veto_end[:-1] = np.clip(veto_end[:-1], None, veto_start[1:])
 
     # 2b. Convert these into strax record-like objects
