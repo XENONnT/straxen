f37ac461aa6b2b200fed15feda44519b983dcaa8
Evan Shockley <ershockley@uchicago.edu>
2020-10-20 19:27:19 -0500

    Bypass utilix errors if no xenon_config found, raise a warning instead

--- straxen/__init__.py
+++ straxen/__init__.py
@@ -1,8 +1,12 @@
 __version__ = '0.11.0'
 
 # load configuration file using utilix
-from utilix.config import Config
-uconfig = Config()
+try:
+    from utilix.config import Config
+    uconfig = Config()
+except FileNotFoundError:
+    print("Warning: no xenon configuration file found")
+
 
 from .common import *
 from .itp_map import *
