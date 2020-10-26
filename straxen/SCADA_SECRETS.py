8cd820f4a57189de2da668034a000fe2a19f3c03
Daniel Wenz <dwenz@kabelmail.de>
2020-10-26 08:27:08 +0100

    Added ValueError

---
+++ straxen/SCADA_SECRETS.py
@@ -0,0 +1,9 @@
+# ------------------------------------------------------------------
+# These are xenon secrets how to implement those into straxen?
+SCData_URL = 'https://xenon1t-daq.lngs.infn.it/slowcontrol/GetSCData?'
+SCLastValue_URL = 'https://xenon1t-daq.lngs.infn.it/slowcontrol/getLastMeasuredValue?'
+SCADA_SECRETS = {"QueryType": "rawbytime",
+                 "username": 'slowcontrolwebserver',
+                 "api_key": 'ssn1sslkvdhittoywjk88w9cmbxf4fmprn68r8hm',
+                 }
+# ------------------------------------------------------------------
