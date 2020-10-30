8fe0f17f03aeae9213cd9019b52e701332fd139a
Daniel Wenz <dwenz@kabelmail.de>
2020-10-30 10:08:09 +0100

    Added posibility to interpolate values.

--- straxen/scada.py
+++ straxen/scada.py
@@ -36,6 +36,8 @@ class SCADAInterface:
                          end=None,
                          run_id=None,
                          time_selection_kwargs={'full_range': True},
+                         filling_kwargs={},
+                         interpolation=False,
                          value_every_seconds=1):
         """
         Function which returns XENONnT slow control values for a given
@@ -130,6 +132,8 @@ class SCADAInterface:
                                 end,
                                 parameter_key,
                                 parameter_name,
+                                interpolation,
+                                filling_kwargs,
                                 value_every_seconds=1):
         """
         Function to query the values of a single parameter from SCData.
@@ -197,18 +201,26 @@ class SCADAInterface:
         except ValueError:
             pass
 
-        # Now fill values in between like Scada would do:
-        df.ffill(inplace=True)
+        # Let user decided whether to ffill or interpolate:
+        if interpolation:
+            df.interpolate(**filling_kwargs, inplace=True)
+        else:
+            # Now fill values in between like Scada would do:
+            df.ffill(**filling_kwargs, inplace=True)
         df.reset_index(inplace=True)
 
         # Step 4. Down-sample data if asked for:
         if value_every_seconds > 1:
-            nt, nv = _average_scada(df['time'].astype(np.int64).values,
-                                    df[parameter_key].values,
-                                    value_every_seconds)
-            df = pd.DataFrame()
-            df['time'] = nt.astype('<M8[ns]')
-            df[parameter_key] = nv
+            if interpolation:
+                warnings.warn('Cannot use interpolation and running average at the same time.'
+                              ' Deactivated the running average.')
+            else:
+                nt, nv = _average_scada(df['time'].astype(np.int64).values,
+                                        df[parameter_key].values,
+                                        value_every_seconds)
+                df = pd.DataFrame()
+                df['time'] = nt.astype('<M8[ns]')
+                df[parameter_key] = nv
 
         return df
 
