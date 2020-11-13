658480a50da3e52635796d68f1c6b1cf9b996525
Joran Angevaare <jorana@nikhef.nl>
2020-11-13 17:40:57 +0100

    Revert "Bump version: 0.12.0 → 0.12.1"
    This reverts commit 4b0a0877339816368505cd3615ccf328d29c3072.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open('HISTORY.md') as file:
     history = file.read()
 
 setuptools.setup(name='straxen',
-                 version='0.12.1',
+                 version='0.12.0',
                  description='Streaming analysis for XENON',
                  author='Straxen contributors, the XENON collaboration',
                  url='https://github.com/XENONnT/straxen',
