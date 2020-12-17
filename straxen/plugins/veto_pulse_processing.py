4df49c3896b738ad1edab30d86d41ccb54984b8b
Joran Angevaare <jorana@nikhef.nl>
2020-12-17 18:31:54 +0100

    also up nv

--- straxen/plugins/veto_pulse_processing.py
+++ straxen/plugins/veto_pulse_processing.py
@@ -31,7 +31,7 @@ class nVETOPulseProcessing(strax.Plugin):
         2. Find hits and apply ZLE
         3. Remove empty fragments.
     """
-    __version__ = '0.0.5'
+    __version__ = '0.0.6'
 
     parallel = 'process'
     rechunk_on_save = False
