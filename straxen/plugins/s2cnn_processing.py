e426e6b323d0b48f1d314dad24bb9afc6ba79eb4
Andrii Terliuk <andrey.terlyuk@gmail.com>
2021-01-06 18:59:16 -0600

    Changed naming convention, added modoule that runs event reconstruction from stored areas

--- straxen/plugins/s2cnn_processing.py
+++ straxen/plugins/s2cnn_processing.py
@@ -80,9 +80,6 @@ export, __all__ = strax.exporter()
                  default=10),
     strax.Option('n_top_pmts', default=straxen.n_top_pmts,
                  help="Number of top PMTs"),
-    strax.Option('min_reconstruction_area',
-                 help='Skip reconstruction if area (PE) is less than this',
-                 default=10),
     strax.Option("s2_cnn_model_path", 
                  help="Path to the CNN model in hdf5 format. WARING, this should include the whole model file and not the weights file", 
                  default="CNN_dw_3L_cm_maxnorm_A_lin_5_2000__Z_const_-1.hdf5"
@@ -91,7 +88,7 @@ export, __all__ = strax.exporter()
                  help="Store patterns as used by CNN",
                  default=True)
 )
-class CNNS2PosRec(strax.Plugin):
+class S2CNNPosRec(strax.Plugin):
     """
     This pluging provides S2 position reconstruction for top array
     
@@ -103,7 +100,7 @@ class CNNS2PosRec(strax.Plugin):
 
     depends_on = ('peaks',)
     parallel = False
-    provides = "CNNS2PosRec"
+    provides = "S2CNNPosRec"
     __version__ = '0.0.1'
     def infer_dtype(self):
         dtype = [('x_TFS2CNN', np.float32,
@@ -158,7 +155,7 @@ class CNNS2PosRec(strax.Plugin):
         return result
 
 @export
-class CNNS2PeakPositions(strax.Plugin):
+class S2CNNPeakPositions(strax.Plugin):
     """
     This pluging puts XY for S2 peaks as peak_positions
 
@@ -166,7 +163,7 @@ class CNNS2PeakPositions(strax.Plugin):
         - x - reconstructed X position in [ cm ]
         - y - reconstructed Y position in [ cm ]
     """
-    depends_on = ('CNNS2PosRec',)
+    depends_on = ('S2CNNPosRec',)
     parallel = False
     provides = 'peak_positions'
     __version__ = '0.0.1'
@@ -183,4 +180,82 @@ class CNNS2PeakPositions(strax.Plugin):
         result['time'], result['endtime'] = peaks['time'], strax.endtime(peaks)
         result['x'] = peaks['x_TFS2CNN']
         result['y'] = peaks['y_TFS2CNN']
+        return result
+
+from time import time
+export, __all__ = strax.exporter()
+@export
+@strax.takes_config(
+    strax.Option('min_reconstruction_area',
+                 help='Skip reconstruction if area (PE) is less than this',
+                 default=10),
+    strax.Option('n_top_pmts', default=straxen.n_top_pmts,
+                 help="Number of top PMTs"),
+    strax.Option("s2_cnn_model_path", 
+                 help="Path to the CNN model in hdf5 format. WARING, this should include the whole model file and not the weights file", 
+                 default="CNN_dw_3L_cm_maxnorm_A_lin_5_2000__Z_const_-1.hdf5"
+                )
+)
+class S2CNNEventPositionFromEventAreas(strax.Plugin):
+    """
+    This pluging provides S2 CNN position reconstruction events 
+
+    returns variables : 
+        - s2_x_TFS2CNN - reconstructed X position of main S2 [ cm ]
+        - s2_y_TFS2CNN - reconstructed Y position of main S2 [ cm ]
+        - alt_s2_x_TFS2CNN - reconstructed X position of alternative S2 [ cm ]
+        - alt_s2_y_TFS2CNN - reconstructed Y position of alternative S2 [ cm ]
+    """
+
+    depends_on = ('EventAreaPerChannel')
+    parallel = False
+    provides = "S2CNNEventPositions"
+    __version__ = '0.0.0'
+    def infer_dtype(self):
+        dtype = [('s2_x_TFS2CNN', np.float32,
+                  'Reconstructed CNN S2 X position of main S2 [ cm ] '),
+                 ('s2_y_TFS2CNN', np.float32,
+                  'Reconstructed CNN S2 Y position of main S2 [ cm ] '),
+                 ('alt_s2_x_TFS2CNN', np.float32,
+                  'Reconstructed CNN S2 X position of alternative S2 [ cm ] '),
+                 ('alt_s2_y_TFS2CNN', np.float32,
+                  'Reconstructed CNN S2 Y position of alternative S2 [ cm ] '),
+                 ]
+        dtype += strax.time_fields
+        return dtype
+    
+    def setup(self):
+        import tensorflow as tf
+        keras = tf.keras
+        self.s2_cnn_model_path = str(self.config['s2_cnn_model_path'])
+        print("S2 CNN reco : trying to load model from : \n\t %s"%self.s2_cnn_model_path)
+        if not os.path.exists(self.s2_cnn_model_path):
+            print("Local file does not exist, tryin downloading the file." )
+            downloader = straxen.MongoDownloader()
+            if self.s2_cnn_model_path in downloader.list_files():
+                self.s2_cnn_model_path = downloader.download_single(self.s2_cnn_model_path)
+                print("Path to downloaded file from database : \n\t : %s"%self.s2_cnn_model_path)
+            else: 
+                raise RuntimeError("Model file not found (locally or in DB): %s"%self.s2_cnn_model_path) 
+        self.cnn_model = keras.models.load_model(self.s2_cnn_model_path)
+        self.converter = DoubleWidthConverter()
+        print("====== Loaded TF CNN model =====")
+        self.cnn_model.summary()
+        print("====== end of model summary =====")
+
+
+    def compute(self, events):
+        result = np.empty(len(events), dtype=self.dtype)
+        result[:] = np.nan
+        result['time'],result['endtime']=events['time'], strax.endtime(events)
+        for type_ in ['s2', 'alt_s2']:
+            peak_mask = (events[type_+'_area'] > self.config['min_reconstruction_area'])
+            areas = events[type_+"_area_per_channel"][peak_mask,0:self.config['n_top_pmts']]
+            if not np.sum(peak_mask):continue          
+            patterns = self.converter.convert_multiple_patterns(areas)
+            patterns = patterns/patterns.max(axis=(1,2))[:,None,None]
+            reco_pos = self.cnn_model.predict(patterns)
+            # I assume that all the networks return cm
+            result[type_+'_x_TFS2CNN'][peak_mask] = reco_pos[:, 0]
+            result[type_+'_y_TFS2CNN'][peak_mask] = reco_pos[:, 1]
         return result
