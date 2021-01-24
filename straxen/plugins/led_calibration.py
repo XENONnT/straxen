372f2083aacce3c06685b8b1ba9be1d4e1f1c0d1
Joran Angevaare <jorana@nikhef.nl>
2021-01-24 12:18:23 +0100

    reduce ammount of files

--- straxen/plugins/led_calibration.py
+++ straxen/plugins/led_calibration.py
@@ -47,7 +47,7 @@ class LEDCalibration(strax.Plugin):
     data_kind = 'led_cal' 
     compressor = 'zstd'
     parallel = 'process'
-    rechunk_on_save = False
+    rechunk_on_save = True
     
     dtype = [('area', np.float32, 'Area averaged in integration windows'),
              ('amplitude_led', np.float32, 'Amplitude in LED window'),
