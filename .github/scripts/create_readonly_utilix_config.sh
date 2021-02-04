48820c1069f0604434318cc5fae12a80cb681308
Joran Angevaare <jorana@nikhef.nl>
2021-02-04 23:08:30 +0100

    add mongo

--- .github/scripts/create_readonly_utilix_config.sh
+++ .github/scripts/create_readonly_utilix_config.sh
@@ -2,15 +2,11 @@
 
 cat > $HOME/.xenon_config <<EOF
 [RunDB]
-rundb_api_url = $RUNDB_API_URL
+rundb_api_url = $RUNDB_API_URL_READONLY
 rundb_api_user = $RUNDB_API_USER_READONLY
 rundb_api_password = $RUNDB_API_PASSWORD_READONLY
-pymongo_url = $PYMONGO_URL
-pymongo_user = $PYMONGO_USER_READONLY
-pymongo_password = $PYMONGO_PASSWORD_READONLY
-pymongo_database = $PYMONGO_DATABASE
 xent_url = $PYMONGO_URL
-xent_user = $PYMONGO_USER_READONLY
-xent_password = $PYMONGO_PASSWORD_READONLY
+xent_user = $PYMONGO_USER
+xent_password = $PYMONGO_PASSWORD
 xent_database = $PYMONGO_DATABASE
 EOF
