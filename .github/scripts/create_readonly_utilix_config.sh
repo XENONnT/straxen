0d9c2305b475096b42a7f8df47d23e982ffa3435
Joran Angevaare <jorana@nikhef.nl>
2021-02-17 19:05:10 +0100

    url is readonly

--- .github/scripts/create_readonly_utilix_config.sh
+++ .github/scripts/create_readonly_utilix_config.sh
@@ -5,7 +5,7 @@ cat > $HOME/.xenon_config <<EOF
 logging_level=debug
 
 [RunDB]
-rundb_api_url = $RUNDB_API_URL_READONLY
+rundb_api_url = $RUNDB_API_URL
 rundb_api_user = $RUNDB_API_USER_READONLY
 rundb_api_password = $RUNDB_API_PASSWORD_READONLY
 xent_url = $PYMONGO_URL
