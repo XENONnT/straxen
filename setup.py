1b043a9075fdf422fb7ad3adef2dfe9988b73d46
Joran Angevaare <jorana@nikhef.nl>
2021-02-22 23:00:37 +0100

    Bump version: 0.15.4 → 0.15.5

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open('HISTORY.md') as file:
     history = file.read()
 
 setuptools.setup(name='straxen',
-                 version='0.15.4',
+                 version='0.15.5',
                  description='Streaming analysis for XENON',
                  author='Straxen contributors, the XENON collaboration',
                  url='https://github.com/XENONnT/straxen',
