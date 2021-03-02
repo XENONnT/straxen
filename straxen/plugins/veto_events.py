bcbe357f5507540d360529b8e8535519a78eb9f5
Daniel Wenz <dwenz@students.uni-mainz.de>
2021-03-02 08:20:58 -0600

    Some tweeks and documentation

--- straxen/plugins/veto_events.py
+++ straxen/plugins/veto_events.py
@@ -8,19 +8,15 @@ from immutabledict import immutabledict
 
 @strax.takes_config(
     strax.Option('event_left_extension_nv', default=0,
-                 help="Include this many ns left of hits in peaks"),
-    strax.Option('event_right_extension_nv', default=300,
-                 help="Include this many ns right of hits in peaks"),
-    strax.Option('event_min_pmts_nv', default=0,
-                 help="Minimum number of contributing PMTs needed to define a peak"),
+                 help="Extends events this many ns to the left"),
+    strax.Option('event_resolving_time_nv', default=300,
+                 help="Resolving time for fixed window concidene [ns]."),
     strax.Option('event_min_hits_nv', default=6,
-                 help="Minimum number of contributing PMTs needed to define a peak"),
+                 help="Minimum number of fully convinet hitlets to define an event."),
     strax.Option('gain_model_nv',
                  help='PMT gain model. Specify as (model_type, model_config)'),
     strax.Option('n_nveto_pmts', type=int,
                  help='Number of TPC PMTs'),
-    strax.Option('sampling_rate_nv', type=int, default=2,
-                 help='Length of a sample in ns.'),
     strax.Option('channel_map', track=False, type=immutabledict,
                  help="immutabledict mapping subdetector to (min, max) "
                       "channel number."),
@@ -49,12 +45,12 @@ class nVETOEvents(strax.OverlapWindowPlugin):
         return veto_event_dtype()
 
     def get_window_size(self):
-        return self.config['event_left_extension_nv'] + self.config['event_right_extension_nv'] + 1
+        return self.config['event_left_extension_nv'] + self.config['event_resolving_time_nv'] + 1
 
     def compute(self, hitlets_nv, start, end):
         intervals = straxen.plugins.nveto_recorder.coincidence(hitlets_nv,
                                                                self.config['event_min_hits_nv'],
-                                                               self.config['event_right_extension_nv'],
+                                                               self.config['event_resolving_time_nv'],
                                                                self.config['event_left_extension_nv']
                                                                )
 
@@ -67,12 +63,15 @@ class nVETOEvents(strax.OverlapWindowPlugin):
         events['time'] = np.clip(intervals[:, 0], start, end)
         events['endtime'] = np.clip(intervals[:, 1], start, end)
 
-        events['event_number_nv'] = np.arange(n_events) + self.event_seen
-
         # Compute center time:
         split_hitlets = strax.split_by_containment(hitlets_nv, events)
         compute_event_properties(events, split_hitlets, start_channel=2000)
 
+        # Cut all those events for which we have less than self.config['event_min_hits_nv'] 
+        # hitlets. (straxen.plugins.nveto_recorder.coincidence works with partially overlaping things)
+        events = events[events['n_hits'] >= self.config['event_min_hits_nv']]
+        n_events = len(events)
+        events['event_number_nv'] = np.arange(n_events) + self.event_seen
         self.event_seen = n_events
 
         return events
@@ -82,6 +81,7 @@ def veto_event_dtype(name_event_number='event_number_nv', n_pmts=120):
     dtype = []
     dtype += strax.time_fields  # because mutable
     dtype += [(('Veto event number in this dataset', name_event_number), np.int64),
+              (('Last hitlet endtime in event [ns].', 'last_hitlet_endtime'), np.int64),
               (('Total area of all hitlets in evnet [pe]', 'area'), np.float32),
               (('Total number of hitlets in events', 'n_hits'), np.int32),
               (('Area in event per channel [pe]', 'area_per_channel'), np.float32, n_pmts),
@@ -113,7 +113,11 @@ def compute_event_properties(events, contained_hitlets, start_channel=2000):
             e['center_time'] = np.sum(t * hitlets['area']) / event_area
         else:
             e['center_time'] = np.nan
-
+            
+        # Compute endtime of last hitlet in event:
+        endtime = strax.endtime(hitlets)
+        e['last_hitlet_endtime'] = max(endtime)
+        
         # Compute per channel properties:
         for h in hitlets:
             ch = h['channel'] - start_channel
