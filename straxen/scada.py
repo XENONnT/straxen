0d0f570d2f47df26cd9d2a6336588842ca53bccd
Daniel Wenz <dwenz@kabelmail.de>
2021-03-04 09:22:15 +0100

    Added changes for user credentials

--- straxen/scada.py
+++ straxen/scada.py
@@ -1,13 +1,23 @@
 import urllib
 import requests
+import json
+
 import pandas as pd
 import numba
 import numpy as np
-import warnings
+
 import strax
 import straxen
 
+from datetime import datetime
+from datetime import timedelta
+import time
+import pytz
+
+import getpass
+import warnings
 import sys
+
 if any('jupyter' in arg for arg in sys.argv):
     # In some cases we are not using any notebooks,
     # Taken from 44952863 on stack overflow thanks!
@@ -15,7 +25,6 @@ if any('jupyter' in arg for arg in sys.argv):
 else:
     from tqdm import tqdm
 
-from straxen import uconfig
 export, __all__ = strax.exporter()
 
 
@@ -30,13 +39,13 @@ class SCADAInterface:
             if you would like to query data via run_ids.
         :param use_progress_bar: Use a progress bar in the Scada interface
         """
+        self.we_are_straxen = False
+        self._token_expire_time = None
+        self._token = None
         try:
-            self.SCData_URL = uconfig.get('scada', 'scdata_url')
-            self.SCLastValue_URL = uconfig.get('scada', 'sclastvalue_url')
-            self.SCADA_SECRETS = dict(QueryType=uconfig.get('scada', 'querytype'),
-                                      username=uconfig.get('scada', 'username'),
-                                      api_key=uconfig.get('scada', 'api_key')
-                                      )
+            self.SCLogin_url = straxen.uconfig.get('scada', 'sclogin_url')
+            self.SCData_URL = straxen.uconfig.get('scada', 'scdata_url')
+            self.SCLastValue_URL = straxen.uconfig.get('scada', 'sclastvalue_url')
 
             # Load parameters from the database.
             self.pmt_file = straxen.get_resource('PMTmap_SCADA.json',
@@ -126,7 +135,7 @@ class SCADAInterface:
             else:
                 start, end = self.context.to_absolute_time_range(run_id, **time_selection_kwargs)
         elif run_id:
-            mes = ('You are trying to query slow control data via run_ids' 
+            mes = ('You are trying to query slow control data via run_ids'
                    ' but you have not specified the context you are '
                    'working with. Please set the context either via '
                    '.st = YOURCONTEXT, or when initializing the '
@@ -144,8 +153,8 @@ class SCADAInterface:
         _fill_gaps = [None, 'None', 'interpolation', 'forwardfill']
 
         if fill_gaps not in _fill_gaps:
-            raise ValueError(f'Wrong argument for "fill_gaps", must be either {_fill_gaps}.' 
-               f' You specified "{fill_gaps}"')
+            raise ValueError(f'Wrong argument for "fill_gaps", must be either {_fill_gaps}.'
+                             f' You specified "{fill_gaps}"')
 
         now = np.datetime64('now')
         if (end // 10**9) > now.astype(np.int64):
@@ -156,6 +165,17 @@ class SCADAInterface:
                    f'time is {now.astype(np.int64)}. I will return for the values for the '
                    'corresponding times as nans instead.')
             warnings.warn(mes)
+
+        if not self._token:
+            # User has not asked for a token yet:
+            self._get_token()
+
+        # Check if token will expire soon, if so renew the token before we query
+        # the parameters:
+        hrs, mins = self._token_expires_in()
+        if hrs == 0 and mins < 30:
+            print('Your token will expire in less than 30 min please get first a new one:')
+            self._get_token()
 
         # Now loop over specified parameters and get the values for those.
         iterator = enumerate(parameters.items())
@@ -247,8 +267,7 @@ class SCADAInterface:
         df.set_index('time', inplace=True)
 
         # Init parameter query:
-        query = self.SCADA_SECRETS.copy()  # Have to copy dict since it is not immutable
-        query['name'] = parameter_name
+        query = {'name': parameter_name}
 
         # Check if first value is in requested range:
         temp_df = self._query(query,
@@ -265,16 +284,16 @@ class SCADAInterface:
         while ntries < max_tries:
             temp_df = self._query(query,
                                   self.SCData_URL,
-                                  start=(start//10**9)+1+offset,
-                                  end=(end//10**9)+1,
+                                  start=(start // 10**9) + 1 + offset,
+                                  end=(end // 10**9) + 1,
                                   query_type_lab=query_type_lab,
                                   seconds_interval=every_nth_value,
-                                  raise_error_message=False  # No valid value in query range... 
+                                  raise_error_message=False  # No valid value in query range...
                                   )  # +1 since it is end before exclusive
             if temp_df.empty:
                 # In case WebInterface does not return any data, e.g. if query range too small
                 break
-            times = (temp_df['timestampseconds'].values*10**9).astype('<M8[ns]')
+            times = (temp_df['timestampseconds'].values * 10**9).astype('<M8[ns]')
             df.loc[times, parameter_key] = temp_df.loc[:, 'value'].values
 
             endtime = temp_df['timestampseconds'].values[-1].astype(np.int64)
@@ -309,14 +328,14 @@ class SCADAInterface:
 
         return df
 
-    @staticmethod
-    def _query(query, 
-               api, 
-               start=None, 
-               end=None, 
-               query_type_lab=False, 
+    def _query(self,
+               query,
+               api,
+               start=None,
+               end=None,
+               query_type_lab=False,
                seconds_interval=None,
-              raise_error_message=True):
+               raise_error_message=True):
         """
         Helper to reduce code. Asks for data and returns result. Raises error
         if api returns error.
@@ -333,18 +352,43 @@ class SCADAInterface:
             query['QueryType'] = 'rawbytime'
             query.pop('interval', None)  # Interval only works with lab
 
+        # Configure query url
         query_url = urllib.parse.urlencode(query)
-        values = requests.get(api + query_url)
-        values = values.json()
-            
+        self._query_url = api + query_url
+        req = urllib.request.Request(self._query_url)
+        req.add_header('Authorization', self._token)
+
+        try:
+            # Try to get result:
+            values = urllib.request.urlopen(req)
+        except urllib.error.HTTPError as e:
+            if e.code != 401:
+                print(f'HTTPError {e}')
+            else:
+                # Invalid token so we have to get a new one,
+                # this should actually never happen, but you never know...
+                print('Your token is invalid. It may have expired please get a new one:')
+                # If the user puts in the wrong credentials the query will fail.
+                self._get_token()
+                req = urllib.request.Request(self._query_url)
+                req.add_header('Authorization', self._token)
+                req.add_header('Authorization', self._token)
+                values = urllib.request.urlopen(req)
+
+        # Read database response and check if query was valid:
+        values = values.read()
+        values = json.loads(values.decode('utf8'))
+
         temp_df = pd.DataFrame(columns=('timestampseconds', 'value'))
         if isinstance(values, dict) and raise_error_message:
+            # Not valid, why:
             query_status = values['status']
             query_message = values['message']
             raise ValueError(f'SCADAapi has not returned values for the '
                              f'parameter "{query["name"]}". It returned the '
                              f'status "{query_status}" with the message "{query_message}".')
         if isinstance(values, list):
+            # Valid so return dataframe
             temp_df = pd.DataFrame(values)
         return temp_df
 
@@ -375,7 +419,7 @@ class SCADAInterface:
         if isinstance(pmts, np.ndarray):
             # convert to a simple list since otherwise we get ambiguous errors
             pmts = list(pmts)
-            
+
         if not hasattr(pmts, '__iter__'):
             # If single PMT convert it to itterable
             pmts = [pmts]
@@ -391,11 +435,80 @@ class SCADAInterface:
         # Now get all relevant names:
         for key, value in pmts_v.items():
             if hv:
-                res[key+'_HV'] = value
+                res[key + '_HV'] = value
             if current:
-                res[key+'_I'] = value[:-4] + 'IMON'
+                res[key + '_I'] = value[:-4] + 'IMON'
 
         return res
+
+    def get_new_token(self):
+        """
+        Function to renew the token of the current session. Asks user for
+        Xe1TViewer/SCADA credentials.
+        """
+        self._get_token()
+
+    def _get_token(self):
+        """
+        Function which asks for user credentials to receive a personalized
+        security token. The token is required to query any data from the
+        slow control historians.
+        """
+        print('Please, enter your Xe1TViewer/SCADA credentials:')
+        time.sleep(1)
+        if not self.we_are_straxen:
+            username = getpass.getpass('Username: ')
+            password = getpass.getpass('Password: ')
+        else:
+            username = straxen.uconfig.get('scada', 'straxen_username')
+            password = straxen.uconfig.get('scada', 'straxen_password')
+
+        login_query = {'username': username,
+                       'password': password,
+                       }
+        res = requests.post(self.SCLogin_url,
+                            data=login_query)
+
+        res = res.json()
+        if 'token' not in res.keys():
+            if res['Message'] == 'Invalid username or password.':
+                raise ValueError('Cannot get security token from Slow Control web API. '
+                                 f'API returned the following reason: {res["Message"]} '
+                                 'Please use your Xe1TViewer/SCADA credentials.')
+            else:
+                raise ValueError('Cannot get security token from Slow Control web API. '
+                                 f'API returned the following reason: {res["Message"]}')
+        else:
+            self._token = res['token']
+            toke_start_time = datetime.now(tz=pytz.timezone('utc'))
+            hours_added = timedelta(hours=3)
+            self._token_expire_time = toke_start_time + hours_added
+            print('Received token, the token is valid for 3 hrs.\n',
+                  f'from {toke_start_time.strftime("%d.%m. %H:%M:%S")} UTC\n',
+                  f'till {self._token_expire_time.strftime("%d.%m. %H:%M:%S")} UTC'
+                  )
+
+    def token_expires_in(self):
+        """
+        Function which displays how long until the current token expires.
+        """
+        if self._token_expire_time:
+            print(f'The current token expires at {self._token_expire_time.strftime("%d.%m. %H:%M:%S")} UTC')
+            hrs, mins = self._token_expires_in()
+            print(f'Which is in {hrs} h and {mins} min.')
+        else:
+            raise ValueError('You do not have any valid token yet. Please call '
+                             '"get_new_token" first".')
+
+    def _token_expires_in(self):
+        """
+        Computes hrs and minutes until token expires.
+        """
+        now = datetime.now(tz=pytz.timezone('utc'))
+        dt = (self._token_expire_time - now).seconds  # time delta in seconds
+        hrs = dt // 3600
+        mins = dt % 3600 // 60
+        return hrs, mins
 
 
 @export
@@ -444,12 +557,12 @@ def _average_scada(times, values, nvalues):
     :return: new time values and
     """
     if len(times) % nvalues:
-        nsamples = (len(times) // nvalues) - 1
+        n_samples = (len(times) // nvalues) - 1
     else:
-        nsamples = (len(times) // nvalues)
-    res = np.zeros(nsamples, dtype=np.float32)
-    new_times = np.zeros(nsamples, dtype=np.int64)
-    for ind in range(nsamples):
+        n_samples = (len(times) // nvalues)
+    res = np.zeros(n_samples, dtype=np.float32)
+    new_times = np.zeros(n_samples, dtype=np.int64)
+    for ind in range(n_samples):
         res[ind] = np.mean(values[ind * nvalues:(ind + 1) * nvalues])
         new_times[ind] = np.mean(times[ind * nvalues:(ind + 1) * nvalues])
 
