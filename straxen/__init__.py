f42aaddd1462bbd7b587f7f9e5979b515a8dd7c2
Joran Angevaare <jorana@nikhef.nl>
2021-03-15 16:46:02 +0100

    fix init file

--- straxen/__init__.py
+++ straxen/__init__.py
@@ -2,6 +2,7 @@ __version__ = '0.15.8'
 
 # Import of utilix config for only once. NB: Should be first due to circular imports
 from utilix import uconfig
+
 # Straxen imports:
 from .common import *
 from .corrections_services import *
@@ -16,10 +17,12 @@ from .online_monitor import *
 from .rundb import *
 from .scada import *
 from .bokeh_utils import *
+
 # Nested structures:
 from . import plugins
 from .plugins import *
 from . import analyses
+
 # Do not make all contexts directly available under straxen.
 # Otherwise we have straxen.demo() etc.
 from . import contexts
