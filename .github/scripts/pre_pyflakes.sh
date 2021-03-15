cdeea191b4cc43f8e8de115f6b1c74255ea89c5b
Joran Angevaare <jorana@nikhef.nl>
2021-03-15 16:18:32 +0100

    fix bash

--- .github/scripts/pre_pyflakes.sh
+++ .github/scripts/pre_pyflakes.sh
@@ -2,17 +2,17 @@
 # Pyflakes does not like the way we do __all__ += []. This simple script
 # Changes all the files in straxen to abide by this convention and
 # removes the lines that have such a signature.
-start=`pwd`
+start="$(pwd)"
 echo $start
 
 cd straxen
-sed -e '/__all__ +=/ s/^#*/#/' -i *.py
+sed -e '/__all__ +=/ s/^#*/#/' -i ./*.py
 
 cd plugins
-sed -e '/__all__ +=/ s/^#*/#/' -i *.py
+sed -e '/__all__ +=/ s/^#*/#/' -i ./*.py
 
 cd ../analyses
-sed -e '/__all__ +=/ s/^#*/#/' -i *.py
+sed -e '/__all__ +=/ s/^#*/#/' -i ./*.py
 
 cd $start
 echo "done"
