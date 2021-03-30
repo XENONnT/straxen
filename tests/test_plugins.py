864feb71ab5fe013d82be5339044bfed81dc514f
Joran Angevaare <jorana@nikhef.nl>
2021-03-30 22:07:06 +0200

    restore #404, add get_correction_from_cmt to corrections_services.py (#409)
    * restore #404
    
    * fix pep8
    
    * fix failing tests
    
    * really fix the tests
    
    * fix trailing whitespace

--- tests/test_plugins.py
+++ tests/test_plugins.py
@@ -22,14 +22,16 @@ testing_config_nT = dict(
     ('to_pe_per_run',
      straxen.aux_repo + '58e615f99a4a6b15e97b12951c510de91ce06045/fax_files/to_pe_nt.npy'),
     s2_xy_correction_map=straxen.pax_file('XENON1T_s2_xy_ly_SR0_24Feb2017.json'),
-    elife_file=straxen.aux_repo + '3548132b55f81a43654dba5141366041e1daaf01/strax_files/elife.npy',
+    elife_conf=straxen.aux_repo + '3548132b55f81a43654dba5141366041e1daaf01/strax_files/elife.npy',
+    baseline_samples_nv=10,
     fdc_map=straxen.pax_file('XENON1T_FDC_SR0_data_driven_3d_correction_tf_nn_v0.json.gz'),
     gain_model_nv=("to_pe_constant", "adc_nv"),
 )
 
 testing_config_1T = dict(
     hev_gain_model=('to_pe_constant', 0.0085),
-    gain_model=('to_pe_constant', 0.0085)
+    gain_model=('to_pe_constant', 0.0085),
+    elife_conf=straxen.aux_repo + '3548132b55f81a43654dba5141366041e1daaf01/strax_files/elife.npy',
 )
 
 test_run_id_nT = '008900'
