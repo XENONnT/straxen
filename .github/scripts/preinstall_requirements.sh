c32e7f032ee5dcee5eefee229ce9dda8dbe8d6b6
Joran Angevaare <jorana@nikhef.nl>
2021-04-05 16:58:47 +0200

    Test dask==2021.4.0

--- .github/scripts/preinstall_requirements.sh
+++ .github/scripts/preinstall_requirements.sh
@@ -3,9 +3,10 @@
 echo "download requirements from base_environment"
 wget -O pre_requirements.txt https://raw.githubusercontent.com/XENONnT/base_environment/master/requirements.txt &> /dev/null
 echo "select important dependencies for strax(en)"
-grep 'numpy\|numba\|tensorflow\|blosc\|scikit-learn\|dask' pre_requirements.txt &> sel_pre_requirements.txt
+grep 'numpy\|numba\|tensorflow\|blosc\|scikit-learn' pre_requirements.txt &> sel_pre_requirements.txt
 echo "Will pre-install:"
 cat sel_pre_requirements.txt
 echo "Start preinstall and rm pre-requirements:"
 pip install -r sel_pre_requirements.txt
 rm pre_requirements.txt sel_pre_requirements.txt
+pip install dask=2021.4.0
