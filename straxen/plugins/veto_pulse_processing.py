2d7a76ba3185663e6c9397746cc7fad973fbe74f
Joran Angevaare <jorana@nikhef.nl>
2021-04-08 17:34:40 +0200

    Update veto_pulse_processing.py

--- straxen/plugins/veto_pulse_processing.py
+++ straxen/plugins/veto_pulse_processing.py
@@ -234,12 +234,11 @@ def clean_up_empty_records(records, record_links, only_last=True):
         child_option=True, parent_option_name='hit_min_amplitude_nv',
         help='Minimum hit amplitude in ADC counts above baseline. '
              'Specify as a tuple of length n_nveto_pmts, or a number.'),
-# TODO -> Reactivate
-#     strax.Option(
-#         'check_raw_record_overlaps',
-#         default=True, track=False,
-#         help='Crash if any of the pulses in raw_records overlap with others '
-#              'in the same channel'),
+    strax.Option(
+        'check_raw_record_overlaps',
+        default=True, track=False,
+        help='Crash if any of the pulses in raw_records overlap with others '
+             'in the same channel'),
 )
 class muVETOPulseProcessing(nVETOPulseProcessing):
     __doc__ = MV_PREAMBLE + nVETOPulseProcessing.__doc__
@@ -259,7 +258,7 @@ class muVETOPulseProcessing(nVETOPulseProcessing):
         return dtype
 
     def compute(self, raw_records_mv):
-# TODO -> Reactivate        
+#         TODO -> Reactivate        
 #         if self.config['check_raw_record_overlaps']:
 #             straxen.check_overlaps(raw_records_mv, n_channels=3000)
         return super().compute(raw_records_mv)
