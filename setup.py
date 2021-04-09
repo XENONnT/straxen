1f553a34d4a0eb256719ddc8c26549894640504f
Daniel Wenz <dwenz@students.uni-mainz.de>
2021-04-09 08:12:25 -0500

    Bump version: 0.16.2 → 0.16.3

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open('HISTORY.md') as file:
     history = file.read()
 
 setuptools.setup(name='straxen',
-                 version='0.16.2',
+                 version='0.16.3',
                  description='Streaming analysis for XENON',
                  author='Straxen contributors, the XENON collaboration',
                  url='https://github.com/XENONnT/straxen',
