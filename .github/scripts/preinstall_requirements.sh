1c7b28f71945c1d523e3f6b1932c884a4b5acef9
Joran Angevaare <jorana@nikhef.nl>
2021-04-28 09:45:21 +0200

    Tensorflow 2.5 is needed for py39

--- .github/scripts/preinstall_requirements.sh
+++ .github/scripts/preinstall_requirements.sh
@@ -3,7 +3,7 @@
 echo "download requirements from base_environment"
 wget -O pre_requirements.txt https://raw.githubusercontent.com/XENONnT/base_environment/master/requirements.txt &> /dev/null
 echo "select important dependencies for strax(en)"
-grep 'numpy\|numba\|tensorflow\|blosc\|scikit-learn\|dask' pre_requirements.txt &> sel_pre_requirements.txt
+grep 'numpy\|numba\|blosc\|scikit-learn\|dask' pre_requirements.txt &> sel_pre_requirements.txt
 echo "Will pre-install:"
 cat sel_pre_requirements.txt
 echo "Start preinstall and rm pre-requirements:"
