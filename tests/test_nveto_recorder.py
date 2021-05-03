9d7e74643bee65bbd574a8c7ccb7e9124e41490a
Daniel Wenz <dwenz@kabelmail.de>
2021-05-03 11:32:31 +0200

    Added tests for coincidence

--- tests/test_nveto_recorder.py
+++ tests/test_nveto_recorder.py
@@ -2,6 +2,7 @@ import strax
 import straxen
 
 import numpy as np
+import unittest
 
 
 class TestMergeIntervals:
@@ -35,12 +36,40 @@ class TestMergeIntervals:
         assert intervals[1]['endtime'] == 22, 'Second interval has the wrong endtime!'
 
 
-class TestCoincidence:
+class TestCoincidence(unittest.TestCase):
+
+    def setUp(self):
+        intervals = np.zeros(4, dtype=strax.time_fields)
+
+        intervals[0]['time'] = 3
+        intervals[0]['endtime'] = 5
+        intervals[1]['time'] = 6
+        intervals[1]['endtime'] = 8
+        intervals[2]['time'] = 9
+        intervals[2]['endtime'] = 10
+
+        intervals[3]['time'] = 38
+        intervals[3]['endtime'] = 42
+        self.intervals = intervals
 
     def test_empty_inputs(self):
-        raw_records = np.zeros(0, dtype=strax.time_fields)
-        intervals = straxen.coincidence(raw_records)
+        intervals = np.zeros(0, dtype=strax.time_fields)
+        intervals = straxen.coincidence(intervals)
         assert len(intervals) == 0, 'Empty input should return empty intervals!'
 
+    def test_without_coincidence(self):
+        coincidence = straxen.coincidence(self.intervals, nfold=1, resolving_time=10, pre_trigger=0)
+        assert len(coincidence) == 2, 'Have not found the correct number of coincidences'
+        assert np.all(coincidence['time'] == (3, 38)), 'Coincidence does not have the correct time'
+        assert np.all(coincidence['endtime'] == (19, 48)), 'Coincidence doe snot have the correct time'
+
     def test_coincidence(self):
-        raw_records = np.zeros(0, dtype=strax.time_fields)
+        coincidence = straxen.coincidence(self.intervals, nfold=3, resolving_time=10, pre_trigger=0)
+        assert len(coincidence) == 1, 'Have not found the correct number of coincidences'
+        assert coincidence['time'] == 3, 'Coincidence does not have the correct time'
+        assert coincidence['endtime'] == 13, 'Coincidence doe snot have the correct time'
+
+        coincidence = straxen.coincidence(self.intervals, nfold=3, resolving_time=10, pre_trigger=2)
+        assert len(coincidence) == 1, 'Have not found the correct number of coincidences'
+        assert coincidence['time'] == 1, 'Coincidence does not have the correct time'
+        assert coincidence['endtime'] == 13, 'Coincidence doe snot have the correct time'
