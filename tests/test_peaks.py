147ed0b465b61fd52848b5e6a62a71717de241bd
Joran Angevaare <jorana@nikhef.nl>
2021-05-07 14:25:47 +0200

    add some comments to the test

--- tests/test_peaks.py
+++ tests/test_peaks.py
@@ -16,6 +16,9 @@ class TestComputePeakBasics(unittest.TestCase):
     def setUp(self, context=straxen.contexts.demo):
         self.st = context()
         self.n_top = self.st.config.get('n_top_pmts', 2)
+
+        # Make sure that the check is on. Otherwise we cannot test it.
+        self.st.config({'check_peak_sum_area': True})
         self.peaks_basics_compute = self.st.get_single_plugin(run_id, 'peak_basics').compute
 
     @settings(deadline=None)
@@ -24,7 +27,7 @@ class TestComputePeakBasics(unittest.TestCase):
            )
     def test_aft_equals1(self, test_peak_idx):
         """Fill top array with area 1"""
-        test_data = self.get_test_data(self.n_top)
+        test_data = self.get_test_peaks(self.n_top)
         test_data[test_peak_idx]['area_per_channel'][:self.n_top] = 1
         test_data[test_peak_idx]['area'] = np.sum(test_data[test_peak_idx]['area_per_channel'])
         peaks = self.peaks_basics_compute(test_data)
@@ -38,17 +41,24 @@ class TestComputePeakBasics(unittest.TestCase):
                                max_value=TEST_DATA_LENGTH - 1),
            )
     def test_bad_peak(self, off_by_factor, test_peak_idx):
+        """
+        Lets deliberately make some data that is not self-consistent to
+            run into the error in the test.
+        """
         if np.isclose(off_by_factor, 1) or np.isclose(off_by_factor, 0):
             return
-        test_data = self.get_test_data(self.n_top)
+        test_data = self.get_test_peaks(self.n_top)
         test_data[test_peak_idx]['area_per_channel'][:self.n_top] = 1
         area = np.sum(test_data[test_peak_idx]['area_per_channel'])
+
+        # Set the area field to a wrong value
         area *= off_by_factor
         test_data[test_peak_idx]['area'] = area
         self.assertRaises(ValueError, self.peaks_basics_compute, test_data)
 
     @staticmethod
-    def get_test_data(n_top, length=2, sum_wf_samples=10):
+    def get_test_peaks(n_top, length=2, sum_wf_samples=10):
+        """Generate some dummy peaks"""
         test_data = np.zeros(TEST_DATA_LENGTH,
                              dtype=strax.dtypes.peak_dtype(
                                  n_channels=n_top + 1,
