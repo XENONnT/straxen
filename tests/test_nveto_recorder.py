a6c94a02f9aa035d262335d7a72078024995d106
Daniel Wenz <dwenz@kabelmail.de>
2021-05-18 12:47:08 +0200

    Extended tests removed hardcoded tests

--- tests/test_nveto_recorder.py
+++ tests/test_nveto_recorder.py
@@ -5,7 +5,12 @@ import numpy as np
 import unittest
 
 
-class TestMergeIntervals:
+class TestMergeIntervals(unittest.TestCase):
+
+    def setUp(self):
+        self.intervals = np.zeros(4, dtype=strax.time_fields)
+        self.intervals['time'] = [2, 3, 7, 20]
+        self.intervals['endtime'] = [4, 7, 8, 22]
 
     def test_empty_intervals(self):
         intervals = np.zeros(0, dtype=strax.time_fields)
@@ -13,44 +18,27 @@ class TestMergeIntervals:
         assert len(intervals) == 0, 'Empty input should return empty intervals!'
 
     def test_merge_overlapping_intervals(self):
-        intervals = np.zeros(4, dtype=strax.time_fields)
-
-        # First interval:
-        intervals[0]['time'] = 2
-        intervals[0]['endtime'] = 4
-        intervals[1]['time'] = 3
-        intervals[1]['endtime'] = 7
-        intervals[2]['time'] = 7
-        intervals[2]['endtime'] = 8
-
-        # Second interval:
-        intervals[3]['time'] = 20
-        intervals[3]['endtime'] = 22
-
-        intervals = straxen.merge_intervals(intervals)
+        intervals = straxen.merge_intervals(self.intervals)
 
         assert len(intervals) == 2, 'Got the wrong number of intervals!'
-        assert intervals[0]['time'] == 2, 'First interval has the wrong time!'
-        assert intervals[0]['endtime'] == 8, 'First interval has the wrong endtime!'
-        assert intervals[1]['time'] == 20, 'Second interval has the wrong time!'
-        assert intervals[1]['endtime'] == 22, 'Second interval has the wrong endtime!'
+
+        time_is_correct = intervals[0]['time'] == self.intervals['time'][0]
+        assert time_is_correct, 'First interval has the wrong time!'
+        time_is_correct = intervals[0]['endtime'] == self.intervals['endtime'][-2]
+        assert time_is_correct, 'First interval has the wrong endtime!'
+
+        time_is_correct = intervals[-1]['time'] == self.intervals['time'][-1]
+        assert time_is_correct, 'Second interval has the wrong time!'
+        time_is_correct = intervals[-1]['endime'] == self.intervals['endime'][-1]
+        assert time_is_correct, 'Second interval has the wrong endtime!'
 
 
 class TestCoincidence(unittest.TestCase):
 
     def setUp(self):
-        intervals = np.zeros(4, dtype=strax.time_fields)
-
-        intervals[0]['time'] = 3
-        intervals[0]['endtime'] = 5
-        intervals[1]['time'] = 6
-        intervals[1]['endtime'] = 8
-        intervals[2]['time'] = 9
-        intervals[2]['endtime'] = 10
-
-        intervals[3]['time'] = 38
-        intervals[3]['endtime'] = 42
-        self.intervals = intervals
+        self.intervals = np.zeros(8, dtype=strax.time_fields)
+        self.intervals['time'] = [3, 6, 9, 12, 15, 18, 21, 38]
+        self.intervals['endtime'] = [5, 8, 10, 13, 16, 19, 23, 42]
 
     def test_empty_inputs(self):
         intervals = np.zeros(0, dtype=strax.time_fields)
@@ -58,18 +46,91 @@ class TestCoincidence(unittest.TestCase):
         assert len(intervals) == 0, 'Empty input should return empty intervals!'
 
     def test_without_coincidence(self):
-        coincidence = straxen.find_coincidence(self.intervals, nfold=1, resolving_time=10, pre_trigger=0)
-        assert len(coincidence) == 2, 'Have not found the correct number of coincidences'
-        assert np.all(coincidence['time'] == (3, 38)), 'Coincidence does not have the correct time'
-        assert np.all(coincidence['endtime'] == (19, 48)), 'Coincidence doe snot have the correct time'
+        resolving_time = 10
+        truth_time = np.array([self.intervals['time'][0],
+                               self.intervals['time'][-1]])
+        truth_endtime = np.array([self.intervals['time'][-2],
+                                  self.intervals['time'][-1]]) + resolving_time,
+        self._test_coincidence(resolving_time=resolving_time,
+                               coincidence=1,
+                               pre_trigger=0,
+                               n_concidences_truth=2,
+                               times_truth=truth_time,
+                               endtime_truth=truth_endtime
+                               )
+        pre_trigger = 2
+        self._test_coincidence(resolving_time=resolving_time,
+                               coincidence=1,
+                               pre_trigger=pre_trigger,
+                               n_concidences_truth=2,
+                               times_truth=truth_time - pre_trigger,
+                               endtime_truth=truth_endtime
+                               )
 
-    def test_coincidence(self):
-        coincidence = straxen.find_coincidence(self.intervals, nfold=3, resolving_time=10, pre_trigger=0)
-        assert len(coincidence) == 1, 'Have not found the correct number of coincidences'
-        assert coincidence['time'] == 3, 'Coincidence does not have the correct time'
-        assert coincidence['endtime'] == 13, 'Coincidence doe snot have the correct time'
+    def test_even_fold(self):
+        resolving_time = 10
+        self._test_coincidence(resolving_time=resolving_time,
+                               coincidence=2,
+                               pre_trigger=0,
+                               n_concidences_truth=1,
+                               times_truth=self.intervals['time'][0],
+                               endtime_truth=self.intervals['time'][-3] + resolving_time,
+                               )
+        pre_trigger = 2
+        self._test_coincidence(resolving_time=resolving_time,
+                               coincidence=2,
+                               pre_trigger=pre_trigger,
+                               n_concidences_truth=1,
+                               times_truth=self.intervals['time'][0] - pre_trigger,
+                               endtime_truth=self.intervals['time'][-3] + resolving_time,
+                               )
 
-        coincidence = straxen.find_coincidence(self.intervals, nfold=3, resolving_time=10, pre_trigger=2)
-        assert len(coincidence) == 1, 'Have not found the correct number of coincidences'
-        assert coincidence['time'] == 1, 'Coincidence does not have the correct time'
-        assert coincidence['endtime'] == 13, 'Coincidence doe snot have the correct time'
+        self._test_coincidence(resolving_time=resolving_time,
+                               coincidence=4,
+                               pre_trigger=pre_trigger,
+                               n_concidences_truth=1,
+                               times_truth=self.intervals['time'][0] - pre_trigger,
+                               endtime_truth=self.intervals['time'][-5] + resolving_time,
+                               )
+
+    def test_odd_fold(self):
+        resolving_time = 10
+        self._test_coincidence(resolving_time=resolving_time,
+                               coincidence=3,
+                               pre_trigger=0,
+                               n_concidences_truth=1,
+                               times_truth=self.intervals['time'][0],
+                               endtime_truth=self.intervals['time'][-4] + resolving_time,
+                               )
+        pre_trigger = 2
+        self._test_coincidence(resolving_time=resolving_time,
+                               coincidence=3,
+                               pre_trigger=pre_trigger,
+                               n_concidences_truth=1,
+                               times_truth=self.intervals['time'][0] - pre_trigger,
+                               endtime_truth=self.intervals['time'][-4] + resolving_time,
+                               )
+
+        self._test_coincidence(resolving_time=resolving_time,
+                               coincidence=5,
+                               pre_trigger=pre_trigger,
+                               n_concidences_truth=0,
+                               times_truth=self.intervals['time'][:0],
+                               endtime_truth=self.intervals['time'][:0],
+                               )
+
+    def _test_coincidence(self, resolving_time, coincidence, pre_trigger,
+                          n_concidences_truth, times_truth, endtime_truth):
+        coincidence = straxen.find_coincidence(self.intervals,
+                                               nfold=coincidence,
+                                               resolving_time=resolving_time,
+                                               pre_trigger=pre_trigger)
+        number_coincidence_correct = len(coincidence) == n_concidences_truth
+        assert number_coincidence_correct, 'Have not found the correct number of coincidences'
+
+        time_is_correct = np.all(coincidence['time'] == times_truth)
+        assert time_is_correct, 'Coincidence does not have the correct time'
+
+        endtime_is_correct = np.all(coincidence['endtime'] == endtime_truth)
+        print(coincidence['endtime'], endtime_truth)
+        assert endtime_is_correct, 'Coincidence does not have the correct endtime'
