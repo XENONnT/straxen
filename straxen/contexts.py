0465baade83bd598bcb9626ba867f16472cdb8c4
jorana <jorana@nikhef.nl>
2021-05-25 12:59:57 +0200

    allow variable event duration

--- straxen/contexts.py
+++ straxen/contexts.py
@@ -264,10 +264,11 @@ x1t_common_config = dict(
     # Smaller right extension since we applied the filter
     peak_right_extension=30,
     # Events*
-    left_event_extension=int(1e6),
-    right_event_extension=int(1e6),
+    left_event_extension=int(0.8e6),
+    right_event_extension=int(0.8e6),
     elife_conf=straxen.aux_repo + '3548132b55f81a43654dba5141366041e1daaf01/strax_files/elife.npy',
     electron_drift_velocity=("electron_drift_velocity_constant", 1.3325e-4, False),
+    max_drift_length=96.9,
 )
 
 
