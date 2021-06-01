9b10c420e598b03f555ac53c98b4b0402cd7dde9
jorana <jorana@nikhef.nl>
2021-06-01 15:34:06 +0200

    fix test for py36

--- setup.py
+++ setup.py
@@ -34,7 +34,6 @@ setuptools.setup(name='straxen',
                      'boltons',
                      'ipython',
                      'ipywidgets',
-                     'dask[complete]',
                  ],
                  python_requires=">=3.6",
                  extras_require={
