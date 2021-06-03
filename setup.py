e1454e60d47e0091469f4761301c6ef5121deea0
jorana <jorana@nikhef.nl>
2021-06-03 19:55:30 +0200

    Bump version: 0.18.7 → 0.18.8

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ with open('HISTORY.md') as file:
     history = file.read()
 
 setuptools.setup(name='straxen',
-                 version='0.18.7',
+                 version='0.18.8',
                  description='Streaming analysis for XENON',
                  author='Straxen contributors, the XENON collaboration',
                  url='https://github.com/XENONnT/straxen',
--- straxen/__init__.py
+++ straxen/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.18.7'
+__version__ = '0.18.8'
 
 # Import of utilix config for only once. NB: Should be first due to circular imports
 from utilix import uconfig
