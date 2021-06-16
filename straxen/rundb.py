45f2adf20da56ad1844eaf856b925f26a8822638
Daniel Wenz <dwenz@students.uni-mainz.de>
2021-06-16 01:09:57 -0500

    RunDB does not support superruns yet.

--- straxen/rundb.py
+++ straxen/rundb.py
@@ -148,6 +148,10 @@ class RunDB(strax.StorageFrontend):
 
     def _find(self, key: strax.DataKey,
               write, allow_incomplete, fuzzy_for, fuzzy_for_options):
+        if key.run_id.startswith('_'):
+            # Superruns are currently not supprorted..
+            raise strax.DataNotAvailable
+        
         if fuzzy_for or fuzzy_for_options:
             raise NotImplementedError("Can't do fuzzy with RunDB yet.")
 
@@ -296,6 +300,10 @@ class RunDB(strax.StorageFrontend):
             yield doc
 
     def run_metadata(self, run_id, projection=None):
+        if run_id.startswith('_'):
+            # Superruns are currently not supprorted..
+            raise strax.DataNotAvailable
+        
         if self.runid_field == 'name':
             run_id = str(run_id)
         else:
