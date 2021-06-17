e0b5878c4782e39a5a0a574048824c16e7c4f3b6
Joran Angevaare <joranangevaare@gmail.com>
2021-06-17 09:41:15 +0200

    update the version

--- straxen/plugins/peaklet_processing.py
+++ straxen/plugins/peaklet_processing.py
@@ -558,6 +558,7 @@ class MergedS2s(strax.OverlapWindowPlugin):
     depends_on = ('peaklets', 'peaklet_classification')
     data_kind = 'merged_s2s'
     provides = 'merged_s2s'
+    __version__ = '0.1.0'
 
     def infer_dtype(self):
         return strax.unpack_dtype(self.deps['peaklets'].dtype_for('peaklets'))
