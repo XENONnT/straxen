f2c547bb5f72d7a6aeddc9590f932cded0160a23
jorana <jorana@nikhef.nl>
2021-06-23 16:24:25 +0200

    if you hack, hack it well

--- straxen/contexts.py
+++ straxen/contexts.py
@@ -200,7 +200,7 @@ def xenonnt_online(output_folder='./strax_data',
                                                    )})
     if _context_config_overwrite is not None:
         st.set_context_config(_context_config_overwrite)
-    st._plugin_class_registry['event_info'].depends_on = [st._plugin_class_registry['event_info'].depends_on + ['event_patternfit']]
+    st._plugin_class_registry['event_info'].depends_on = list(st._plugin_class_registry['event_info'].depends_on) + ['event_patternfit']
     return st
 
 
