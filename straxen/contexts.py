2b610c56807924ba3df08b02abc484f8dc0743e7
jorana <jorana@nikhef.nl>
2021-06-23 19:48:08 +0200

    set reasonable defaults for 1T

--- straxen/contexts.py
+++ straxen/contexts.py
@@ -283,8 +283,7 @@ x1t_common_config = dict(
         ((2, 1), (4.5, 0.4))),
     peak_min_pmts=2,
     # MergedS2s
-    s2_merge_max_duration=15_000,
-    s2_merge_max_gap=3500,
+    s2_merge_gap_thresholds=((1.7, 5e3), (4.0, 500), (5.0, 0.)),
     # Peaks
     # Smaller right extension since we applied the filter
     peak_right_extension=30,
@@ -295,7 +294,6 @@ x1t_common_config = dict(
     right_event_extension=int(1e6),
     elife_conf=('elife_xenon1t', 'v1', False),
     electron_drift_velocity=("electron_drift_velocity_constant", 1.3325e-4),
-    event_info_function='disabled',
     max_drift_length=96.9,
     electron_drift_time_gate=("electron_drift_time_gate_constant", 1700),
 )
