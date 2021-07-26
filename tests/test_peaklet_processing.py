33aa3af5419f822a1c1072a5cf9cbf5acc208583
Daniel Wenz <dwenz@students.uni-mainz.de>
2021-07-26 12:18:07 -0500

    Fixed test

--- tests/test_peaklet_processing.py
+++ tests/test_peaklet_processing.py
@@ -7,19 +7,20 @@ import straxen
 
 
 @settings(deadline=None)
-@given(st.integers(min_value=0, max_value=100, size=20),)
+@given(st.lists(st.integers(min_value=0, max_value=10), min_size=8, max_size=8, unique=True),)
 def test_create_outside_peaks_region(time):
     time = np.sort(time)
-    time_intervals = np.zeros(len(time), strax.time_dt_fields)
+    time_intervals = np.zeros(len(time)//2, strax.time_dt_fields)
     time_intervals['time'] = time[::2]
     time_intervals['length'] = time[1::2] - time[::2]
     time_intervals['dt'] = 1
 
     st = straxen.contexts.demo()
     p = st.get_single_plugin('0', 'peaklets')
-    outside = p.create_outside_peaks_region(time_intervals)
+    outside = p.create_outside_peaks_region(time_intervals, 0, np.max(time))
 
     touching = strax.touching_windows(outside, time_intervals, window=0)
 
     for tw in touching:
+        print(tw)
         assert np.diff(tw) == 0, 'Intervals overlap although they should not!'
