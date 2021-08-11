095f17b256acdae96dc558ab0cbf42bd1df94df9
Daniel Wenz <dwenz@kabelmail.de>
2021-08-11 13:51:22 +0200

    Add a little test

---
+++ tests/test_led_calibration.py
@@ -0,0 +1,24 @@
+import strax
+from strax.testutils import several_fake_records
+import straxen
+import numpy as np
+from hypothesis import given
+
+
+@given(several_fake_records)
+def test_ext_timings_nv(records):
+    """
+    Little test for nVetoExtTimings.
+    """
+    records['pulse_length'] = records['length']
+
+    st = straxen.contexts.xenonnt_led()
+    plugin = st.get_single_plugin('1', 'ext_timings_nv')
+    hits = strax.find_hits(records)
+    result = plugin.compute(hits, records)
+
+    assert len(result) == len(hits)
+    assert np.all(result['time'] == hits['time'])
+    assert np.all(result['pulse_i'] == hits['record_i'])
+    true_dt = hits['time'] - records[hits['record_i']]['time']
+    assert np.all(result['delta_time'] == true_dt)
