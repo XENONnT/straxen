a2e60051c365b2a329c24fe1dd09fd1aa1327834
Kexin Liu <kexinliu@dali-login1.rcc.local>
2021-09-22 03:06:39 -0500

    rewrite with straxen split function

--- straxen/plugins/event_shadow_test.py
+++ straxen/plugins/event_shadow_test.py
@@ -9,31 +9,35 @@ class Shadow(strax.Plugin):
     # Name of the data type this plugin provides
     provides = ('event_shadow',) #a tuple, same level as 'event_ifo'etc.
 
-    depends_on = ('event_basics',)
+    depends_on = ('event_basics','peak_basics',)
 
     # Numpy datatype of the output
     dtype = [('shadow', np.float32,'previous s2 shadow at event level [PE/ns]')] + strax.time_fields
 
     # Version of the plugin. Increment this if you change the algorithm.
-    __version__ = '0.0.1'
+    __version__ = '0.0.2'
 
     def compute(self, events):
 
-        shadow = np.zeros(len(events))
-        pre_s2_x = np.zeros(len(events))
-        pre_s2_y = np.zeros(len(events))
-        distance_to_pre_s2 = np.zeros(len(events))
-        len_events = len(events)
-
-        for i in range(1,20):
-            new_shadow = events['s2_area'][0:len_events-i]/(events['s2_center_time'][i:]-events['s2_center_time'][0:len_events-i])
-            test = new_shadow > shadow[i:]
-            pre_s2_x[i:]=events['s2_x'][0:len_events-i]*test + pre_s2_x[i:]*(~test)
-            pre_s2_y[i:]=events['s2_y'][0:len_events-i]*test + pre_s2_y[i:]*(~test)
-            shadow[i:] = new_shadow*test + shadow[i:]*(~test)
-
-        #distance_to_pre_s2 = pre_s2_x**2 + pre_s2_y**2
+        roi_dt = np.dtype([(('back in time', 'time'), int),
+                                         (('till it begin','endtime'), int)])
+        roi = np.zeros(len(event), dtype=roi_dt)
         
+        n_seconds = int(5e9)
+        roi['time'] = event['time'] - n_seconds
+        roi['endtime'] = event['time']
+        split_try = strax.split_by_containment(peak, roi)
+
+        shadow = np.zeros(len(event))
+        for event_i, event_a in enumerate(event):
+            for peak_i, peak_a in enumerate(split_try[event_i]):
+                new_shadow = 0
+                if (peak_a['area']>1e3) & (peak_a['type']==2):
+                    new_shadow = peak_a['area']/(event_a['time']-peak_a['center_time'])
+                    
+                if new_shadow > shadow[event_i]:
+                    shadow[event_i] = new_shadow
+       
         return dict(time=events['time'],
             endtime=strax.endtime(events),
                     shadow = shadow)
