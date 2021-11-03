208e06ee7f3d697e9b42e9b4d1b9bb76765ed7ae
Dacheng Xu <xudctron@gmail.com>
2021-11-03 15:31:11 -0500

    Refactor event and peak shadow, add skip_drift_time
    Event shadow directly depend on peak shadow

--- straxen/plugins/peak_shadow.py
+++ straxen/plugins/peak_shadow.py
@@ -6,24 +6,28 @@ export, __all__ = strax.exporter()
 @export
 @strax.takes_config(
     strax.Option('pre_s2_area_threshold', default=1000,
-                 help='Only take S2s large than this into account when calculating PeakShadow.'),
+                 help='Only take S2s large than this into account when calculating PeakShadow [PE]'),
     strax.Option('time_window_backward', default=int(3e9),
-                 help='Search for S2s causing shadow in this time window'))
+                 help='Search for S2s causing shadow in this time window [ns]'),
+    strax.Option('skip_drift_time', default=int(3e6),
+                 help='Deduct max drift time to avoid peak interference in a single event [ns]'))
 class PeakShadow(strax.Plugin):
     """
     This plugin can find and calculate the previous S2 shadow at peak level,
     with time window backward and previous S2 area as options.
     It also gives the area and position infomation of these previous S2s.
     """
-    __version__ = '0.0.1'
+    __version__ = '0.0.2'
     depends_on = ('peak_basics', 'peak_positions')
     provides = 'peak_shadow'
     save_when = strax.SaveWhen.EXPLICIT
 
     def infer_dtype(self):
-        dtype = [('pre_s2_area', np.float32, 'previous s2 area [PE]'),
+        dtype = [('shadow', np.float32, 'previous s2 shadow [PE/ns]'),
+                 ('pre_s2_area', np.float32, 'previous s2 area [PE]'),
                  ('shadow_dt', np.int64, 'time diffrence to the previous s2 [ns]'),
-                 ('shadow', np.float32, 'previous s2 shadow [PE/ns]')]
+                 ('pre_s2_x', np.float32,'x of previous s2 peak causing shadow [cm]'),
+                 ('pre_s2_y', np.float32,'y of previous s2 peak causing shadow [cm]')]
         dtype += strax.time_fields
         return dtype
 
@@ -31,12 +35,15 @@ class PeakShadow(strax.Plugin):
         roi_dt = np.dtype([(('when s1 ends', 'time'), int), (('till the upper limit of drfit time', 'endtime'), int)])
         roi_shadow = np.zeros(len(peaks), dtype=roi_dt)
         n_seconds = self.config['time_window_backward']
+        n_drift_time = self.config['skip_drift_time']
         roi_shadow['time'] = peaks['time'] - n_seconds
-        roi_shadow['endtime'] = peaks['time']
+        roi_shadow['endtime'] = peaks['time'] - n_drift_time
 
         mask_pre_s2 = (peaks['area'] > self.config['pre_s2_area_threshold']) & (peaks['type']==2)
         split_peaks = strax.split_touching_windows(peaks[mask_pre_s2], roi_shadow)
         res = np.zeros(len(peaks), self.dtype)
+        res['pre_s2_x'] = np.nan
+        res['pre_s2_y'] = np.nan
         compute_shadow(peaks, split_peaks, res)
 
         res['time'] = peaks['time']
@@ -55,6 +62,8 @@ def _compute_shadow(peaks, split_peaks, res):
         for s2_a in split_peaks[p_i]:
             new_shadow = s2_a['area'] / (p_a['time'] - s2_a['center_time'])
             if new_shadow > res['shadow'][p_i]:
-                res['pre_s2_area'][p_i] = s2_a['area']
                 res['shadow'][p_i] = new_shadow
+                res['pre_s2_area'][p_i] = s2_a['area']
                 res['shadow_dt'][p_i] = p_a['time'] - s2_a['center_time']
+                res['pre_s2_x'][p_i] = p_a['x']
+                res['pre_s2_y'][p_i] = p_a['y']
