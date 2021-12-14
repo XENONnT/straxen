2d4421fb7640f9689da5e9a8f6a253bec5001323
Joran Angevaare <jorana@nikhef.nl>
2021-12-14 12:10:41 +0100

    add docs

--- tests/test_deprecated.py
+++ tests/test_deprecated.py
@@ -1,3 +1,9 @@
+"""
+These tests are for deprecated functions that we will remove in future releases
+
+This is as such a bit of a "to do list" of functions to remove from straxen
+"""
+
 import straxen
 import matplotlib.pyplot as plt
 
