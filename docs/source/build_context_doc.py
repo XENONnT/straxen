fb2da7416f46cceb29e4e78386fab5f78eb91a28
Joran Angevaare <jorana@nikhef.nl>
2021-12-21 17:38:07 +0100

    fix #439

---
+++ docs/source/build_context_doc.py
@@ -0,0 +1,34 @@
+import os
+this_dir = os.path.dirname(os.path.realpath(__file__))
+
+
+base_doc = """
+========
+Contexts
+========
+The contexts are a class from strax and used everywhere in straxen
+
+Below, all of the contexts functions are shown including the 
+`minianalyses <https://straxen.readthedocs.io/en/latest/tutorials/mini_analyses.html?`_
+
+
+Contexts documentation
+----------------------
+
+.. automodule:: strax.context
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+"""
+
+
+def main():
+    """Maybe we one day want to expend this, but for now, let's start with this"""
+    out = base_doc
+    with open(this_dir + f'/reference/context.rst', mode='w') as f:
+        f.write(out)
+
+
+if __name__ == '__main__':
+    main()
