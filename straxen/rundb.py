aa9d188053ebecf27f25cfb2746c0ede4c5f5019
Joran Angevaare <jorana@nikhef.nl>
2021-12-22 09:42:15 +0100

    revert change

--- straxen/rundb.py
+++ straxen/rundb.py
@@ -243,6 +243,7 @@ class RunDB(strax.StorageFrontend):
         projection.update({
             k: True
             for k in f'name number'.split()})
+
         results_dict = dict()
         for doc in self.collection.find(
                 {**run_query, **dq}, projection=projection):
