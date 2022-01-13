b72cc8a8df1de7c2ef20cc286ac2da77138fe0af
sophiafarrell <sja5@rice.edu>
2022-01-12 19:31:01 -0600

    add s2 posteriors in new plugin

--- straxen/plugins/event_w_bayes_class.py
+++ straxen/plugins/event_w_bayes_class.py
@@ -16,22 +16,30 @@ class EventwBayesClass(strax.Plugin):
         dtype += strax.time_fields
         dtype += [('s1_s1prob', np.float32, 'Given an S1, S1 ln probability')]
         dtype += [('s1_s2prob', np.float32, 'Given an S1, S2 ln probability')]
+        dtype += [('s2_s1prob', np.float32, 'Given an S2, S1 ln probability')]
+        dtype += [('s2_s2prob', np.float32, 'Given an S2, S2 ln probability')]
 
         return dtype
 
     def compute(self, peaklets, events):
 
         result = np.empty(len(events), dtype=self.dtype) ### caution here zeros means 1 remember this is ln(Pr), so do not initiate with zeros
-        s1 = np.zeros(len(events), strax.time_fields) 
-        s1['time'] = events['s1_time']
-        s1['endtime'] = events['s1_endtime']
-        fci = strax.fully_contained_in(peaklets, s1)
-        # -1 if no contained, see https://github.com/AxFoundation/strax/blob/109a9da0194dcf5b99992e16ba1bf598874c7e7c/strax/processing/general.py#L105
-        mask = fci >= 0
-        fci = fci[mask]
-        result[fci]['s1_s1prob'] = peaklets[mask]['s1_prob']
-        result[fci]['s1_s2prob'] = peaklets[mask]['s2_prob']
-        result['time'] = events['time']
-        result['endtime'] = events['endtime']
-   
+
+        s1 = np.zeros(len(events), strax.time_fields)
+        s2 = np.zeros(len(events), strax.time_fields)
+
+        # For S1 and S2, match to peak posteriors
+        for peaks, name in zip([s1, s2],
+                              ['s1', 's2']):
+            peaks['time'] = events[f'{name}_time']
+            peaks['endtime'] = events[f'{name}_endtime']
+            fci = strax.fully_contained_in(peaklets, peaks)
+            # -1 if no contained, see https://github.com/AxFoundation/strax/blob/109a9da0194dcf5b99992e16ba1bf598874c7e7c/strax/processing/general.py#L105
+            mask = fci >= 0
+            fci = fci[mask]
+            result[fci][f'{name}_s1prob'] = peaklets[mask]['s1_prob']
+            result[fci][f'{name}_s2prob'] = peaklets[mask]['s2_prob']
+            result['time'] = events['time']
+            result['endtime'] = events['endtime']
+
         return result
