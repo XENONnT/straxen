817db7e329aaafdd12e6e89bce61af57d7cf16df
Joran Angevaare <jorana@nikhef.nl>
2022-01-23 13:48:30 +0100

    remove hacks

--- tests/storage/test_rucio_local.py
+++ tests/storage/test_rucio_local.py
@@ -107,16 +107,6 @@ class TestBasics(unittest.TestCase):
         For testing purposes, slightly alter the RucioFrontend such that
          we can run tests outside of dali too
         """
-        if not straxen.utilix_is_configured():
-            return
-        if 'rcc' not in socket.getfqdn():
-            # If we are not on RCC, for testing, add some dummy site
-            straxen.RucioLocalFrontend.local_rses = {
-                'UC_DALI_USERDISK': r'.rcc.',
-                'test_rucio': f'{socket.getfqdn()}'}
-            straxen.RucioLocalFrontend.local_prefixes = {'test_rucio': r'./rucio_test'}
-            straxen.RucioLocalFrontend.get_rse_prefix = lambda *x: 'test_rucio'
-
         # Some non-existing keys that we will try finding in the test cases.
         cls.test_keys = [
             strax.DataKey(run_id=run_id,
@@ -132,21 +122,6 @@ class TestBasics(unittest.TestCase):
                                              maximum_run_number=10_010,
                                              )
         st.select_runs()
-
-    def test_find_local(self):
-        """Make sure that we don't find the non existing data"""
-        rucio = straxen.RucioLocalFrontend()
-        with self.assertRaises(strax.DataNotAvailable):
-            rucio.find(self.test_keys[0])
-
-    @unittest.skipIf(not straxen.utilix_is_configured(), "No db access, cannot test!")
-    def test_find_several_local(self):
-        """Let's try finding some keys (won't be available)"""
-        rucio = straxen.RucioLocalFrontend()
-        print(rucio)
-        found = rucio.find_several(self.test_keys)
-        # We shouldn't find any of these
-        assert found == [False for _ in self.test_keys]
 
     @unittest.skipIf(not straxen.utilix_is_configured(), "No db access, cannot test!")
     def test_find_several_remote(self):
