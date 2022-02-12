50abd9dba2467b05c0d2744211ee4d96a7ae3d05
Daniel Wenz <dwenz@kabelmail.de>
2022-02-12 13:17:43 +0100

    Bump version of merged S2s

--- straxen/plugins/peaklet_processing.py
+++ straxen/plugins/peaklet_processing.py
@@ -672,7 +672,7 @@ class MergedS2s(strax.OverlapWindowPlugin):
     depends_on = ('peaklets', 'peaklet_classification', 'lone_hits')
     data_kind = 'merged_s2s'
     provides = 'merged_s2s'
-    __version__ = '0.4.1'
+    __version__ = '0.5.0'
 
     def setup(self):
         self.to_pe = straxen.get_correction_from_cmt(self.run_id,
