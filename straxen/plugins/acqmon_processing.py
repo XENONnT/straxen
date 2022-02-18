fdb11899fa9c48d95304cc1aa62f74a62480c24a
Joran Angevaare <jorana@nikhef.nl>
2022-02-18 13:06:54 +0100

    test new version

--- straxen/plugins/acqmon_processing.py
+++ straxen/plugins/acqmon_processing.py
@@ -146,7 +146,7 @@ class VetoIntervals(strax.OverlapWindowPlugin):
 
     def infer_dtype(self):
         dtype = [(('veto interval [ns]', 'veto_interval'), np.int64),
-                 (('veto signal type', 'veto_type'), np.str_('U9'))]
+                 (('veto signal type', 'veto_type'), np.str_('U20'))]
         dtype += strax.time_fields
         return dtype
 
@@ -195,7 +195,7 @@ class VetoIntervals(strax.OverlapWindowPlugin):
         if n_artificial:
             result[vetos_seen:n_artificial]['time'] = artificial_deadtime['time']
             result[vetos_seen:n_artificial]['endtime'] = strax.endtime(artificial_deadtime)
-            result[vetos_seen:n_artificial]['veto_type'] = 'straxen_deadtime'
+            result[vetos_seen:n_artificial]['veto_type'] = 'straxen_deadtime_veto'
             vetos_seen += n_artificial
 
         result = result[:vetos_seen]
@@ -216,90 +216,156 @@ def channel_select_(rr, ch):
 
 
 @export
-@strax.takes_config(
-    strax.Option('veto_proximity_window', default=int(5e8), type=int, track=True,
-                 help='Maximum separation between veto stop and start pulses [ns]'))
 class VetoProximity(strax.OverlapWindowPlugin):
     """
     Find the closest next/previous veto start and end to each event center.
-
-    previous_x: Time in ns between the time center of an event and the previous x
-    next_x: Time in ns between the time center of an event and the next x
-    This also considers any x inside the event. x could be either:
-        - busy_x: busy on/off signal
-        - he_x:   high energy channels busy on/off signal
-        - hev_x:  high energy veto on/off signal
     """
 
-    __version__ = '0.1.3_test'
-    depends_on = ('event_basics', 'aqmon_hits')
+    __version__ = '1.1.3_test'
+    # Strictly speaking, we could depend on 'events', but then you couldn't
+    # change the event_window_fields to e.g. s1_time and s2_endtime.
+    depends_on = ('event_basics', 'veto_intervals')
     provides = 'veto_proximity'
     data_kind = 'events'
     save_when = strax.SaveWhen.ALWAYS
-    veto_names = ['busy', 'he', 'hev']
+
+    event_window_fields = straxen.URLConfig(
+        default=('time', 'endtime'),
+        help='Fields to determine where to look for overlaps for using '
+             'this plugin in the events. The default uses start and endtime '
+             'of an event, but this can also be the S1 or S2 start/endtime'
+    )
+
+    veto_proximity_window=straxen.URLConfig(
+        default=int(1e9),
+        help='Maximum separation between veto stop and start pulses [ns]'
+    )
+
+    veto_names = ['busy', 'he', 'hev', 'straxen_deadtime']
+
 
     def infer_dtype(self):
         dtype = []
-        for n in self.veto_names:
-            dtype += [
-                ((f'Time to previous {n} veto start [ns]', f'previous_{n}_on'), np.int64),
-                ((f'Time to previous {n} veto end [ns]', f'previous_{n}_off'), np.int64),
-                ((f'Time to next {n} veto start [ns]', f'next_{n}_on'), np.int64),
-                ((f'Time to next {n} veto end [ns]', f'next_{n}_off'), np.int64),
-                ]
         dtype += strax.time_fields
+        start_field, stop_field = self.event_window_fields
+        for name in self.veto_names:
+            dtype += [
+                (('Duration of event overlapping with veto [ns]',
+                  f'veto_{name}__overlap'),
+                 np.int64),
+                ((f'Time (absolute) to previous "{name}"-veto from "{start_field}" of event [ns]',
+                  f'time_to_previous_{name}'),
+                 np.int64),
+                ((f'Time (absolute) to next "{name}"-veto from from "{stop_field}" of event [ns]',
+                  f'time_to_next_{name}'),
+                 np.int64),
+            ]
         return dtype
 
-    def setup(self):
-        print('VetoProximity is outdated!')
-        self.channel_map = {name: ch + straxen.n_hard_aqmon_start for ch, name in
-                            enumerate(['sum_wf', 'm_veto_sync',
-                                       'hev_stop', 'hev_start',
-                                       'he_stop', 'he_start',
-                                       'busy_stop', 'busy_start'])}
-        self.states = ['on', 'off']
-
     def get_window_size(self):
-        return int(self.config['veto_proximity_window'] * 100)
+        return self.veto_proximity_window
 
-    def compute(self, events, aqmon_hits):
+    def set_result_for_veto(self,
+                            result_buffer: np.ndarray,
+                            event_window: np.ndarray,
+                            veto_intervals: np.ndarray,
+                            veto_name: str) -> None:
+        """
+        Fill the result buffer inplace. Goal is to find vetos with
+        <veto_name> that are either during, before or after the
+         current event_window.
+
+        :param result_buffer: The buffer to fill inplace
+        :param event_window: start/stop boundaries of the event to consider.
+            Should be an array with ['time'] and ['endtime'] which can be
+            based on event start/end times or S1/S2 times
+        :param veto_intervals: veto intervals datatype
+        :param veto_name: The name of the veto to fill the result buffer for
+        :return: Nothing, results are filled in place
+        """
+        # Set defaults to be some very long time
+        result_buffer[f'time_to_previous_{veto_name}'] = T_NO_VETO_FOUND
+        result_buffer[f'time_to_next_{veto_name}'] = T_NO_VETO_FOUND
+
+        selected_intervals = veto_intervals[veto_intervals[f'{veto_name}_veto']]
+        if not len(selected_intervals):
+            return
+
+        vetos_during_event = strax.touching_windows(selected_intervals,
+                                                    event_window)
+
+        # Figure out the vetos *during* an event
+        for event_i, veto_window in enumerate(vetos_during_event):
+            if veto_window[1] - veto_window[0]:
+                vetos_in_window = vetos_during_event[veto_window[0]:
+                                                     veto_window[1]].copy()
+                starts = np.clip(vetos_in_window['time'],
+                                 event_window[event_i]['time'],
+                                 event_window[event_i]['endtime'])
+                strops = np.clip(vetos_in_window['endtime'],
+                                 event_window[event_i]['time'],
+                                 event_window[event_i]['endtime'])
+                # Now sum over all the stops-starts that are clipped
+                # within the duration of the event
+                result_buffer[f'veto_{veto_name}__overlap'] = np.sum(strops - starts)
+
+        # Find the next and previous vetos
+        times_to_prev, times_to_next = self.abs_time_to_prev_next(event_window, selected_intervals)
+        mask_prev = times_to_prev > 0
+        result_buffer[f'time_to_previous_{veto_name}'][mask_prev]=times_to_prev[mask_prev]
+
+        max_next = times_to_next>0
+        result_buffer[f'time_to_next_{veto_name}'][mask_prev]=times_to_next[max_next]
+        # for event_i, ev_wind in enumerate(event_window):
+        #     # Two cases left, either vetos are before or after the event window
+        #     interval_before = selected_intervals['endtime'] < ev_wind['time']
+        #     interval_after = selected_intervals['time'] > ev_wind['endtime']
+        #
+        #     if np.sum(interval_before):
+        #         prev_intervals = selected_intervals[interval_before]
+        #         time_to_prev = np.abs(ev_wind['time']-prev_intervals['endtime'])
+        #         prev_idx = np.argmin(time_to_prev)
+        #         result_buffer[f'time_to_previous_{veto_name}'][event_i] = time_to_prev[prev_idx]
+        #
+        #     if np.sum(interval_after):
+        #         next_intervals = selected_intervals[interval_after]
+        #         time_to_next = np.abs(next_intervals['endtime']-ev_wind['endtime'])
+        #         next_idx = np.argmin(time_to_next)
+        #         result_buffer[f'time_to_next_{veto_name}'][event_i] = time_to_next[next_idx]
+
+    @numba.njit
+    @staticmethod
+    def abs_time_to_prev_next(event_window, selected_intervals):
+        times_to_prev = np.ones(len(event_window))*-1
+        times_to_next = np.ones(len(event_window))*-1
+        for event_i, ev_wind in enumerate(event_window):
+            # Two cases left, either vetos are before or after the event window
+            interval_before = selected_intervals['endtime'] < ev_wind['time']
+            interval_after = selected_intervals['time'] > ev_wind['endtime']
+
+            if np.sum(interval_before):
+                prev_intervals = selected_intervals[interval_before]
+                time_to_prev = np.abs(ev_wind['time']-prev_intervals['endtime'])
+                prev_idx = np.argmin(time_to_prev)
+                times_to_prev[event_i] = time_to_prev[prev_idx]
+
+            if np.sum(interval_after):
+                next_intervals = selected_intervals[interval_after]
+                time_to_next = np.abs(next_intervals['endtime']-ev_wind['endtime'])
+                next_idx = np.argmin(time_to_next)
+                times_to_next[event_i] = time_to_next[next_idx]
+        return times_to_prev, times_to_next
+
+    def compute(self, events, veto_intervals):
         result = np.zeros(len(events), self.dtype)
-        t_event_centers = (events['time'] + events['endtime']) // 2
-
-        for name in self.veto_names:
-            # For each state find the next and previous veto
-            for state in self.states:
-                prev = f'previous_{name}_{state}'
-                nxt = f'next_{name}_{state}'
-
-                if state == 'on':
-                    aqmon_chan = self.channel_map[f'{name}_start']
-                else:
-                    aqmon_chan = self.channel_map[f'{name}_stop']
-                veto_start_time_selection = aqmon_hits[aqmon_hits['channel'] == aqmon_chan]['time']
-
-                inx = 0
-                for event_i, event_center in enumerate(t_event_centers):
-                    if len(veto_start_time_selection):
-                        inx = np.searchsorted(veto_start_time_selection, event_center, side='right')
-
-                    # Time to previous veto on/off
-                    # Just using a huge value that will not fit in any potential
-                    # DAQVetoCut range
-                    if inx == 0:
-                        previous_veto = T_NO_VETO_FOUND
-                    else:
-                        previous_veto = event_center - veto_start_time_selection[inx - 1]
-                    # Time to next veto on/off
-                    if inx == len(veto_start_time_selection):
-                        next_veto = T_NO_VETO_FOUND
-                    else:
-                        next_veto = veto_start_time_selection[inx] - event_center
-
-                    result[event_i][prev] = previous_veto
-                    result[event_i][nxt] = next_veto
-
-        # Add the events time and endtime to the final result
         result['time'] = events['time']
         result['endtime'] = events['endtime']
+
+        # Get containers for touching windows based on self.event_window_fields
+        event_window = np.zeros(len(events), dtype=strax.time_fields)
+        event_window['time'] = events[self.event_window_fields[0]]
+        event_window['endtime'] = events[self.event_window_fields[1]]
+
+        for veto_name in self.veto_names:
+            self.set_result_for_veto(result, event_window, veto_intervals, veto_name)
         return result
