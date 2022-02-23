48a8545033c19fd3d8393b5b204aca867eaf8b10
ahiguera-mx <ahiguera-mx@fried.rice.edu>
2022-02-23 16:46:19 -0600

    improve code based on review

--- straxen/plugins/bayes_peaklet_classification.py
+++ straxen/plugins/bayes_peaklet_classification.py
@@ -1,11 +1,20 @@
 import numpy as np
+import numba
 from scipy.special import logsumexp
 import strax
 import straxen
 
+
 class BayesPeakletClassification(strax.Plugin):
     """
     Bayes Peaklet classification
+    Returns the ln probability of a each event belonging to the S1 and S2 class.
+    Uses conditional probabilities and data parameterization learned from wfsim data.
+
+    :param peaklets: peaklets
+    :param waveforms: peaklets waveforms in PE/ns
+    :param quantiles: quantiles in ns
+    :returns: the ln probability of a each peaklet belonging to S1 and S2 class
     """
     provides = 'peaklet_classification_bayes'
     depends_on = ('peaklets',)
@@ -17,9 +26,9 @@ class BayesPeakletClassification(strax.Plugin):
 
     # Descriptor configs
     bayes_config_file = straxen.URLConfig(
-            default='resource://'
-            'conditional_probabilities_and_bins_v1_w_global_v6.npz?fmt=npy',
-        help='Bayes configuration file, conditional probabilities and Bayes discrete bins'
+        default='resource://'
+        'conditional_probabilities_and_bins_v1_w_global_v6.npz?fmt=npy',
+        help='Bayes configuration file, conditional probabilities tables and Bayes discrete bins'
     )
     num_nodes = straxen.URLConfig(
         default=50,
@@ -33,77 +42,16 @@ class BayesPeakletClassification(strax.Plugin):
     def setup(self):
 
         self.class_prior = np.ones(self.classes)/self.classes
-        self.bins = self.bayes_config_file['bins'] 
+        self.bins = self.bayes_config_file['bins']
         self.cpt = self.bayes_config_file['cprob']
-       
+
     def compute(self, peaklets):
 
-        s1_ln_prob = np.zeros(len(peaklets), dtype=np.float32)
-        s2_ln_prob = np.zeros(len(peaklets), dtype=np.float32)
+        waveforms, quantiles = compute_wf_and_quantiles(peaklets, self.num_nodes)
 
-        waveforms = np.zeros((len(peaklets), self.num_nodes))
-        quantiles = np.zeros((len(peaklets), self.num_nodes))
-
-        ###
-        # calculate waveforms and quantiles.
-        ###
-        num_samples = peaklets['data'].shape[1]
-        step_size = int(num_samples/self.num_nodes)
-        steps = np.arange(0, num_samples+1, step_size)
-
-        data = peaklets['data'].copy()
-        dts = peaklets['dt'].copy()
-        data[data < 0.0] = 0.0
-        # can we do this faster?
-        for i, p in enumerate(peaklets):
-            fp = np.arange(0, num_samples+1, 1)*p['dt']
-            xp = np.append([0.0], np.cumsum(data[i, :]) / np.sum(data[i, :]))
-            cumsum_steps = np.interp(np.linspace(0., 1., self.num_nodes, endpoint=False), xp, fp)
-            cumsum_steps = np.append(cumsum_steps, fp[-1])
-            quantiles[i, :] = cumsum_steps[1:] - cumsum_steps[:-1]
-        
-        for j in range(self.num_nodes):
-            waveforms[:, j] = np.sum(data[:, steps[j]:steps[j+1]],axis=1)
-        waveforms = waveforms/(dts*step_size)[:,np.newaxis]
-
-        del data
-        ###
-        # Bin the waveforms and quantiles.
-        ###
-        waveform_bin_edges = self.bins[0, :][self.bins[0, :] > -1]
-        waveform_num_bin_edges = len(waveform_bin_edges)
-        quantile_bin_edges = self.bins[1, :][self.bins[1, :] > -1]
-        quantile_num_bin_edges = len(quantile_bin_edges)
-
-        waveform_values = np.digitize(waveforms, bins=waveform_bin_edges)-1
-        waveform_values[waveform_values < 0] = int(0)
-        waveform_values[waveform_values > int(waveform_num_bin_edges - 2)] = int(waveform_num_bin_edges - 2)
-
-        quantile_values = np.digitize(quantiles, bins=quantile_bin_edges)-1
-        quantile_values[quantile_values < 0] = int(0)
-        quantile_values[quantile_values > int(quantile_num_bin_edges - 2)] = int(quantile_num_bin_edges - 2)
-
-        values_for_inference = np.append(waveform_values, quantile_values, axis=1)
-
-        ###
-        # Inference of the binned values.
-        ###
-        distributions = [[] for i in range(self.num_nodes*2)]
-        for i in np.arange(0, self.num_nodes, 1):
-            distributions[i] = np.asarray(self.cpt[i, :waveform_num_bin_edges-1, :])
-        for i in np.arange(self.num_nodes, self.num_nodes * 2, 1):
-            distributions[i] = np.asarray(self.cpt[i, :quantile_num_bin_edges-1, :])
-
-        lnposterior = np.zeros((len(peaklets), self.num_nodes*2, self.classes))
-        for i in range(self.num_nodes*2):
-            lnposterior[:, i, :] = np.log(distributions[i][values_for_inference[:, i], :])
-
-        lnposterior_sumsamples = np.sum(lnposterior, axis=1)
-        lnposterior_sumsamples = np.sum([lnposterior_sumsamples, np.log(self.class_prior)[np.newaxis, ...]])
-        lnposterior_normed = lnposterior_sumsamples - logsumexp(lnposterior_sumsamples, axis=1)[..., np.newaxis]
-
-        s1_ln_prob = lnposterior_normed[:, 0]
-        s2_ln_prob = lnposterior_normed[:, 1]
+        s1_ln_prob, s2_ln_prob = compute_inference(self.bins, self.num_nodes, self.cpt,
+                                                   self.classes, self.class_prior,
+                                                   waveforms, quantiles)
 
         return dict(time=peaklets['time'],
                     dt=peaklets['dt'],
@@ -112,3 +60,82 @@ class BayesPeakletClassification(strax.Plugin):
                     s1_ln_prob=s1_ln_prob,
                     s2_ln_prob=s2_ln_prob
                     )
+
+
+def compute_wf_and_quantiles(peaklets: np.ndarray, num_nodes: int):
+    """
+    Compute waveforms and quantiles for a given number of nodes(atributes)
+    :param peaklets:
+    :param num_nodes: number of nodes or atributes
+    :return: waveforms and quantiles
+    """
+    waveforms = np.zeros((len(peaklets), num_nodes))
+    quantiles = np.zeros((len(peaklets), num_nodes))
+
+    num_samples = peaklets['data'].shape[1]
+    step_size = int(num_samples/num_nodes)
+    steps = np.arange(0, num_samples+1, step_size)
+
+    data = peaklets['data'].copy()
+    dts = peaklets['dt'].copy()
+    data[data < 0.0] = 0.0
+    for i, p in enumerate(peaklets):
+        sample_number = np.arange(0, num_samples+1, 1)*p['dt']
+        frac_of_cumsum = np.append([0.0], np.cumsum(data[i, :]) / np.sum(data[i, :]))
+        cumsum_steps = np.interp(np.linspace(0., 1., num_nodes, endpoint=False), frac_of_cumsum, sample_number)
+        cumsum_steps = np.append(cumsum_steps, sample_number[-1])
+        quantiles[i, :] = cumsum_steps[1:] - cumsum_steps[:-1]
+
+    for j in range(num_nodes):
+        waveforms[:, j] = np.sum(data[:, steps[j]:steps[j+1]], axis=1)
+    waveforms = waveforms/(dts*step_size)[:, np.newaxis]
+
+    del data
+    return waveforms, quantiles
+
+
+def compute_inference(bins: int, num_nodes: int, cpt: np.ndarray, classes: int, class_prior: np.ndarray,
+                      waveforms: np.ndarray, quantiles: np.ndarray):
+    """
+    Bin the waveforms and quantiles according to Bayes bins and compute inference
+    :param bins: Bayes bins
+    :param num_nodes: number of nodes or atributes
+    :param cpt: conditioanl probability tables
+    :param classes: number of classes
+    :param class_prior: class_prior
+    :param waveforms: waveforms
+    :param quantiles: quantiles
+    :return: ln probability per class S1/S2
+    """
+    # Bin the waveforms and quantiles.
+    waveform_bin_edges = bins[0, :][bins[0, :] > -1]
+    waveform_num_bin_edges = len(waveform_bin_edges)
+    quantile_bin_edges = bins[1, :][bins[1, :] > -1]
+    quantile_num_bin_edges = len(quantile_bin_edges)
+
+    waveform_values = np.digitize(waveforms, bins=waveform_bin_edges)-1
+    waveform_values[waveform_values < 0] = int(0)
+    waveform_values[waveform_values > int(waveform_num_bin_edges - 2)] = int(waveform_num_bin_edges - 2)
+
+    quantile_values = np.digitize(quantiles, bins=quantile_bin_edges)-1
+    quantile_values[quantile_values < 0] = int(0)
+    quantile_values[quantile_values > int(quantile_num_bin_edges - 2)] = int(quantile_num_bin_edges - 2)
+
+    values_for_inference = np.append(waveform_values, quantile_values, axis=1)
+
+    # Inference
+    distributions = [[] for i in range(num_nodes*2)]
+    for i in np.arange(0, num_nodes, 1):
+        distributions[i] = np.asarray(cpt[i, :waveform_num_bin_edges-1, :])
+    for i in np.arange(num_nodes, num_nodes * 2, 1):
+        distributions[i] = np.asarray(cpt[i, :quantile_num_bin_edges-1, :])
+
+    lnposterior = np.zeros((len(waveforms), num_nodes*2, classes))
+    for i in range(num_nodes*2):
+        lnposterior[:, i, :] = np.log(distributions[i][values_for_inference[:, i], :])
+
+    lnposterior_sumsamples = np.sum(lnposterior, axis=1)
+    lnposterior_sumsamples = np.sum([lnposterior_sumsamples, np.log(class_prior)[np.newaxis, ...]])
+    lnposterior_normed = lnposterior_sumsamples - logsumexp(lnposterior_sumsamples, axis=1)[..., np.newaxis]
+
+    return lnposterior_normed[:, 0], lnposterior_normed[:, 1]
--- straxen/plugins/event_w_bayes_class.py
+++ straxen/plugins/event_w_bayes_class.py
@@ -2,35 +2,38 @@ import strax
 import straxen
 import numpy as np
 
-class EventwBayesClass(strax.Plugin):
 
+class EventwBayesClass(strax.Plugin):
+    """
+    Append at event level the posterior probability for an
+    S1, S2, alt_S1 and alt_S2
+    """
     provides = 'event_w_bayes_class'
     depends_on = ('peaklet_classification_bayes', 'event_basics')
     data_kind = 'events'
     __version__ = '0.0.1'
 
-
     def infer_dtype(self):
-        
+
         dtype = []
         dtype += strax.time_fields
-        dtype += [('s1_s1_ln_prob', np.float32, 'Given an S1, S1 ln probability')]
-        dtype += [('s1_s2_ln_prob', np.float32, 'Given an S1, S2 ln probability')]
-        dtype += [('s2_s1_ln_prob', np.float32, 'Given an S2, S1 ln probability')]
-        dtype += [('s2_s2_ln_prob', np.float32, 'Given an S2, S2 ln probability')]
+        for name in ['s1', 's2', 'alt_s1', 'alt_s2']:
+            dtype += [(f'{name}_s1_ln_prob', np.float32, f'Given an {name}, s1 ln probability')]
+            dtype += [(f'{name}_s2_ln_prob', np.float32, f'Given an {name}, s2 ln probability')]
 
         return dtype
 
     def compute(self, peaklets, events):
 
-        result = np.empty(len(events), dtype=self.dtype) ### caution here zeros means 1 remember this is ln(Pr), so do not initiate with zeros
+        result = np.empty(len(events), dtype=self.dtype)  # caution here zeros means 1 remember this is ln(Pr), so do not initiate with zeros
 
         s1 = np.zeros(len(events), strax.time_fields)
         s2 = np.zeros(len(events), strax.time_fields)
+        s1_alt = np.zeros(len(events), strax.time_fields)
+        s2_alt = np.zeros(len(events), strax.time_fields)
 
         # For S1 and S2, match to peak posteriors
-        for peaks, name in zip([s1, s2],
-                              ['s1', 's2']):
+        for peaks, name in zip([s1, s2, s1_alt, s2_alt], ['s1', 's2', 'alt_s1', 'alt_s2']):
             peaks['time'] = events[f'{name}_time']
             peaks['endtime'] = events[f'{name}_endtime']
             fci = strax.fully_contained_in(peaklets, peaks)
@@ -42,10 +45,11 @@ class EventwBayesClass(strax.Plugin):
             result['time'] = events['time']
             result['endtime'] = events['endtime']
 
-        # events can be made out of one peak, this is to enusure user should not look at the other prob
-        no_s1 = np.where(events['s1_index']==-1)
-        if no_s1:
-            result[f's1_s1_ln_prob'][no_s1] = np.nan
-            result[f's1_s2_ln_prob'][no_s1] = np.nan
-        
+        # events can be made out of one peak, this is to enusure user should not look at prob
+        for name in (['s1', 'alt_s1', 'alt_s2']):
+            no_s_peak = np.where(events[f'{name}_index'] == -1)
+            if no_s_peak:
+                result[f'{name}_s1_ln_prob'][no_s_peak] = np.nan
+                result[f'{name}_s2_ln_prob'][no_s_peak] = np.nan
+
         return result
