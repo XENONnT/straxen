706ac493c11afda0f3da0a4c718a858b4f8379f4
Evan Shockley <shockley.evan@gmail.com>
2022-02-28 08:05:35 -0500

    Update corrected areas (#931)
    * Update corrected areas
    
    
    Co-authored-by: Joran R. Angevaare <jorana@nikhef.nl>
    Co-authored-by: Daniel Wenz <43881800+WenzDaniel@users.noreply.github.com>
    Co-authored-by: Aaron Higuera <64651045+ahiguera-mx@users.noreply.github.com>
    Co-authored-by: Daniel Wenz <dwenz@kabelmail.de>
    Co-authored-by: Shingo Kazama <kazama@isee.nagoya-u.ac.jp>
    Co-authored-by: Lutz Althüser <coding@l-althueser.de>

--- straxen/corrections_services.py
+++ straxen/corrections_services.py
@@ -19,7 +19,7 @@ corrections_w_file = ['mlp_model', 'cnn_model', 'gcn_model',
 
 single_value_corrections = ['elife_xenon1t', 'elife', 'baseline_samples_nv',
                             'electron_drift_velocity', 'electron_drift_time_gate',
-                            'se_gain', 'rel_extraction_eff']
+                            'se_gain', 'rel_extraction_eff', 'relative_light_yield']
 
 arrays_corrections = ['hit_thresholds_tpc', 'hit_thresholds_he',
                       'hit_thresholds_nv', 'hit_thresholds_mv']
