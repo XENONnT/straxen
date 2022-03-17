7c5004876bac4e5314900cbc7dec02ca78dad3ca
Yossi Mosbacher <joe.mosbacher>
2022-03-17 19:08:39 +0200

    add cmt2 plugin notebook

--- straxen/plugins/cmt2_example.py
+++ straxen/plugins/cmt2_example.py
@@ -46,20 +46,3 @@ class CMT2Example(strax.Plugin):
        
         return  result
 
-
-@export
-class EventInfo1T(strax.MergeOnlyPlugin):
-    """
-    Plugin which merges the information of all event data_kinds into a
-    single data_type.
-
-    This only uses 1T data-types as several event-plugins are nT only
-    """
-    depends_on = ['event_basics',
-                  'event_positions',
-                  'corrected_areas',
-                  'energy_estimates',
-                  ]
-    provides = 'event_info'
-    save_when = strax.SaveWhen.ALWAYS
-    __version__ = '0.0.1'
