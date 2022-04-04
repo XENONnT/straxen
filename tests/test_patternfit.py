01a0213b03f6662fd5139600e9b3956d66e46298
GiovanniVolta <gvolta@dali-login1.rcc.local>
2022-04-04 11:32:27 -0500

    I dont really get given decoration

--- tests/test_patternfit.py
+++ tests/test_patternfit.py
@@ -4,10 +4,10 @@ from straxen.plugins.event_patternfit import binom_test
 
 
 @settings(deadline=None)
-#@given(strategies.floats(0., 1.), strategies.floats(1e-3, 1.-1e-3), strategies.floats(1., 1000))
-@given(0.0, 0.05, 10.0)
+@given(strategies.floats(0., 1.), strategies.floats(1e-3, 1.-1e-3), strategies.floats(1., 1000))
+#@given(0.0, 0.05, 10.0)
 @example(0., 0.05, 10.) #AFT_observed, AFT_expected (from xyz), S1_total
 def test_patternfit_stats(aftobs, aft, s1tot):
     s1top = aftobs*s1tot
-    assert(binom_test(s1top, s1tot, aft) == 1.)
-    
+    assert( (binom_test(s1top, s1tot, aft) >= 0) & (binom_test(s1top, s1tot, aft) <= 1) )
+        
