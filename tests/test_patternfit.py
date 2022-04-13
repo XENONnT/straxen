39c950f3a1defbf819f285755716f4ddc15d2a12
Knut Dundas Morå <knutdundasmoraa@gmail.com>
2022-04-13 15:20:35 -0500

    add check of PDF inversion

--- tests/test_patternfit.py
+++ tests/test_patternfit.py
@@ -1,6 +1,7 @@
 import unittest
 from hypothesis import strategies, given, settings, example
-from straxen.plugins.event_patternfit import binom_test
+from straxen.plugins.event_patternfit import binom_test, lbinom_pmf_mode, lbinom_pmf, lbinom_pmf_inverse
+import numpy as np
 
 
 @settings(deadline=None)
@@ -10,4 +11,36 @@ from straxen.plugins.event_patternfit import binom_test
 def test_patternfit_stats(aftobs, aft, s1tot):
     s1top = aftobs*s1tot
     assert( (binom_test(s1top, s1tot, aft) >= 0) & (binom_test(s1top, s1tot, aft) <= 1) )
+
+@settings(deadline=None)
+@given(strategies.floats(0., 1.), strategies.floats(1e-3, 1.-1e-3), strategies.floats(1., 1000))
+@example(0., 0.05, 10.) #AFT_observed, AFT_expected (from xyz), S1_total
+def test_inverse_pdf(aftobs, aft, s1tot):
+    s1top = aftobs * s1tot
+    #code stolen from binom_test, checking if the other point on 
+
+    k = s1top
+    n = s1tot
+    p = aft
+
+    mode = lbinom_pmf_mode(0, n, 0, (n, p))
+    distance = abs(mode - k)
+    target = lbinom_pmf(k, n, p)
+
+    if k < mode: 
+        j_min = mode
+        j_max = min(mode + 1.5 * distance, n)
+        j = lbinom_pmf_inverse(j_min, j_max, target, (n, p))
+    else:
+        j_min = max(mode - 1.5 * distance, 0)
+        j_max = mode
+        j = lbinom_pmf_inverse(j_min, j_max, target, (n, p))  
+    if np.isnan(j):
+        #if no other crossing is found, we cannot check if the two give same pdf
+        assert(True)
+    else:
+        pdf_value = lbinom_pmf(j, n, p)
+        np.testing.assert_almost_equal(pdf_value, target,2) #values in log
+
+
         
