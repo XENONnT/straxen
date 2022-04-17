0394659e00c0afbd8c26389461e2426185ef32b8
GiovanniVolta <gvolta@dali-login1.rcc.local>
2022-04-17 05:05:26 -0500

    I think the tests are fine now

--- tests/test_patternfit.py
+++ tests/test_patternfit.py
@@ -6,7 +6,7 @@ import scipy.stats as sps
 
 @settings(deadline=None)
 #AFT_observed, AFT_expected (from xyz), S1_total
-@given(strategies.floats(0., 1.), strategies.floats(0.01, 0.99), strategies.floats(2., 100))
+@given(strategies.floats(0., 1.), strategies.floats(0.01, 0.99), strategies.floats(2., 1000))
 #@example(0., 0.05, 10.) 
 def test_patternfit_stats(aftobs, aft, s1tot):
     s1top = aftobs*s1tot
@@ -14,7 +14,7 @@ def test_patternfit_stats(aftobs, aft, s1tot):
 
 @settings(deadline=None)
 #AFT_observed, AFT_expected (from xyz), S1_total
-@given(strategies.floats(0., 1.), strategies.floats(0.01, 0.99), strategies.floats(2., 100))
+@given(strategies.floats(0., 1.), strategies.floats(0.01, 0.99), strategies.floats(2., 1000))
 #@example(0., 0.05, 10.) 
 def test_inverse_pdf(aftobs, aft, s1tot):
     s1top = aftobs * s1tot
@@ -36,38 +36,37 @@ def test_inverse_pdf(aftobs, aft, s1tot):
         j_min = max(mode - 2.0 * distance, 0)
         j_max = mode
         j = lbinom_pmf_inverse(j_min, j_max, target, (n, p))  
-    if np.isnan(j):
+    if np.isnan(j) or j == n:
         #if no other crossing is found, we cannot check if the two give same pdf
         assert(True)
     else:
         pdf_value = lbinom_pmf(j, n, p)
         #values in log, it is not _really_ crucial that they're precisely equal
-        np.testing.assert_almost_equal(pdf_value, target, decimal=1) 
-
+        np.testing.assert_almost_equal(pdf_value, target, decimal=2) 
 
 @settings(deadline=None)
 @strategies.composite 
 def n_and_k(draw):
-    n = draw(strategies.integers(2, 100))
+    n = draw(strategies.integers(2, 1000))
     k = draw(strategies.integers(0, n))
     return n,k
 
 @settings(deadline=None)
-@given(nk = n_and_k(), p = strategies.floats(1e-2, 0.99))
+@given(nk = n_and_k(), p = strategies.floats(0.01, 0.99))
 @example((10, 5), 0.5)
 def test_pmf(nk, p):
     #test that at integer n, k binom_pmf agrees with the scipy result
     n, k = nk
     sps_lpmf = sps.binom(n, p).logpmf(k)
     straxen_lpmf = lbinom_pmf(k, n, p)
-    np.testing.assert_almost_equal(straxen_lpmf, sps_lpmf, decimal=1)
+    np.testing.assert_almost_equal(straxen_lpmf, sps_lpmf, decimal=2)
 
 @settings(deadline=None)
-@given(nk = n_and_k(), p = strategies.floats(1e-2, 0.99))
+@given(nk = n_and_k(), p = strategies.floats(0.01, 0.99))
 #@example((10, 5), 0.5)
 def test_pvalue(nk, p):
     #test that at integer n, k binom_pmf agrees with the scipy result
     n, k = nk
     sps_pval = sps.binom(n, p).sf(k + 0.1)
     straxen_pval = binom_sf(k, n, p)
-    np.testing.assert_almost_equal(straxen_pval, sps_pval, decimal=1)
+    np.testing.assert_almost_equal(straxen_pval, sps_pval, decimal=2)
