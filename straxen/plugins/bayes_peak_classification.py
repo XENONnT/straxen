a041fd13f9a82b73611a341fc7b67a9dc0dfea8d
Joran Angevaare <jorana@nikhef.nl>
2022-04-28 11:11:23 +0200

    fix review

--- straxen/plugins/bayes_peak_classification.py
+++ straxen/plugins/bayes_peak_classification.py
@@ -57,7 +57,7 @@ class BayesPeakClassification(strax.Plugin):
                                                    self.n_bayes_classes, self.class_prior,
                                                    waveforms, quantiles)
         result['time'] = peaks['time']
-        result['endtime'] = peaks['time'] + peaks['dt'] * peaks['length']
+        result['endtime'] = strax.endtime(peaks)
         result['ln_prob_s1'] = ln_prob_s1
         result['ln_prob_s2'] = ln_prob_s2
         return result
@@ -88,12 +88,12 @@ def _compute_wf_and_quantiles(data, sample_length, bayes_n_nodes: int):
     cumsum_steps = np.zeros(bayes_n_nodes + 1, dtype=np.float64)
     frac_of_cumsum = np.zeros(num_samples + 1)
     sample_number_div_dt = np.arange(0, num_samples+1, 1)
-    for i, (waveform, dt) in enumerate(zip(data, sample_length)):
+    for i, (samples, dt) in enumerate(zip(data, sample_length)):
         # reset buffers
         frac_of_cumsum[:] = 0
         cumsum_steps[:] = 0
 
-        frac_of_cumsum[1:] = np.cumsum(waveform)
+        frac_of_cumsum[1:] = np.cumsum(samples)
         frac_of_cumsum[1:] = frac_of_cumsum[1:]/frac_of_cumsum[-1]
 
         cumsum_steps[:-1] = np.interp(inter_points, frac_of_cumsum, sample_number_div_dt * dt)
@@ -101,7 +101,7 @@ def _compute_wf_and_quantiles(data, sample_length, bayes_n_nodes: int):
         quantiles[i] = cumsum_steps[1:] - cumsum_steps[:-1]
 
         for j in range(bayes_n_nodes):
-            waveforms[i][j] = np.sum(waveform[steps[j]:steps[j+1]])
+            waveforms[i][j] = np.sum(samples[steps[j]:steps[j+1]])
         waveforms[i] /= (step_size*dt)
 
     return waveforms, quantiles
@@ -117,7 +117,7 @@ def compute_inference(bins: int,
     """
     Bin the waveforms and quantiles according to Bayes bins and compute inference
     :param bins: Bayes bins
-    :param bayes_n_nodes: number of nodes or atributes
+    :param bayes_n_nodes: number of nodes or attributes
     :param cpt: conditional probability tables
     :param n_bayes_classes: number of classes
     :param class_prior: class_prior
@@ -131,15 +131,55 @@ def compute_inference(bins: int,
     quantile_bin_edges = bins[1, :][bins[1, :] > -1]
     quantile_num_bin_edges = len(quantile_bin_edges)
     waveform_values = np.digitize(waveforms, bins=waveform_bin_edges)-1
-    _set_2d_to_zero_or_max_val(waveform_values,
-                               min_val=0,
-                               max_val=np.int64(waveform_num_bin_edges - 2))
+    waveform_values = np.clip(waveform_values, 0, waveform_num_bin_edges - 2)
 
     quantile_values = np.digitize(quantiles, bins=quantile_bin_edges)-1
-    _set_2d_to_zero_or_max_val(quantile_values,
-                               min_val=0,
-                               max_val=np.int64(quantile_num_bin_edges - 2))
+    quantile_values = np.clip(quantile_values, 0, quantile_num_bin_edges - 2)
 
+    lnposterior = get_log_posterior(bayes_n_nodes,
+                                    waveforms,
+                                    cpt,
+                                    waveform_num_bin_edges,
+                                    quantile_num_bin_edges,
+                                    n_bayes_classes,
+                                    waveform_values,
+                                    quantile_values,
+                                    )
+
+    lnposterior_sumsamples = np.sum(lnposterior, axis=1)
+    lnposterior_sumsamples = lnposterior_sumsamples + np.log(class_prior)
+
+    # If you need to numbafy, use
+    # github.com/XENONnT/straxen/blob/c06a611a5a4289c65b9830fc1e68027149b6a5fd/straxen/plugins/bayes_peak_classification.py#L119  # noqa
+    normalization = logsumexp(lnposterior_sumsamples, axis=1)
+    ln_prob_s1 = lnposterior_sumsamples[:, 0]-normalization
+    ln_prob_s2 = lnposterior_sumsamples[:, 1]-normalization
+    return ln_prob_s1, ln_prob_s2
+
+
+def get_log_posterior(bayes_n_nodes: int,
+                      waveforms: np.ndarray,
+                      cpt: np.ndarray,
+                      waveform_num_bin_edges: int,
+                      quantile_num_bin_edges: int,
+                      n_bayes_classes: int,
+                      waveform_values: np.ndarray,
+                      quantile_values: np.ndarray,
+                      ) -> np.ndarray:
+    """
+    # TODO, add a description what we are computing here
+
+    :param bayes_n_nodes: number of nodes or attributes
+    :param waveforms: waveforms
+    :param cpt: conditional probability tables
+    :param waveform_num_bin_edges: number of bins for waveforms
+    :param quantile_num_bin_edges:number of bins for quantiles
+    :param n_bayes_classes: number of classes
+    :param waveform_values: digitized waveforms
+    :param quantile_values: digitized quantiles
+    :return: log-posterior for waveforms and quantiles. NB! This is not
+        normalized
+    """
     wf_posterior = _get_log_posterior(
         nodes=bayes_n_nodes,
         n_bins=waveform_num_bin_edges-1,
@@ -160,16 +200,7 @@ def compute_inference(bins: int,
     lnposterior = np.zeros((len(waveforms), bayes_n_nodes*2, n_bayes_classes))
     lnposterior[:, :bayes_n_nodes] = wf_posterior
     lnposterior[:, bayes_n_nodes:] = quantile_posterior
-    lnposterior_sumsamples = np.sum(lnposterior, axis=1)
-    lnposterior_sumsamples = lnposterior_sumsamples + np.log(class_prior)
-
-    # If you need to numbafy, use
-    # github.com/XENONnT/straxen/blob/c06a611a5a4289c65b9830fc1e68027149b6a5fd/straxen/plugins/bayes_peak_classification.py#L119  # noqa
-    normalization = logsumexp(lnposterior_sumsamples, axis=1)
-    ln_prob_s1 = lnposterior_sumsamples[:, 0]-normalization
-    ln_prob_s2 = lnposterior_sumsamples[:, 1]-normalization
-    return ln_prob_s1, ln_prob_s2
-
+    return lnposterior
 
 @numba.njit
 def _get_log_posterior(nodes, n_bins, n_classes, cpt, wf_len, wf_values):
@@ -179,14 +210,3 @@ def _get_log_posterior(nodes, n_bins, n_classes, cpt, wf_len, wf_values):
         distribution = cpt[i, :n_bins, :]
         ln_posterior[:, i, :] = np.log(distribution[wf_values[:, i], :])
     return ln_posterior
-
-
-@numba.njit
-def _set_2d_to_zero_or_max_val(values, min_val, max_val):
-    """Clipping between min_val and max_val for 2d array"""
-    for k, w in enumerate(values):
-        for kk, ww in enumerate(w):
-            if ww < min_val:
-                values[k][kk] = min_val
-            if ww > max_val:
-                values[k][kk] = max_val
