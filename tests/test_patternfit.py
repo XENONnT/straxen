26c9682fdc93c4784be8519c768f2d2630c6ebc4
Giovanni Volta <38431109+GiovanniVolta@users.noreply.github.com>
2022-04-29 08:38:38 +0200

    Fixing binomial (#991)
    * dixing binomial test
    
    * fix type and add s1 n channel in event area per channel
    
    * handle k == mode differently
    
    * Update event_patternfit.py
    
    * Update event_patternfit.py
    
    * add test
    
    * add test
    
    * add line
    
    * define given
    
    * I dont really get given decoration
    
    * add check of PDF inversion
    
    * simple test of binomial computation at integer values
    
    * Loosen tolerance
    
    * Loosen tolerance
    
    * loosing test and return 1
    
    * trying to make the test working
    
    * I think the tests are fine now
    
    Co-authored-by: GiovanniVolta <gvolta@dali-login1.rcc.local>
    Co-authored-by: Joran R. Angevaare <jorana@nikhef.nl>
    Co-authored-by: Knut Dundas Morå <knutdundasmoraa@gmail.com>
    Co-authored-by: Knut Dundas Morå <kdm2160@columbia.edu>
    Co-authored-by: Shingo Kazama <kazama@isee.nagoya-u.ac.jp>

---
+++ tests/test_patternfit.py
@@ -0,0 +1,72 @@
+import unittest
+from hypothesis import strategies, given, settings, example
+from straxen.plugins.event_patternfit import binom_test, lbinom_pmf_mode, lbinom_pmf, lbinom_pmf_inverse, binom_sf
+import numpy as np
+import scipy.stats as sps
+
+@settings(deadline=None)
+#AFT_observed, AFT_expected (from xyz), S1_total
+@given(strategies.floats(0., 1.), strategies.floats(0.01, 0.99), strategies.floats(2., 1000))
+#@example(0., 0.05, 10.) 
+def test_patternfit_stats(aftobs, aft, s1tot):
+    s1top = aftobs*s1tot
+    assert( (binom_test(s1top, s1tot, aft) >= 0) & (binom_test(s1top, s1tot, aft) <= 1) )
+
+@settings(deadline=None)
+#AFT_observed, AFT_expected (from xyz), S1_total
+@given(strategies.floats(0., 1.), strategies.floats(0.01, 0.99), strategies.floats(2., 1000))
+#@example(0., 0.05, 10.) 
+def test_inverse_pdf(aftobs, aft, s1tot):
+    s1top = aftobs * s1tot
+    #code stolen from binom_test, checking if the other point on
+
+    k = s1top
+    n = s1tot
+    p = aft
+
+    mode = lbinom_pmf_mode(0, n, 0, (n, p))
+    distance = abs(mode - k)
+    target = lbinom_pmf(k, n, p)
+
+    if k < mode: 
+        j_min = mode
+        j_max = min(mode + 2.0 * distance, n)
+        j = lbinom_pmf_inverse(j_min, j_max, target, (n, p))
+    else:
+        j_min = max(mode - 2.0 * distance, 0)
+        j_max = mode
+        j = lbinom_pmf_inverse(j_min, j_max, target, (n, p))  
+    if np.isnan(j) or j == n:
+        #if no other crossing is found, we cannot check if the two give same pdf
+        assert(True)
+    else:
+        pdf_value = lbinom_pmf(j, n, p)
+        #values in log, it is not _really_ crucial that they're precisely equal
+        np.testing.assert_almost_equal(pdf_value, target, decimal=2) 
+
+@settings(deadline=None)
+@strategies.composite 
+def n_and_k(draw):
+    n = draw(strategies.integers(2, 1000))
+    k = draw(strategies.integers(0, n))
+    return n,k
+
+@settings(deadline=None)
+@given(nk = n_and_k(), p = strategies.floats(0.01, 0.99))
+@example((10, 5), 0.5)
+def test_pmf(nk, p):
+    #test that at integer n, k binom_pmf agrees with the scipy result
+    n, k = nk
+    sps_lpmf = sps.binom(n, p).logpmf(k)
+    straxen_lpmf = lbinom_pmf(k, n, p)
+    np.testing.assert_almost_equal(straxen_lpmf, sps_lpmf, decimal=2)
+
+@settings(deadline=None)
+@given(nk = n_and_k(), p = strategies.floats(0.01, 0.99))
+#@example((10, 5), 0.5)
+def test_pvalue(nk, p):
+    #test that at integer n, k binom_pmf agrees with the scipy result
+    n, k = nk
+    sps_pval = sps.binom(n, p).sf(k + 0.1)
+    straxen_pval = binom_sf(k, n, p)
+    np.testing.assert_almost_equal(straxen_pval, sps_pval, decimal=2)
