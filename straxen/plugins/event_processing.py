60c6ba343326179d6a667b1f1364c9208c8b7982
Jianyu Long <jylong@uchicago.edu>
2022-05-05 10:04:04 -0500

    fixed bug

--- straxen/plugins/event_processing.py
+++ straxen/plugins/event_processing.py
@@ -713,7 +713,7 @@ class CorrectedAreas(strax.Plugin):
                       (f'{peak_type}cs2', np.float32, f'Corrected area of {peak_name} S2 [PE]'), ]
         return dtype
 
-   ''' @staticmethod
+    ''' @staticmethod
     def rotate(x_arr, y_arr, theta):
         new_x = np.cos(theta)*x_arr+np.sin(theta)*y_arr
         new_y = -np.sin(theta)*x_arr+np.cos(theta)*y_arr
