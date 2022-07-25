a0318776511e4e00d6964db73568446bd64f776a
Matteo Guida <matteo12guida@gmail.com>
2022-07-25 14:37:43 +0200

    S1 posrec plugin update

--- straxen/plugins/s1_position_reconstruction.py
+++ straxen/plugins/s1_position_reconstruction.py
@@ -1,4 +1,9 @@
-"""S1-based 3D position reconstruction for Xenon-nT"""
+""" 
+   Author and Maintainer: Matteo Guida (guidam@mpi-hd.mpg.de).
+   Purpose: Novel S1-based 3D position reconstruction for Xenon-nT, based on networks trained on data top and bottom S1 PMT hit patterns, considering as "true" position the S2-based reconstructed and corrected positions. In fact, the S1-based approach cannot exceed a resolution of a couple of cm and this allows to train the networks in a data-driven way. For more indormation, please see (https://zenodo.org/record/6347581#.Yt5jeHZBy3A).
+"""
+
+
 
 import os
 import tempfile
@@ -7,33 +12,33 @@ import numpy as np
 import strax
 import straxen
 from warnings import warn
-
 export, __all__ = strax.exporter()
 
-# Current default algorithm: data-driven CNN.
+# Current default algorithm: data-driven CNN. 
 DEFAULT_S1POSREC_ALGO_OPTION = tuple([strax.Option("default_s1reconstruction_algorithm",
-                                                   help="default S1 reconstruction algorithm that provides (x,y,z)",
-                                                   default="cnn_s1",
-                                                   )])
-
+                 help="default S1 reconstruction algorithm that provides (x,y,z)",
+                 default="cnn_s1",
+                 )])
 
 @export
 @strax.takes_config(
-    strax.Option("s1_cnn_model_path",
-                 help="Path to the S1 CNN model",
-                 default="/project2/lgrandi/guidam/CNN_S1_XYZ_SAVED_MODELS/version_datadriven_00_080921"
-                 ),
-    strax.Option('min_reconstruction_area',
+    strax.Option("s1_cnn_model_path", 
+                     help="Path to the S1 CNN model", 
+                     default=("/project2/lgrandi/guidam/CNN_S1_XYZ_SAVED_MODELS/version_datadriven_00_080921")
+                    ),
+    strax.Option('min_s1_area_s1_posrec',
                  help='Skip reconstruction if area (PE) is less than this',
                  default=1000),
 )
+
+
 class S1EventPositionBase(strax.Plugin):
     """
     Base class for S1 three-dimensional position reconstruction.
     This class should only be used when subclassed for the different algorithms. 
     """
 
-    depends_on = ('events', 'event_area_per_channel',)
+    depends_on = ('events','event_area_per_channel',)
     algorithm = None
     compressor = 'zstd'
     parallel = True
@@ -44,12 +49,13 @@ class S1EventPositionBase(strax.Plugin):
             raise NotImplementedError(f'Base class should not be used without '
                                       f'algorithm as done in {__class__.__name__}')
         dtype = [('x_' + self.algorithm, np.float32,
-                  f'Reconstructed {self.algorithm} S1 X position (cm)'),
+                  f'Reconstructed {self.algorithm} S1 X position (cm), uncorrected'),
                  ('y_' + self.algorithm, np.float32,
-                  f'Reconstructed {self.algorithm} S1 Y position (cm)'),
+                  f'Reconstructed {self.algorithm} S1 Y position (cm), uncorrected'),
                  ('z_' + self.algorithm, np.float32,
-                  f'Reconstructed {self.algorithm} S1 Z position (cm)')
-                 ]
+                  f'Reconstructed {self.algorithm} S1 Z position (cm), uncorrected')
+                  ]
+
         dtype += strax.time_fields
         return dtype
 
@@ -73,15 +79,16 @@ class S1EventPositionBase(strax.Plugin):
             except straxen.mongo_storage.CouldNotLoadError as e:
                 raise RuntimeError(f'Model files {self.model_file} is not found') from e
         with tempfile.TemporaryDirectory() as tmpdirname:
-            # Allow to load the model both from folder or from file.
-            if tarfile.is_tarfile(self.model_file):
+        # Allow to load the model both from folder or from file.
+            try:
                 tar = tarfile.open(self.model_file, mode="r:gz")
                 tar.extractall(path=tmpdirname)
                 self.model = tf.keras.models.load_model(tmpdirname)
-            else:
+            except Exception as e:
                 self.model = tf.keras.models.load_model(self.model_file)
 
-    def compute(self, events):
+
+    def compute(self,events):
         result = np.ones(len(events), dtype=self.dtype)
         result['time'], result['endtime'] = events['time'], strax.endtime(events)
 
@@ -93,21 +100,24 @@ class S1EventPositionBase(strax.Plugin):
             # This plugin is disabled since no model is provided
             return result
 
-        # Keep large peaks only
-        event_mask = events['s1_area'] > self.config['min_reconstruction_area']
+        # Reconstruct position only for large peaks, otherwise severe inaccuracy.
+        event_mask = events['s1_area_per_channel'].sum(axis=1) > self.config['min_s1_area_s1_posrec']
+        
         if not np.sum(event_mask):
-            # Nothing to do, and .predict crashes on empty arrays
+            # No peaks fulfilling the conditions, return nan array.
             return result
 
-        # Getting actual position reconstruction
         _in = events['s1_area_per_channel'][event_mask]
+        
         with np.errstate(divide='ignore', invalid='ignore'):
-            # Normalise patters by dividing by largest PMT output between the two arrays.
-            _in = _in / _in.max(axis=1, keepdims=True)
+        # Normalise patters by dividing by largest PMT output between the two arrays. 
+            _in = _in / _in.max(axis=1,keepdims=True)
+            
+        # Getting actual position reconstruction
         _out = self.model.predict(_in)
 
         # writing output to the result
-
+        
         result['x_' + self.algorithm][event_mask] = _out[:, 0]
         result['y_' + self.algorithm][event_mask] = _out[:, 1]
         result['z_' + self.algorithm][event_mask] = _out[:, 2]
@@ -131,37 +141,21 @@ class S1EventPositionBase(strax.Plugin):
         model_file = straxen.get_config_from_cmt(self.run_id, model_from_config)
         return model_file
 
-
 @export
 @strax.takes_config(
     strax.Option('cnn_s1_model',
-                 help='Neural network model.'
+                 help='Neural network model.' 
                       'If CMT, specify as (CMT_model, (cnn_s1_model, ONLINE), True)))'
                       'Set to None to skip the computation of this plugin.',
-                 default="/project2/lgrandi/guidam/CNN_S1_XYZ_SAVED_MODELS/version00_020521"
-                 #                  default=("CMT_model", ('cnn_s1_model', "None"), True)
-                 )
+                default="/project2/lgrandi/guidam/CNN_S1_XYZ_SAVED_MODELS/version00_020521"
+#                  default=("CMT_model", ('cnn_s1_model', "None"), True)
+                )
 )
+
 class S1EventPositionCNN(S1EventPositionBase):
     """Convolutional Neural Network (CNN) neural net for S1 (x,y,z) position reconstruction"""
     provides = "s1_event_positions_cnn"
     algorithm = "cnn_s1"
-    __version__ = '0.0.1'
-
-
-@export
-@strax.takes_config(
-    strax.Option('mlp_s1_model',
-                 help='Neural network model.'
-                      'If CMT, specify as (CMT_model, (mlp_s1_model, ONLINE), True)))'
-                      'Set to None to skip the computation of this plugin.',
-                 default='/dali/lgrandi/jgrigat/mlp_s1_models_data_based/v00_211004'
-                 )
-)
-class PeakPositionsMLP(PeakPositionsBaseNT):
-    """Multilayer Perceptron (MLP) neural net for S1 (x,y,z) position reconstruction"""
-    provides = "s1_event_positions_mlp"
-    algorithm = "mlp_s1"
     __version__ = '0.0.1'
 
 
@@ -182,7 +176,7 @@ class S1EventPosition(strax.MergeOnlyPlugin):
     input plugins, don't save this plugins output.
     """
     provides = "s1_event_positions"
-    depends_on = ("s1_event_positions_cnn", "s1_event_positions_mlp")
+    depends_on = ("s1_event_positions_cnn")
     save_when = strax.SaveWhen.NEVER
     __version__ = '0.0.0'
 
