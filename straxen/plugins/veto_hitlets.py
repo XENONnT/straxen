4eb866cf16658f6b17d07c93aafbb167955e6aa0
Yossi Mosbacher <joe.mosbacher>
2022-09-14 15:23:26 +0300

    veto hitlets and pule_processing

--- straxen/plugins/veto_hitlets.py
+++ straxen/plugins/veto_hitlets.py
@@ -4,42 +4,12 @@ from immutabledict import immutabledict
 import strax
 import straxen
 
-from straxen.get_corrections import is_cmt_option
-from straxen.plugins.veto_pulse_processing import MV_PREAMBLE, NV_HIT_OPTIONS
+from straxen.plugins.veto_pulse_processing import MV_PREAMBLE, NV_HIT_DEFAULTS, MV_HIT_DEFAULTS
 
 export, __all__ = strax.exporter()
 
 
 @export
-@strax.takes_config(
-    *NV_HIT_OPTIONS,
-    strax.Option(
-        'min_split_nv',
-        default=0.063, track=True, infer_type=False,
-        help='Minimum height difference pe/sample between local minimum and maximum, '
-             'that a pulse get split.'),
-    strax.Option(
-        'min_split_ratio_nv',
-        default=0.75, track=True, infer_type=False,
-        help='Min ratio between local maximum and minimum to split pulse (zero to switch this '
-             'off).'),
-    strax.Option(
-        'entropy_template_nv',
-        default='flat', track=True, infer_type=False,
-        help='Template data is compared with in conditional entropy. Can be either "flat" or an '
-             'template array.'),
-    strax.Option(
-        'entropy_square_data_nv',
-        default=False, track=True, infer_type=False,
-        help='Parameter which decides if data is first squared before normalized and compared to '
-             'the template.'),
-    strax.Option('channel_map', track=False, type=immutabledict,
-                 help="immutabledict mapping subdetector to (min, max) "
-                      "channel number."),
-    strax.Option('gain_model_nv',
-                 default=("to_pe_model_nv", "ONLINE", True), infer_type=False,
-                 help='PMT gain model. Specify as (model_type, model_config, nT = True)'),
-)
 class nVETOHitlets(strax.Plugin):
     """
     Plugin which computes the nveto hitlets and their parameters.
@@ -70,12 +40,53 @@ class nVETOHitlets(strax.Plugin):
 
     dtype = strax.hitlet_dtype()
 
+    save_outside_hits_nv = straxen.URLConfig(
+        default=NV_HIT_DEFAULTS['save_outside_hits_nv'], track=True, infer_type=False,
+        help='Save (left, right) samples besides hits; cut the rest')
+
+    hit_min_amplitude_nv = straxen.URLConfig(
+        infer_type=False,
+        default=NV_HIT_DEFAULTS['hit_min_amplitude_nv'], track=True,
+        help='Minimum hit amplitude in ADC counts above baseline. '
+             'Specify as a tuple of length n_nveto_pmts, or a number, '
+             'or a string like "pmt_commissioning_initial" which means calling '
+             'hitfinder_thresholds.py, '
+             'or a tuple like (correction=str, version=str, nT=boolean), '
+             'which means we are using cmt.')
+
+    min_split_nv = straxen.URLConfig(
+        default=0.063, track=True, infer_type=False,
+        help='Minimum height difference pe/sample between local minimum and maximum, '
+             'that a pulse get split.')
+
+    min_split_ratio_nv = straxen.URLConfig(
+        default=0.75, track=True, infer_type=False,
+        help='Min ratio between local maximum and minimum to split pulse (zero to switch this '
+             'off).')
+
+    entropy_template_nv = straxen.URLConfig(
+        default='flat', track=True, infer_type=False,
+        help='Template data is compared with in conditional entropy. Can be either "flat" or an '
+             'template array.')
+
+    entropy_square_data_nv = straxen.URLConfig(
+        default=False, track=True, infer_type=False,
+        help='Parameter which decides if data is first squared before normalized and compared to '
+             'the template.')
+
+    channel_map = straxen.URLConfig(track=False, type=immutabledict,
+                 help="immutabledict mapping subdetector to (min, max) "
+                      "channel number.")
+
+    gain_model_nv = straxen.URLConfig(
+                 default="cmt://to_pe_model_nv?version=ONLINE", infer_type=False,
+                 help='PMT gain model. Specify as (model_type, model_config, nT = True)')
+
     def setup(self):
-        self.channel_range = self.config['channel_map']['nveto']
+        self.channel_range = self.channel_map['nveto']
         self.n_channel = (self.channel_range[1] - self.channel_range[0]) + 1
 
-        to_pe = straxen.get_correction_from_cmt(self.run_id,
-                                                self.config['gain_model_nv'])
+        to_pe = self.gain_model_nv
 
         # Create to_pe array of size max channel:
         self.to_pe = np.zeros(self.channel_range[1] + 1, dtype=np.float32)
@@ -83,22 +94,14 @@ class nVETOHitlets(strax.Plugin):
 
         # Check config of `hit_min_amplitude_nv` and define hit thresholds
         # if cmt config
-        if is_cmt_option(self.config['hit_min_amplitude_nv']):
-            self.hit_thresholds = straxen.get_correction_from_cmt(
-                self.run_id, self.config['hit_min_amplitude_nv'])
-        # if hitfinder_thresholds config
-        elif isinstance(self.config['hit_min_amplitude_nv'], str):
-            self.hit_thresholds = straxen.hit_min_amplitude(
-                self.config['hit_min_amplitude_nv'])
-        else:  # int or array
-            self.hit_thresholds = self.config['hit_min_amplitude_nv']
-
+        self.hit_thresholds = self.hit_min_amplitude_nv
+        
     def compute(self, records_nv, start, end):
         records_nv = remove_switched_off_channels(records_nv, self.to_pe)
         hits = strax.find_hits(records_nv, min_amplitude=self.hit_thresholds)
 
         temp_hitlets = strax.create_hitlets_from_hits(hits,
-                                                      self.config['save_outside_hits_nv'],
+                                                      self.save_outside_hits_nv,
                                                       self.channel_range,
                                                       chunk_start=start,
                                                       chunk_end=end)
@@ -117,8 +120,8 @@ class nVETOHitlets(strax.Plugin):
                                          self.to_pe,
                                          data_type='hitlets',
                                          algorithm='local_minimum',
-                                         min_height=self.config['min_split_nv'],
-                                         min_ratio=self.config['min_split_ratio_nv']
+                                         min_height=self.min_split_nv,
+                                         min_ratio=self.min_split_ratio_nv,
                                          )
 
         # Compute other hitlet properties:
@@ -146,50 +149,6 @@ def remove_switched_off_channels(hits, to_pe):
 
 
 @export
-@strax.takes_config(
-    strax.Option(
-        'save_outside_hits_mv',
-        default=(2, 5), track=True, infer_type=False,
-        child_option=True, parent_option_name='save_outside_hits_nv',
-        help='Save (left, right) samples besides hits; cut the rest'),
-    strax.Option(
-        'hit_min_amplitude_mv', infer_type=False,
-        default=('hit_thresholds_mv', 'ONLINE', True), track=True,
-        help='Minimum hit amplitude in ADC counts above baseline. '
-             'Specify as a tuple of length n_mveto_pmts, or a number, '
-             'or a string like "pmt_commissioning_initial" which means calling '
-             'hitfinder_thresholds.py, '
-             'or a tuple like (correction=str, version=str, nT=boolean),'
-             'which means we are using cmt.'),
-    strax.Option(
-        'min_split_mv',
-        default=100, track=True, infer_type=False,
-        child_option=True, parent_option_name='min_split_nv',
-        help='Minimum height difference pe/sample between local minimum and maximum, '
-             'that a pulse get split.'),
-    strax.Option(
-        'min_split_ratio_mv',
-        default=0, track=True, infer_type=False,
-        child_option=True, parent_option_name='min_split_ratio_nv',
-        help='Min ratio between local maximum and minimum to split pulse (zero to switch this '
-             'off).'),
-    strax.Option(
-        'entropy_template_mv',
-        default='flat', track=True, infer_type=False,
-        child_option=True, parent_option_name='entropy_template_nv',
-        help='Template data is compared with in conditional entropy. Can be either "flat" or a '
-             'template array.'),
-    strax.Option(
-        'entropy_square_data_mv',
-        default=False, track=True, infer_type=False,
-        child_option=True, parent_option_name='entropy_square_data_nv',
-        help='Parameter which decides if data is first squared before normalized and compared to '
-             'the template.'),
-    strax.Option('gain_model_mv',
-                 default=("to_pe_model_mv", "ONLINE", True), infer_type=False,
-                 child_option=True, parent_option_name='gain_model_nv',
-                 help='PMT gain model. Specify as (model_type, model_config)'),
-)
 class muVETOHitlets(nVETOHitlets):
     __doc__ = MV_PREAMBLE + nVETOHitlets.__doc__
     __version__ = '0.0.2'
@@ -201,29 +160,60 @@ class muVETOHitlets(nVETOHitlets):
 
     dtype = strax.hitlet_dtype()
 
+    save_outside_hits_mv = straxen.URLConfig(
+        default=MV_HIT_DEFAULTS['save_outside_hits_mv'], track=True, infer_type=False,
+        child_option=True, parent_option_name='save_outside_hits_nv',
+        help='Save (left, right) samples besides hits; cut the rest')
+
+    hit_min_amplitude_mv = straxen.URLConfig(
+        infer_type=False,
+        default=MV_HIT_DEFAULTS['hit_min_amplitude_mv'], track=True,
+        help='Minimum hit amplitude in ADC counts above baseline. '
+             'Specify as a tuple of length n_mveto_pmts, or a number, '
+             'or a string like "pmt_commissioning_initial" which means calling '
+             'hitfinder_thresholds.py, '
+             'or a tuple like (correction=str, version=str, nT=boolean),'
+             'which means we are using cmt.')
+
+    min_split_mv = straxen.URLConfig(
+        default=100, track=True, infer_type=False,
+        child_option=True, parent_option_name='min_split_nv',
+        help='Minimum height difference pe/sample between local minimum and maximum, '
+             'that a pulse get split.')
+
+    min_split_ratio_mv = straxen.URLConfig(
+        default=0, track=True, infer_type=False,
+        child_option=True, parent_option_name='min_split_ratio_nv',
+        help='Min ratio between local maximum and minimum to split pulse (zero to switch this '
+             'off).')
+
+    entropy_template_mv = straxen.URLConfig(
+        default='flat', track=True, infer_type=False,
+        child_option=True, parent_option_name='entropy_template_nv',
+        help='Template data is compared with in conditional entropy. Can be either "flat" or a '
+             'template array.')
+
+    entropy_square_data_mv = straxen.URLConfig(
+        default=False, track=True, infer_type=False,
+        child_option=True, parent_option_name='entropy_square_data_nv',
+        help='Parameter which decides if data is first squared before normalized and compared to '
+             'the template.')
+
+    gain_model_mv = straxen.URLConfig(
+                 default="cmt://to_pe_model_mv?version=ONLINE",infer_type=False,
+                 child_option=True, parent_option_name='gain_model_nv',
+                 help='PMT gain model. Specify as (model_type, model_config)')
+
     def setup(self):
-        self.channel_range = self.config['channel_map']['mv']
+        self.channel_range = self.channel_map['mv']
         self.n_channel = (self.channel_range[1] - self.channel_range[0]) + 1
 
-        to_pe = straxen.get_correction_from_cmt(
-            self.run_id, self.config['gain_model_mv'])
-
+        to_pe = self.gain_model_mv
         # Create to_pe array of size max channel:
         self.to_pe = np.zeros(self.channel_range[1] + 1, dtype=np.float32)
         self.to_pe[self.channel_range[0]:] = to_pe[:]
 
-        # Check config of `hit_min_amplitude_mv` and define hit thresholds
-        # if cmt config
-        if is_cmt_option(self.config['hit_min_amplitude_mv']):
-            self.hit_thresholds = straxen.get_correction_from_cmt(self.run_id,
-                                                                  self.config[
-                                                                      'hit_min_amplitude_mv'])
-        # if hitfinder_thresholds config
-        elif isinstance(self.config['hit_min_amplitude_mv'], str):
-            self.hit_thresholds = straxen.hit_min_amplitude(
-                self.config['hit_min_amplitude_mv'])
-        else:  # int or array
-            self.hit_thresholds = self.config['hit_min_amplitude_mv']
+        self.hit_thresholds = self.hit_min_amplitude_mv
 
     def compute(self, records_mv, start, end):
         return super().compute(records_mv, start, end)
