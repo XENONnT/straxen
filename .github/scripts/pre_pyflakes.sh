8c5e77509e7d93d44bedb53737a7ed463c5adb20
Joran Angevaare <j.angevaare@nikhef.nl>
2022-09-26 12:04:38 +0200

    Revert "fix it more"
    This reverts commit d181deb793628f1d4730fcf4d10707d20c192d19.

--- .github/scripts/pre_pyflakes.sh
+++ .github/scripts/pre_pyflakes.sh
@@ -2,11 +2,6 @@
 # Pyflakes does not like the way we do __all__ += []. This simple script
 # Changes all the files in straxen to abide by this convention and
 # removes the lines that have such a signature.
-
-# Fixes 
-# AttributeError: 'Binding' object has no attribute 'names'
-# "pyflakes" failed during execution due to "'Binding' object has no attribute 'names'"
-# Run flake8 with greater verbosity to see more details
 start="$(pwd)"
 echo $start
 
@@ -17,9 +12,6 @@ cd plugins
 sed -e '/__all__ +=/ s/^#*/#/' -i ./*.py
 
 cd ../analyses
-sed -e '/__all__ +=/ s/^#*/#/' -i ./*.py
-
-cd ../storage
 sed -e '/__all__ +=/ s/^#*/#/' -i ./*.py
 
 cd $start
