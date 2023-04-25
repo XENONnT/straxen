1acdebd9a4bf15f4a84e7211dfe189a99667f8ca
dachengxu <dx2227@columbia.edu>
2023-04-25 15:31:49 -0500

    Bump the version of peaklets
    Following: https://github.com/AxFoundation/strax/pull/716

--- straxen/plugins/peaklets/peaklets.py
+++ straxen/plugins/peaklets/peaklets.py
@@ -37,7 +37,7 @@ class Peaklets(strax.Plugin):
     parallel = 'process'
     compressor = 'zstd'
 
-    __version__ = '1.0.1'
+    __version__ = '1.1.0'
 
     peaklet_gap_threshold = straxen.URLConfig(
         default=700, infer_type=False,
