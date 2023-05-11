0aeb68347071e61f4e88b628dec62fbb6f3334f5
micha weiss <michaweiss89@gmail.com>
2023-05-11 18:47:43 +0300

    Update multi scatter Ignore nan in the sum of peaks. (#1162)
    * Update multi scatter Ignore nan in the sum of peaks.
    
    * Fix cS2 AFT for MS events and add average x, y, z
    
    * Slim code style
    
    * Add Multiplicity calculation
    
    * Update plugins version and names
    
    * Update the unit in event_basics
    
    * Update test
    
    * Add comments to ms variables
    
    * Add Docstring to PeakCorrectedAreas plugin
    
    * Fix bug
    
    * Minor change
    
    ---------
    
    Co-authored-by: Henning Schulze Eißing <h_schu55@uni-muenster.de>
    Co-authored-by: Dacheng Xu <dx2227@columbia.edu>

--- straxen/plugins/peaks/peak_per_event.py
+++ straxen/plugins/peaks/peak_per_event.py
@@ -12,31 +12,29 @@ class EventPeaks(strax.Plugin):
     Link - https://xe1t-wiki.lngs.infn.it/doku.php?id=weiss:analysis:ms_plugin
     """
     __version__ = '0.0.1'
-    depends_on = ('event_basics', 'peak_basics','peak_positions')
-    provides = 'peaks_per_event'
+    depends_on = ('event_basics', 'peak_basics', 'peak_positions')
+    provides = 'peak_per_event'
     data_kind = 'peaks'
+    save_when = strax.SaveWhen.TARGET
 
     def infer_dtype(self):
-        dtype = [
+        dtype = strax.time_fields + [
             ('drift_time', np.float32, 'Drift time between main S1 and S2 in ns'),
             ('event_number', np.int64, 'Event number in this dataset'),
-             ]
-        dtype += strax.time_fields
+        ]
         return dtype
 
-    save_when = strax.SaveWhen.TARGET
- 
     def compute(self, events, peaks):
         split_peaks = strax.split_by_containment(peaks, events)
         split_peaks_ind = strax.fully_contained_in(peaks, events)
         result = np.zeros(len(peaks), self.dtype)
         result.fill(np.nan)
-   
-         #1. Assign peaks features to main S1 and main S2 in the event
+
+        # Assign peaks features to main S1 and main S2 in the event
         for event_i, (event, sp) in enumerate(zip(events, split_peaks)):
-            result[f'drift_time'][(split_peaks_ind==event_i)] = sp[f'center_time']-event[f's1_center_time']
-        result[f'event_number']= split_peaks_ind
-        result[f'drift_time'][peaks["type"]!=2]= np.nan
+            result['drift_time'][split_peaks_ind==event_i] = sp['center_time'] - event['s1_center_time']
+        result['event_number'] = split_peaks_ind
+        result['drift_time'][peaks['type'] != 2] = np.nan
         result['time'] = peaks['time']
         result['endtime'] = strax.endtime(peaks)
         return result
