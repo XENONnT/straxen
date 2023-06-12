da0e195c971bf3085b85cfd1eb538d7b4ccde189
Min Zhong <minzhong@midway2-login2.rcc.local>
2023-06-12 16:49:49 -0500

    Change to regular Plugin, remove s1_bottom_n_channels.

--- straxen/plugins/events/event_area_per_channel.py
+++ straxen/plugins/events/event_area_per_channel.py
@@ -6,14 +6,14 @@ export, __all__ = strax.exporter()
 
 
 @export
-class EventAreaPerChannel(strax.LoopPlugin):
+class EventAreaPerChannel(strax.Plugin):
     """
     Simple plugin that provides area per channel for main and alternative S1/S2 in the event.
     """
     
     depends_on = ('event_basics', 'peaks')
     provides = "event_area_per_channel"
-    __version__ = '0.0.3'
+    __version__ = '0.1.0'
 
     compressor = 'zstd'
     save_when = strax.SaveWhen.EXPLICIT
@@ -44,28 +44,28 @@ class EventAreaPerChannel(strax.LoopPlugin):
                   np.int16),
                  (("Main S1 top count of contributing PMTs", "s1_top_n_channels"),
                   np.int16),
-                 (("Main S1 bottom count of contributing PMTs", "s1_bottom_n_channels"),
-                  np.int16),
                  ]
         dtype += strax.time_fields
         return dtype
 
-    def compute_loop(self, event, peaks):
-        result = {}
-        result['time'] = event['time']
-        result['endtime'] = strax.endtime(event)
+    def compute(self, events, peaks):
+        result = np.zeros(len(events), self.dtype)
+        result['time'] = events['time']
+        result['endtime'] = strax.endtime(events)
 
-        for type_ in ['s1', 's2', 'alt_s1', 'alt_s2']:
-            type_index = event[f'{type_}_index']
-            if type_index != -1:
-                type_area_per_channel = peaks['area_per_channel'][type_index]
-                result[f'{type_}_area_per_channel'] = type_area_per_channel
-                result[f'{type_}_length'] = peaks['length'][type_index]
-                result[f'{type_}_dt'] = peaks['dt'][type_index]
-                if type_ == 's1':
-                    result['s1_n_channels'] = len(type_area_per_channel[type_area_per_channel > 0])
-                    result['s1_top_n_channels'] = len(type_area_per_channel[:self.config['n_top_pmts']][
-                                                          type_area_per_channel[:self.config['n_top_pmts']] > 0])
-                    result['s1_bottom_n_channels'] = len(type_area_per_channel[self.config['n_top_pmts']:][
-                                                             type_area_per_channel[self.config['n_top_pmts']:] > 0])
+        split_peaks = strax.split_by_containment(peaks, events)
+        
+        for event_i, (event, sp) in enumerate(zip(events, split_peaks)):
+            for type_ in ['s1', 's2', 'alt_s1', 'alt_s2']:
+                type_index = event[f'{type_}_index']
+                if type_index != -1:
+                    type_area_per_channel = sp['area_per_channel'][type_index]
+                    result[f'{type_}_area_per_channel'][event_i] = type_area_per_channel
+                    result[f'{type_}_length'][event_i] = sp['length'][type_index]
+                    result[f'{type_}_dt'][event_i] = sp['dt'][type_index]
+                    if type_ == 's1':
+                        result['s1_n_channels'][event_i] = len(type_area_per_channel[type_area_per_channel > 0])
+                        result['s1_top_n_channels'][event_i] = len(type_area_per_channel[:self.config['n_top_pmts']][
+                                                                   type_area_per_channel[:self.config['n_top_pmts']] > 0])
+
         return result
