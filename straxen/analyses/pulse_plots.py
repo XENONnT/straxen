ad79e54ea4ee978bd199b92a567206ed096baa11
Daniel Wenz <dwenz@students.uni-mainz.de>
2023-08-11 08:16:35 -0500

    Add missing docstrings

--- straxen/analyses/pulse_plots.py
+++ straxen/analyses/pulse_plots.py
@@ -10,6 +10,28 @@ import straxen
 def plot_pulses_tpc(context, raw_records, run_id, time_range=None,
                     plot_hits=False, plot_median=False,
                     max_plots=20, store_pdf=False, path=''):
+    """
+    Mini-analyis to plot pulses for the specified list of records.
+    You have to provide a a run-id for which pulses should be plotted.
+    You can use the same arguments as for get_array to select a specific
+    time range or data (see also further ).
+
+    In addition you can provide the following arguments:
+    
+    :param plot_hits: If True plot hit boundaries including the left
+        and right extension as orange shaded regions.
+    :param plot_median: If true plots pulses sample median as dotted
+        line.
+    :param max_plots: Limits the number of figures. If you would like
+        to plot more pulses you should put the plots in a PDF.
+    :param store_pdf: If true figures are put to a PDF instead of
+        plotting them to your notebook. The file name is automatically
+        generated including the time range and run_id.
+    :param path: Relative path where the PDF should be stored. By default
+        it is the directory of the notebook.
+    :param detector_ending: Ending of the corresponding detector. Empty
+        string for TPC '_nv' for neutron-veto and '_mv' muon-veto. 
+    """
     plot_pulses(context, raw_records, run_id, time_range,
                 plot_hits, plot_median,
                 max_plots, store_pdf, path)
@@ -19,6 +41,28 @@ def plot_pulses_tpc(context, raw_records, run_id, time_range=None,
 def plot_pulses_mv(context, raw_records_mv, run_id, time_range=None,
                    plot_hits=False, plot_median=False,
                    max_plots=20, store_pdf=False, path=''):
+    """
+    Mini-analyis to plot pulses for the specified list of records.
+    You have to provide a a run-id for which pulses should be plotted.
+    You can use the same arguments as for get_array to select a specific
+    time range or data (see also further ).
+
+    In addition you can provide the following arguments:
+    
+    :param plot_hits: If True plot hit boundaries including the left
+        and right extension as orange shaded regions.
+    :param plot_median: If true plots pulses sample median as dotted
+        line.
+    :param max_plots: Limits the number of figures. If you would like
+        to plot more pulses you should put the plots in a PDF.
+    :param store_pdf: If true figures are put to a PDF instead of
+        plotting them to your notebook. The file name is automatically
+        generated including the time range and run_id.
+    :param path: Relative path where the PDF should be stored. By default
+        it is the directory of the notebook.
+    :param detector_ending: Ending of the corresponding detector. Empty
+        string for TPC '_nv' for neutron-veto and '_mv' muon-veto. 
+    """
     plot_pulses(context, raw_records_mv, run_id, time_range,
                 plot_hits, plot_median,
                 max_plots, store_pdf, path, detector_ending='_mv')
@@ -28,6 +72,28 @@ def plot_pulses_mv(context, raw_records_mv, run_id, time_range=None,
 def plot_pulses_nv(context, raw_records_nv, run_id, time_range=None,
                    plot_hits=False, plot_median=False,
                    max_plots=20, store_pdf=False, path=''):
+    """
+    Mini-analyis to plot pulses for the specified list of records.
+    You have to provide a a run-id for which pulses should be plotted.
+    You can use the same arguments as for get_array to select a specific
+    time range or data (see also further ).
+
+    In addition you can provide the following arguments:
+    
+    :param plot_hits: If True plot hit boundaries including the left
+        and right extension as orange shaded regions.
+    :param plot_median: If true plots pulses sample median as dotted
+        line.
+    :param max_plots: Limits the number of figures. If you would like
+        to plot more pulses you should put the plots in a PDF.
+    :param store_pdf: If true figures are put to a PDF instead of
+        plotting them to your notebook. The file name is automatically
+        generated including the time range and run_id.
+    :param path: Relative path where the PDF should be stored. By default
+        it is the directory of the notebook.
+    :param detector_ending: Ending of the corresponding detector. Empty
+        string for TPC '_nv' for neutron-veto and '_mv' muon-veto. 
+    """
     plot_pulses(context, raw_records_nv, run_id, time_range,
                 plot_hits, plot_median,
                 max_plots, store_pdf, path, detector_ending='_nv')
