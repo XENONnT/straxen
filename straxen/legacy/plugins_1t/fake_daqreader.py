72d46fd36c91367f8d32cf080e1339058a984367
WenzDaniel <dwenz@kabelmail.de>
2023-11-21 03:43:53 -0600

    Remove not needed import

--- straxen/legacy/plugins_1t/fake_daqreader.py
+++ straxen/legacy/plugins_1t/fake_daqreader.py
@@ -1,9 +1,6 @@
 from straxen.plugins.raw_records.daqreader import DAQReader
 from immutabledict import immutabledict
 
-import strax
-
-
 class Fake1TDAQReader(DAQReader):
     provides = ("raw_records", "raw_records_diagnostic", "raw_records_aqmon")
 
