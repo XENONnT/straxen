f23319dd37a23dddcc37156e62d112e36c8deafc
Daniel Wenz <43881800+WenzDaniel@users.noreply.github.com>
2023-12-12 09:43:17 +0100

    Pull out FakeDAQ to legacy plugins (#1292)

--- straxen/legacy/contexts_1t.py
+++ straxen/legacy/contexts_1t.py
@@ -2,6 +2,7 @@ from immutabledict import immutabledict
 import strax
 import straxen
 from straxen.common import pax_file
+from .plugins_1t.fake_daqreader import Fake1TDAQReader
 
 
 def get_x1t_context_config():
@@ -164,7 +165,7 @@ def fake_daq():
         ),
         **get_x1t_context_config(),
     )
-    st.register(straxen.Fake1TDAQReader)
+    st.register(Fake1TDAQReader)
     return st
 
 
