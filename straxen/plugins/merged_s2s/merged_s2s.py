8afd915dc68cc8fca3d1dd024968834c818dc4b4
WenzDaniel <dwenz@kabelmail.de>
2023-12-19 01:42:16 -0600

    Do not bump version to keep lineage

--- straxen/plugins/merged_s2s/merged_s2s.py
+++ straxen/plugins/merged_s2s/merged_s2s.py
@@ -16,7 +16,7 @@ class MergedS2s(strax.OverlapWindowPlugin):
     """Merge together peaklets if peak finding favours that they would form a single peak
     instead."""
 
-    __version__ = "1.1.0"
+    __version__ = "1.0.2"
 
     depends_on: Tuple[str, ...] = ("peaklets", "peaklet_classification", "lone_hits")
     data_kind = "merged_s2s"
