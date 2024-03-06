08930404525e632451f2546217254d23a06ba6a0
Dacheng Xu <dx2227@columbia.edu>
2024-03-06 23:18:56 +0800

    Minor change of indents (#1341)

--- straxen/url_config.py
+++ straxen/url_config.py
@@ -742,7 +742,7 @@ def alphabetize_url_kwargs(url: str):
         if url != URLConfig.format_url_kwargs(url) and WARN:
             warnings.warn(
                 "From straxen version 2.1.0 onward, URLConfig parameters"
-                "will be sorted alphabetically before being passed to the plugins,"
+                " will be sorted alphabetically before being passed to the plugins,"
                 " this will change the lineage hash for non-sorted URLs. To load"
                 " data processed with non-sorted URLs, you will need to use an"
                 " older version."
