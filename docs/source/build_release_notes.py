5945d83341f00ef2b5a7fa848df4e28a2f824c3f
dachengx <dx2227@columbia.edu>
2024-05-15 10:23:24 +0800

    Update documentation and fix bug

--- docs/source/build_release_notes.py
+++ docs/source/build_release_notes.py
@@ -5,6 +5,6 @@ from straxen.docs_utils import convert_release_notes
 if __name__ == "__main__":
     this_dir = os.path.dirname(os.path.realpath(__file__))
     notes = os.path.join(this_dir, "..", "..", "HISTORY.md")
-    target = os.path.join(this_dir, "release_notes.rst")
+    target = os.path.join(this_dir, "reference", "release_notes.rst")
     pull_url = "https://github.com/XENONnT/straxen/pull"
     convert_release_notes(notes, target, pull_url)
