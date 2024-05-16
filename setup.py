1312d952e853355cfdc1ad0fa026298a0bf809d9
Dacheng Xu <dx2227@columbia.edu>
2024-05-16 10:26:18 -0400

    Bump to v2.2.3 (#1383)
    * Update HISTORY.md
    
    * Bump version: 2.2.2 → 2.2.3

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open("HISTORY.md") as file:
 
 setuptools.setup(
     name="straxen",
-    version="2.2.2",
+    version="2.2.3",
     description="Streaming analysis for XENON",
     author="Straxen contributors, the XENON collaboration",
     url="https://github.com/XENONnT/straxen",
