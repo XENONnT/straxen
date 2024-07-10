81f14dc06401e75204bbb9d3168f53f5104b55ba
RoiFrankel <roi.frankel@weizmann.ac.il>
2024-07-10 17:02:17 +0300

    peak_per_event.py
    Comments were moved from code to description of PR

--- straxen/plugins/peaks/peak_per_event.py
+++ straxen/plugins/peaks/peak_per_event.py
@@ -37,29 +37,13 @@ class EventPeaks(strax.Plugin):
             result["drift_time"][split_peaks_ind == event_i] = (
                 sp["center_time"] - event["s1_center_time"]
             )
-        """
-        Start changed part
-        This section of the code ensures consistency between event numbers in peaks and events.
-
-        We start by sorting the indices of fully contained peaks. Then, we create a dictionary called 'mapping',
-        where keys represent event numbers in peak level data, and values represent event numbers in event level data.
-
-        The 'mapping' dictionary ensures that each event number at the peak level corresponds to the correct event
-        number at the event level. We then use 'corrected_split_peaks_ind' to update the event numbers of
-        peaks based on the 'mapping' dictionary. This process ensures that event numbers match between peaks and events.
-
-        The 'result['event_number']' array is updated with 'corrected_split_peaks_ind', ensuring that event numbers
-        in peaks are consistent with those in events, maintaining the original length and preserving -1 values for peaks
-        where event numbers are not applicable.
-        """
+## Start of new part
         sorted_indices_split_peaks_ind = np.argsort(
             split_peaks_ind
-        )  # Sort the indices of fully contained peaks for mapping
+        ) 
         mapping = {
             val: events["event_number"][i]
-            for val,  # Create a mapping dictionary to match event numbers in peaks
-            ##to event numbers in events
-            i in zip(
+            for val,i in zip(
                 np.unique(
                     split_peaks_ind[sorted_indices_split_peaks_ind][
                         split_peaks_ind[sorted_indices_split_peaks_ind] != -1
@@ -68,13 +52,13 @@ class EventPeaks(strax.Plugin):
                 range(len(events["event_number"])),
             )
         }
-        # Correct event numbers of peaks based on the mapping dictionary
+
         corrected_split_peaks_ind = np.array(
             [mapping[val] if val in mapping else val for val in split_peaks_ind]
-        )  ##  we run on all numbers in split_paeaks_ind and call them val
-        # Update the event_number field in the result array with corrected event numbers for peaks
+        ) 
+        
         result["event_number"] = corrected_split_peaks_ind
-        """End changed part."""
+## End of new part
         result["drift_time"][peaks["type"] != 2] = np.nan
         result["time"] = peaks["time"]
         result["endtime"] = strax.endtime(peaks)
