c284d441e12b81c085d420705e39e831e9dfb5e6
Juehang Qin <qinjuehang@rice.edu>
2024-08-02 10:21:12 -0500

    Minor fix to make sure mypy correctly infers type in jax URLConfig

--- straxen/config/protocols.py
+++ straxen/config/protocols.py
@@ -300,7 +300,7 @@ def open_jax_model(model_path: str, **kwargs):
         # Extract and read the serialized JAX object
         file_obj = f.extractfile(filename)
         if file_obj is not None:
-            serialized_jax_object = f.extractfile(filename).read()
+            serialized_jax_object = file_obj.read()
         else:
             raise ValueError("Model file is empty!")
     # Deserialize the JAX object and return its callable function
