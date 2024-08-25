1dadb67b35d124c795479ffcfba65af0981dafaf
dachengx <dx2227@columbia.edu>
2024-08-25 07:01:10 -0500

    Minor change

--- straxen/plugins/peaks/peak_per_event.py
+++ straxen/plugins/peaks/peak_per_event.py
@@ -8,9 +8,10 @@ export, __all__ = strax.exporter()
 
 @export
 class EventPeaks(strax.Plugin):
-    """
-    0.0.2 , Add event number for peaks and drift times of all s2 depending on the largest s1.
+    """Add event number for peaks and drift times of all s2 depending on the largest s1.
+
     Link - https://xe1t-wiki.lngs.infn.it/doku.php?id=weiss:analysis:ms_plugin
+
     """
 
     __version__ = "0.0.2"
