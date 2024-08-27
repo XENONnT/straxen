79d0161c2f9c1cd9244eaf98b50d0a0fb5329800
WenzDaniel <dwenz@kabelmail.de>
2024-08-27 07:07:51 -0500

    Remove not needed files

--- straxen/plugins/events_nv/__init__.py
+++ straxen/plugins/events_nv/__init__.py
@@ -4,8 +4,5 @@ from .event_positions_nv import *
 from . import events_nv
 from .events_nv import *
 
-from . import events_sync_nv
-from .events_sync_nv import *
-
 from . import event_waveform_nv
 from .event_waveform_nv import *
