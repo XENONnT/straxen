b18eea1438ea70bb1fd301c329c89374cf98efa0
yuema137 <3124558229@qq.com>
2024-10-04 15:36:04 -0500

    add docstring for the _get_rse function

--- straxen/storage/rucio_remote.py
+++ straxen/storage/rucio_remote.py
@@ -119,6 +119,23 @@ class RucioRemoteBackend(strax.FileSytemBackend):
         self.rses_only = strax.to_str_tuple(rses_only)
 
     def _get_rse(self, dset_did):
+        """
+        Determine the appropriate Rucio Storage Element (RSE) for a dataset.
+
+        Parameters:
+        -----------
+        dset_did : str
+            The dataset identifier.
+
+        Returns:
+        --------
+        str
+            The name of the selected RSE (selection based on the list of available RSEs).
+
+        Notes:
+        ------
+        Uses self.rses_only to filter available RSEs if set.
+        """
         rses = admix.rucio.get_rses(dset_did)
         rses = list(set(rses) & set(self.rses_only)) if self.rses_only else rses
         rse = admix.downloader.determine_rse(rses)
