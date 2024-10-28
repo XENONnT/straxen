0f8ce814cb35cc2e573a895a3c4453c46b7d8435
Giovanni Volta <38431109+GiovanniVolta@users.noreply.github.com>
2024-10-28 13:15:04 +0100

    add cehck of default comment

--- straxen/plugins/led_cal/led_calibration.py
+++ straxen/plugins/led_cal/led_calibration.py
@@ -56,6 +56,12 @@ class LEDCalibration(strax.Plugin):
         ),
     )
 
+    defualt_run_comments = straxen.URLConfig(
+        default=['auto, SC user: ', 'pulserScript: ', 'userComment: Weekly LED calibration'],
+        infer_type=False,
+        help=("List of default comments the automatic script for runs in PMT calibration. "),
+    )
+    
     noise_run_comments = straxen.URLConfig(
         default=["SPE_calibration_step0", "Gain_calibration_step3"],
         infer_type=False,
@@ -160,7 +166,7 @@ class LEDCalibration(strax.Plugin):
 
         """
 
-        self.is_led_on = is_the_led_on(self.run_doc, self.noise_run_comments)
+        self.is_led_on = is_the_led_on(self.run_doc, self.defualt_run_comments, self.noise_run_comments)
 
         mask = np.where(np.in1d(raw_records["channel"], self.channel_list))[0]
         raw_records_active_channels = raw_records[mask]
@@ -197,7 +203,7 @@ class LEDCalibration(strax.Plugin):
         return temp
 
 
-def is_the_led_on(run_doc, noise_run_comments):
+def is_the_led_on(run_doc, defualt_run_comments, noise_run_comments):
     """Utilizing the run database metadata to determine whether the run ID corresponds to LED on or
     LED off runs.
 
@@ -215,6 +221,11 @@ def is_the_led_on(run_doc, noise_run_comments):
         if all(key in doc for key in required_keys):
             # Check if 'comment' contains any of the noise run comments
             comment = doc["comment"]
+
+            # Check if the comment matches the expected pattern
+            if not all(x in comment for x in defualt_run_comments):
+                raise ValueError("The comment does not match the expected pattern.")
+                
             if any(noise_comment in comment for noise_comment in noise_run_comments):
                 return False
             else:
