ce1b02b3815aa73b423727636ad6658a1b4633a0
Shixiao Liang <liangsx@rice.edu>
2024-12-20 18:01:30 -0600

    add keras3 protocol and adjust posrec plugin

--- straxen/config/protocols.py
+++ straxen/config/protocols.py
@@ -114,7 +114,7 @@ def open_neural_net(model_path: str, custom_objects=None, **kwargs):
     with tempfile.TemporaryDirectory() as tmpdirname:
         tar = tarfile.open(model_path, mode="r:gz")
         tar.extractall(path=tmpdirname)
-        return tf.saved_model.load(tmpdirname)
+        return tf.keras.models.load_model(tmpdirname, custom_objects=custom_objects)
 
 
 @URLConfig.register("itp_dict")
@@ -309,3 +309,15 @@ def open_jax_model(model_path: str, **kwargs):
         serialized_jax_object = file_obj.read()
     # Deserialize the JAX object and return its callable function
     return export.deserialize(serialized_jax_object).call
+
+@URLConfig.register("keras3")
+def open_neural_net(model_path: str, custom_objects=None, **kwargs):
+    """Load a Keras model from a Keras file."""
+    # Nested import to reduce loading time of import straxen and it not
+    # base requirement
+    import tensorflow as tf
+
+    if not os.path.exists(model_path):
+        raise FileNotFoundError(f"No file at {model_path}")
+
+    return tf.keras.models.load_model(model_path, custom_objects=custom_objects)
