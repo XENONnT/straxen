0e0769be2445a65cbdd207e955c399e2ad03ec9d
dachengx <dx2227@columbia.edu>
2025-01-15 11:30:00 -0600

    Roll back

--- straxen/config/protocols.py
+++ straxen/config/protocols.py
@@ -113,6 +113,7 @@ def open_tf_neural_net(model_path: str, custom_objects=None, **kwargs):
 
     if not os.path.exists(model_path):
         raise FileNotFoundError(f"No file at {model_path}")
+
     with tempfile.TemporaryDirectory() as tmpdirname:
         tar = tarfile.open(model_path, mode="r:gz")
         tar.extractall(path=tmpdirname)
@@ -120,7 +121,7 @@ def open_tf_neural_net(model_path: str, custom_objects=None, **kwargs):
 
 
 @URLConfig.register("keras3")
-def open_keras_neural_net(model_path: str, **kwargs):
+def open_keras_neural_net(model_path: str, custom_objects=None, **kwargs):
     """Load a Keras model from a Keras file."""
     # Nested import to reduce loading time of import straxen and it not
     # base requirement
@@ -129,7 +130,7 @@ def open_keras_neural_net(model_path: str, **kwargs):
     if not os.path.exists(model_path):
         raise FileNotFoundError(f"No file at {model_path}")
 
-    return tf.saved_model.load(model_path)
+    return tf.keras.models.load_model(model_path, custom_objects=custom_objects)
 
 
 @URLConfig.register("itp_dict")
