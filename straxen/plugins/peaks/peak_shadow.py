0bd75f3caf33970a80117574dd1bb99584a5a687
chnlkx <lkx21@mails.tsinghua.edu.cn>
2025-01-15 01:14:33 -0600

    add setup

--- straxen/plugins/peaks/peak_shadow.py
+++ straxen/plugins/peaks/peak_shadow.py
@@ -51,6 +51,50 @@ class PeakShadow(strax.OverlapWindowPlugin):
         ),
     )
 
+    position_shadow_s2_area_limit = straxen.URLConfig(
+        default=5e4,
+        type=float,
+        track=True,
+        help="Max S2 area to implement this cut, LowER analysis upper limit [PE]",
+    )
+
+    position_shadow_ellipse_parameters_mode = straxen.URLConfig(
+        default={
+            "bkg": [-0.969, -2.485, 0.564, 0.766, 1.798],  # Used by bkg and ted runs
+            "ar37": [-0.969, -2.485, 0.564, 0.766, 1.798],
+            "radon": [-0.895, -2.226, 0.713, 0.818, 1.370],
+            "radon_hev": [-0.873, -2.192, 0.624, 0.795, 1.504],
+            "ambe": [-0.911, -1.947, 0.821, 1.047, 0.734],
+            "kr83m": [-0.862, -2.531, 0.652, 0.779, 1.922],
+            "ybe": [-0.961, -2.344, 0.796, 0.885, 1.534],
+            "rn222": [-0.940, -2.537, 0.719, 0.829, 1.612],
+        },
+        type=dict,
+        track=True,
+        help="cut parameters for the ellipse curve in different run mode",
+    )
+
+    position_shadow_straight_parameters_mode = straxen.URLConfig(
+        default={
+            "bkg": [-0.947, -4.231],  # Used by bkg and ted runs
+            "ar37": [-0.947, -4.231],
+            "radon": [-0.975, -3.388],
+            "radon_hev": [-0.868, -3.110],
+            "ambe": [-0.430, -1.993],
+            "kr83m": [-1.022, -4.195],
+            "ybe": [-0.801, -2.670],
+            "rn222": [-0.929, -4.009],
+        },
+        type=dict,
+        track=True,
+        help="cut parameters for the straight line in different run mode",
+    )
+
+    run_mode = straxen.URLConfig(
+        default="run_mode://plugin.run_id",
+        help="Run mode to be used for the cut. It can affect the parameters of the cut.",
+    )
+
     def get_window_size(self):
         return 10 * self.shadow_time_window_backward
 
@@ -132,6 +176,7 @@ class PeakShadow(strax.OverlapWindowPlugin):
             + s2_position_shadow_dtype
             + nearest_dtype
             + strax.time_fields
+            + [("type", np.int8, "Classification of the peak")]
         )
         return dtype
 
@@ -142,6 +187,11 @@ class PeakShadow(strax.OverlapWindowPlugin):
         dtype += [("x", np.float32), ("y", np.float32)]
         dtype += [("nearest_dt", np.int64)]
         return dtype
+
+    def setup(self):
+        self.s2_area_limit = self.position_shadow_s2_area_limit
+        self.ellipse_parameters = self.position_shadow_ellipse_parameters_mode[self.run_mode]
+        self.straight_parameters = self.position_shadow_straight_parameters_mode[self.run_mode]
 
     def compute(self, peaks):
         argsort = np.argsort(peaks["center_time"], kind="mergesort")
@@ -214,8 +264,11 @@ class PeakShadow(strax.OverlapWindowPlugin):
         result["pdf_s2_position_shadow"] = halfcauchy.pdf(
             distance, scale=self.getsigma(self.shadow_sigma_and_baseline, current_peak["area"])
         )
-
-        # 6. Set time and endtime for peaks
+        # 6. mark the S2 peaks failed the position shadow selection as type-21
+        result["type"] = np.where(
+            self.compute_position_shadow_cut(peaks, result), peaks["type"], 21
+        )
+        # 7. Set time and endtime for peaks
         result["time"] = current_peak["time"]
         result["endtime"] = strax.endtime(current_peak)
         return result
@@ -256,3 +309,41 @@ class PeakShadow(strax.OverlapWindowPlugin):
                     result["x"][p_i] = casting_peak["x"]
                     result["y"][p_i] = casting_peak["y"]
                     result["dt"][p_i] = suspicious_peak["center_time"] - casting_peak["center_time"]
+
+    @staticmethod
+    @np.errstate(invalid="ignore")
+    def prob_cut_line(log10_pdf, x0, y0, a, b, rlimit, pstraight):
+        top = 0
+        # The top line in 2D histogram
+        topline = np.full(len(log10_pdf), top)
+
+        # Draw a half ellipse
+        halfellipse = np.where(
+            np.abs(log10_pdf - x0) < a * rlimit,
+            y0 - np.sqrt(b**2 * (rlimit**2 - (log10_pdf - x0) ** 2 / a**2)),
+            top,
+        )
+
+        # A horizontal tangent line at the bottom of ellipse
+        botline = np.where(log10_pdf > x0, y0 - b * rlimit, top)
+
+        # A straight line with slope
+        straight = np.polyval(pstraight, log10_pdf)
+
+        # The lowest of 4 lines
+        line = np.min(np.vstack([topline, halfellipse, botline, straight]), axis=0)
+        return line
+
+    @np.errstate(divide="ignore")
+    def compute_position_shadow_cut(self, peaks, peak_shadows):
+        log10_time_shadow = np.log10(
+            peak_shadows["shadow_s2_position_shadow"] / peak_shadows["pdf_s2_position_shadow"]
+        )
+        log10_pdf = np.log10(peak_shadows["pdf_s2_position_shadow"])
+        mask = ~(
+            log10_time_shadow
+            > self.prob_cut_line(log10_pdf, *self.ellipse_parameters, self.straight_parameters)
+        )
+        mask |= peaks["area"] > self.s2_area_limit
+        mask |= peaks["type"] != 2
+        return mask
