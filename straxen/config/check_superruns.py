a92fc85f0ecd81113869173766ec6f73a0ac5a34
Dacheng Xu <dx2227@columbia.edu>
2025-01-26 17:10:31 -0500

    Make sure all subruns have the same configs (#1546)
    * Make sure all subruns have the same configs
    
    * Add test of the safeguard
    
    * Add more tests
    
    * Skip the check if `check_superrun_configs` is False
    
    * Skip the checking in combining mode
    
    * Add more checks
    
    * Skpi the test if no DB access
    
    * debug

---
+++ straxen/config/check_superruns.py
@@ -0,0 +1,109 @@
+import strax
+import straxen
+
+
+def get_components_wrapper(func):
+    """Check whether all configs for superruns-allowed plugins are the same for subruns."""
+
+    def wrapper(self, run_id, targets=tuple(), **kwargs):
+        is_superrun = run_id.startswith("_")
+        if (
+            not is_superrun
+            or not self.context_config["check_superrun_configs"]
+            or kwargs.get("combining", False)
+        ):
+            return func(self, run_id=run_id, targets=targets, **kwargs)
+
+        configs = self._superrun_configs(run_id, targets)
+        if not configs:
+            return func(self, run_id=run_id, targets=targets, **kwargs)
+        superrun_hashes = {run_id: _hashed_url_config(configs)}
+
+        # collect all url config of all subruns and convert them into hash
+        sub_run_spec = self.run_metadata(run_id, projection="sub_run_spec")["sub_run_spec"]
+        for sub_run_id in sub_run_spec:
+            configs = self._superrun_configs(sub_run_id, targets)
+            superrun_hashes[sub_run_id] = _hashed_url_config(configs)
+
+        # check whether all subruns have the same configs
+        seen = strax.deterministic_hash(superrun_hashes[run_id])
+        for sub_run_id, value in superrun_hashes.items():
+            if seen != strax.deterministic_hash(value):
+                diff = [k for k in value if value[k] != superrun_hashes[run_id][k]]
+                raise ValueError(
+                    "Superrun configs are not the same for all subruns. "
+                    f"Specifically, the following configs are different: {diff}."
+                )
+
+    return wrapper
+
+
+strax.Context.get_components = get_components_wrapper(strax.Context.get_components)
+
+
+@strax.Context.add_method
+def _superrun_configs(self, run_id, targets):
+    targets = strax.to_str_tuple(targets)
+    plugins = self._get_plugins(targets=targets, run_id=run_id)
+    configs = dict()
+    for data_type, plugin in plugins.items():
+        if plugin.allow_superrun:
+            for k, v in plugin.config.items():
+                if not isinstance(v, str):
+                    continue
+                configs[k] = (v, plugins[data_type])
+    return configs
+
+
+PLUGIN_ATTR_CONVERT = ["run_id", "algorithm"]
+
+
+def _hashed_url_config(configs):
+    """Convert all url into string and hash them."""
+    # get all configs for superruns-allowed plugins if they are str
+    hash = dict()
+    for key, value in configs.items():
+        url, plugin = value
+        url = straxen.URLConfig.format_url_kwargs(url)
+        arg, extra_kwargs = straxen.URLConfig.split_url_kwargs(url)
+        # only extract value, avoid attr=map
+        if "attr" in extra_kwargs:
+            extra_kwargs["attr"] = "value"
+        # transform run_id and algorithm from plugin
+        # run_id and algorithm are the only two keys that
+        # can be extracted from plugin for now
+        # if later we have more keys need to be converted from plugin
+        # to make xedocs work, we need to add them here
+        for k in PLUGIN_ATTR_CONVERT:
+            if k in extra_kwargs:
+                extra_kwargs[k] = getattr(
+                    plugin,
+                    extra_kwargs[k].partition(straxen.URLConfig.NAMESPACE_SEP)[-1],
+                )
+        # filter out plugin related kwargs
+        _extra_kwargs = dict()
+        for k, v in extra_kwargs.items():
+            flag = not isinstance(v, str)
+            flag |= isinstance(v, str) and straxen.URLConfig.PLUGIN_ATTR_PREFIX not in v
+            if flag:
+                _extra_kwargs[k] = v
+        url = straxen.URLConfig.format_url_kwargs(arg, **_extra_kwargs)
+        if "resource" in url:
+            protocol, arg, kwargs = straxen.URLConfig.url_to_ast(url)
+            while protocol != "resource":
+                protocol, arg, kwargs = arg
+            if isinstance(arg, tuple):
+                url = straxen.URLConfig.ast_to_url(arg)
+            else:
+                url = arg
+        evaluated = straxen.URLConfig.evaluate_dry(url)
+        try:
+            strax.hashablize(evaluated)
+        except TypeError as e:
+            raise ValueError(
+                f"Cannot hash url {url} converted from config {key} in superrun safeguard. "
+                f"Because its evaluated value not hashable: {evaluated}. "
+                f"Error: {e}"
+            )
+        hash[key] = strax.deterministic_hash(evaluated)
+    return hash
