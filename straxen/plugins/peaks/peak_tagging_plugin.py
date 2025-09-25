0ab613fc202950db1ad3e78d21a1d0597b784c32
RoiFrankel <roi.frankel@weizmann.ac.il>
2025-09-25 16:24:22 +0300

    Fix comments and formatting in peak tagging plugin

--- straxen/plugins/peaks/peak_tagging_plugin.py
+++ straxen/plugins/peaks/peak_tagging_plugin.py
@@ -1,5 +1,4 @@
 import strax
-import straxen
 import numpy as np
 
 export, __all__ = strax.exporter()
@@ -27,24 +26,24 @@ class peak_tagging(strax.Plugin):
         ]
         return dtype
 
-    ### fuction is used to devide between diffrent populations in the S2 area vs range_50p_area plane
+    # fuction is used to devide between diffrent populations in the S2 area vs range_50p_area plane
     def linear_line(self, data, x1, x2, y1, y2):
         return (data - x1) * (y2 - y1) / (x2 - x1) + y1
 
     def compute(self, events, peaks):
 
-        ### plugin constants
-        ### first solid seperation limit points
+        # plugin constants
+        # first solid seperation limit points
         x_points_1spl = np.array([1.5e3, 2.5e3, 2.9e3, 3.3e3])
         y_points_1spl = np.array([5e1, 6e3, 8e3, 1e4])
 
-        ### second solid seperation limit point
+        # second solid seperation limit point
         x_points_2spl = np.array(
             [3.3e3, 4e3, 5.5e3, 7.5e3, 1.03e4]
-        )  ## x points for the left-up line, second part of the S2 region seperation line
+        )  # x points for the left-up line, second part of the S2 region seperation line
         y_points_2spl = np.array(
             [1e4, 1.15e4, 1.5e4, 2.4e4, 3.6e4]
-        )  ## y points for the left-up line, second part of the S2 region seperation line
+        )  # y points for the left-up line, second part of the S2 region seperation line
 
         # Polynomial fits for the first and second solid seperation limit lines
         coefficients_1spl = np.polyfit(x_points_1spl, y_points_1spl, len(x_points_1spl) - 1)
@@ -74,7 +73,7 @@ class peak_tagging(strax.Plugin):
             coefficients_2spl, x_points_2spl[0]  # Y-value at start of second polynomial curve
         )  # (used for offset calculation in mask_S2_V2, mask_in_edge_2)
 
-        ### end of plugin constats
+        # end of plugin constats
 
         split_peaks = strax.split_by_containment(peaks, events)
         split_peaks_ind = strax.fully_contained_in(peaks, events)
@@ -82,18 +81,18 @@ class peak_tagging(strax.Plugin):
         drift_times = np.full(len(peaks), np.nan, dtype=np.float32)
         peaks_tags = np.full(
             len(peaks), "S2_not_associated_to_event", dtype="U50"
-        )  ## initial value for all peaks, array of peak tags to fill
+        )  # initial value for all peaks, array of peak tags to fill
         for event_i, (event, sp) in enumerate(zip(events, split_peaks)):
             mask = split_peaks_ind == event_i  ## mask for peaks associated with the current event
             drift_times[mask] = sp["center_time"] - event["s1_center_time"]
             temp_peaks_tags_array = np.full(
                 len(sp["area"]), "undefined", dtype="U50"
-            )  ### temporary array to hold the peak tags for the current event
+            )  # temporary array to hold the peak tags for the current event
 
             sp_S2_mask = sp["type"] == 2  ## mask for S2 peaks associated with the current event
 
-            ### creating the physical S2 region mask
-            ######## start S2 area>V3
+            # creating the physical S2 region mask
+            # start S2 area>V3
             mask_S2_V3 = (
                 (sp["area"] > v3)
                 & (sp["range_50p_area"] > bottom_physical_horizontal_line_2)
@@ -102,8 +101,8 @@ class peak_tagging(strax.Plugin):
                     < self.linear_line(sp["area"], v3, x_2_of_4spl, y_2_of_3spl, y_2_of_4spl)
                 )
             )
-            ######## end S2 area>V3
-            ######## start V2<S2 area<V3
+            # end S2 area>V3
+            # start V2<S2 area<V3
             mask_S2_V2 = (
                 (sp["area"] <= v3)
                 & (sp["area"] > x_points_2spl[2])
@@ -119,28 +118,28 @@ class peak_tagging(strax.Plugin):
                     )
                 )
             )
-            ######## end V2<S2 area<V3
-            ######## start V1<S2 area<V2
+            # end V2<S2 area<V3
+            # start V1<S2 area<V2
             mask_S2_V1 = (
                 (sp["area"] > x_points_2spl[0])
                 & (sp["area"] <= x_points_2spl[2])
                 & (sp["range_50p_area"] > bottom_physical_horizontal_line_1)
                 & (sp["range_50p_area"] < np.polyval(coefficients_2spl, sp["area"]))
             )
-            ######## end V1<S2 area<V2
-            ######## start S2 area<V1
+            # end V1<S2 area<V2
+            # start S2 area<V1
             mask_S2_V0 = (
                 (sp["area"] <= x_points_2spl[0])
                 & (sp["area"] > x_points_1spl[0])
                 & (sp["range_50p_area"] > bottom_physical_horizontal_line_1)
                 & (sp["range_50p_area"] < np.polyval(coefficients_1spl, sp["area"]))
             )
-            ######## end S2 area<V1
+            # end S2 area<V1
             mask_S2_physical_region = mask_S2_V0 | mask_S2_V1 | mask_S2_V2 | mask_S2_V3
-            ### end of creating the physical S2 region mask
+            # end of creating the physical S2 region mask
 
-            ### creating the edge region mask
-            ##### start edge region 2
+            # creating the edge region mask
+            # start edge region 2
             mask_in_edge_2 = (
                 (sp["area"] <= stop_point_edge)
                 & (sp["area"] > x_points_2spl[2])
@@ -156,27 +155,27 @@ class peak_tagging(strax.Plugin):
                     )
                 )
             )
-            ##### end edge region 2
-            ##### start edge region 1 and 0
+            # end edge region 2
+            # start edge region 1 and 0
             mask_in_edge_1 = (
                 (sp["area"] > x_points_2spl[0])
                 & (sp["area"] <= x_points_2spl[2])
                 & (sp["range_50p_area"] > y_mid_for_edge)
                 & (sp["range_50p_area"] < np.polyval(coefficients_2spl, sp["area"]))
             )
-            ##### end edge region 1
-            ##### start edge region 0
+            # end edge region 1
+            # start edge region 0
             mask_in_edge_0 = (
                 (sp["area"] <= x_points_2spl[0])
                 & (sp["area"] > x_points_1spl[0])
                 & (sp["range_50p_area"] > y_mid_for_edge)
                 & (sp["range_50p_area"] < np.polyval(coefficients_1spl, sp["area"]))
             )
-            ##### end edge region 0
+            # end edge region 0
             mask_in_edge = mask_in_edge_0 | mask_in_edge_1 | mask_in_edge_2
-            ### end of creating the edge region mask
+            # end of creating the edge region mask
 
-            ### creating the e-train leakage mask based on the seperation parameters and area/dt*length condition
+            # creating the e-train leakage mask based on the seperation parameters and area/dt*length condition
             # Safe division with NaN/zero handling
             # Calculate denominators first to check for zeros/NaNs
             denom_1 = sp["endtime"] - sp["time"]
@@ -239,9 +238,9 @@ class peak_tagging(strax.Plugin):
             mask_e_train_leakage_2 = np.isfinite(variable_to_check) & (variable_to_check < 0.3)
 
             mask_e_train_leakage = mask_e_train_leakage_1 & mask_e_train_leakage_2
-            ### end of creating the e-train leakage mask
+            # end of creating the e-train leakage mask
 
-            ###### do the peaks tagging per event
+            # do the peaks tagging per event
             temp_peaks_tags_array[(sp_S2_mask) & (mask_S2_physical_region) & (~mask_in_edge)] = (
                 "S2_physical"
             )
@@ -254,7 +253,7 @@ class peak_tagging(strax.Plugin):
             temp_peaks_tags_array[(sp_S2_mask) & (~mask_S2_physical_region)] = "S2_not_physical"
             peaks_tags[mask] = temp_peaks_tags_array
 
-        ### finalizing the peak tagging by adding the S1 and undefined tags for all of the peaks
+        # finalizing the peak tagging by adding the S1 and undefined tags for all of the peaks
         undefined_mask = peaks["type"] == 0
         peaks_tags[undefined_mask] = "undefined"
         mask_S1 = peaks["type"] == 1
