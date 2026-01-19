f5e812f464d325326f6fefee5988171cf1574b7f
cfuselli <cfuselli@nikhef.nl>
2026-01-19 10:15:13 -0600

    remove prints

--- straxen/config/url_config.py
+++ straxen/config/url_config.py
@@ -128,9 +128,6 @@ class URLConfig(strax.Config):
         :return: (Any) The return value of the protocol on these arguments
 
         """
-
-        print("URL EVAL", protocol, arg, kwargs)
-
         
         if protocol is not None and arg is None:
             protocol, arg, kwargs = cls.url_to_ast(protocol)
@@ -148,8 +145,6 @@ class URLConfig(strax.Config):
 
         # Just to be on the safe side
         kwargs = straxen.filter_kwargs(meth, kwargs)
-
-        print("RETURN METH", arg, kwargs)
         
         return meth(arg, **kwargs)
 
