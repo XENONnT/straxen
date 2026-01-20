61492f06537e485abce1a03d5eca73f4a2df5165
cfuselli <cfuselli@nikhef.nl>
2026-01-20 09:14:37 -0600

    no som

--- straxen/plugins/peaks/peaks_vanilla.py
+++ straxen/plugins/peaks/peaks_vanilla.py
@@ -81,6 +81,7 @@ class PeaksVanilla(strax.Plugin):
 
         return peaks
 
+
     @staticmethod
     def replace_merged(peaklets, merged_s2s, merge_s0=True):
         # pick out type FAR_XYPOS_S2_TYPE because they might overlap with other merged S2s
