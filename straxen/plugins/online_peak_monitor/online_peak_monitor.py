4a91462821d9632d8b086b57eaaf045544c2979d
Felix Semler <68687832+Naderschah@users.noreply.github.com>
2026-03-19 13:36:00 +0100

    Changed defaults for om plot

--- straxen/plugins/online_peak_monitor/online_peak_monitor.py
+++ straxen/plugins/online_peak_monitor/online_peak_monitor.py
@@ -35,7 +35,7 @@ class OnlinePeakMonitor(strax.Plugin):
 
     area_vs_width_bounds = straxen.URLConfig(
         type=tuple,
-        default=((0, 5), (0, 5)),
+        default=((-2.5, 0.5), (-2.5, 0.5)),
         help="Boundaries of log-log histogram of area vs width",
     )
 
@@ -85,7 +85,7 @@ class OnlinePeakMonitor(strax.Plugin):
 
     online_se_bounds = straxen.URLConfig(
         type=tuple,
-        default=(7, 70),
+        default=(15, 70),
         help="Window for online monitor [PE] to look for the SE gain, value",
     )
 
